6453a053fc7edf3cb28afd155092494034bf1c34
Alastair Black <a.black@GWScience.co.uk>
2019-04-30 13:54:32 +0100

    LU weighting validation
    Tolerance added to LU weighting validation

--- swacmod/validation.py
+++ swacmod/validation.py
@@ -1177,8 +1177,8 @@ def val_lu_spatial(data, name):
                  keys=range(1, tot + 1),
                  len_list=[len(lzn)])
 
-    if not all(sum(i) == 1.0 for i in lus.values()):
-        msg = 'Parameter "%s" requires the sum of its values to be 1.0'
+    if not all(abs(1 - sum(i)) < 1e-5  for i in lus.values()):
+        msg = 'Parameter "%s" requires the sum of its values to be 1.0 within a tolerance of 1e-5'
         raise u.ValidationError(msg % name)
 
 
