796598182474a189d65eabbcfca2890056615074
wihtric <wihtric@gmail.com>
2021-02-10 15:02:02 +0000

    Dev(natproc): fin_sw_direct_recharge

--- swacmod/finalization.py
+++ swacmod/finalization.py
@@ -1050,14 +1050,14 @@ def fin_sw_direct_recharge(data, name):
     1) if not provided, set it to 99999.
     """
     params = data["params"]
-    zones = data["params"]["sw_zone_names"]
-    if params[name] is None:
-        params[name] = dict((k, [99999.9 for _ in zones])
-                            for k in range(1, 13))
-        logging.info('\t\tDefaulted "%s" to [99999]', name)
 
-    params["sw_direct_rech"] = sorted(params[name].items(), key=lambda x: x[0])
-    params["sw_direct_rech"] = np.array([i[1] for i in params["sw_direct_rech"]])
+    if params[name] != None:
+        # params[name] = dict((k, [99999.9 for _ in zones])
+        #                     for k in range(1, 13))
+        # logging.info('\t\tDefaulted "%s" to [99999]', name)
+        zones = data["params"]["sw_zone_names"]
+        params["sw_direct_rech"] = sorted(params[name].items(), key=lambda x: x[0])
+        params["sw_direct_rech"] = np.array([i[1] for i in params["sw_direct_rech"]])
 
 ###############################################################################
 def fin_sw_activation(data, name):
