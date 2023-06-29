2212b1a4e355f6368daaa743ffbe834af5a1f0cf
Chris Foley <chris@nice-code.co.uk>
2023-06-29 17:40:24 +0100

    Time series data can be backed by a file.

--- swacmod/time_series_data.py
+++ swacmod/time_series_data.py
@@ -1,6 +1,8 @@
 import ast
 import csv
 import logging
+import numpy
+import os
 import yaml
 
 try:
@@ -29,11 +31,32 @@ class CsvTimeSeriesData(TimeSeriesData):
 			rows = [[ast.literal_eval(j) for j in row]
 					for row in reader]
 			
-			print(f"type(rows) = {type(rows)}")
-			print(f"type(rows[0]) = {type(rows[0])}")
-			print(f"type(rows[0][0]) = {type(rows[0][0])}")
-
 			self.rows = rows
+		except IndexError as err:
+			message = f"Could not read file: {csv_filename}"
+			raise u.InputOutputError(message)
+
+	def row(self, index):
+		return self.rows[index]
+
+	def __len__(self):
+		return len(self.rows)
+
+	def __getitem__(self, subscript):
+		return self.rows.__getitem__(subscript)
+
+class CsvTimeSeriesData_File_Backed(TimeSeriesData):
+	def __init__(self, param_name, csv_filename):
+		try:
+			reader = csv.reader(open(csv_filename, "r"))
+		except IOError as err:
+			message = f"Could not read file: {csv_filename}"
+			raise u.InputOutputError(message)
+		try:
+			rows = [[ast.literal_eval(j) for j in row]
+					for row in reader]
+			
+			self.rows = convert_rows_to_file_backed_array(rows, csv_filename)
 		except IndexError as err:
 			message = f"Could not read file: {csv_filename}"
 			raise u.InputOutputError(message)
@@ -65,6 +88,56 @@ class YamlTimeSeriesData(TimeSeriesData):
 	def __getitem__(self, subscript):
 		return self.rows.__getitem__(subscript)
 
+class YamlTimeSeriesData_File_Backed(TimeSeriesData):
+	def __init__(self, param_name, csv_filename):
+		try:
+			rows = load_yaml(csv_filename)[param_name]
+		except (IOError, KeyError) as err:
+			message = f"Could not read file: {csv_filename}"
+			raise u.InputOutputError(message)
+		self.rows = convert_rows_to_file_backed_array(rows, csv_filename)
+
+	def row(self, index):
+		return self.rows[index]
+
+	def __len__(self):
+		return len(self.rows)
+	
+	def __getitem__(self, subscript):
+		return self.rows.__getitem__(subscript)
+
+def convert_rows_to_file_backed_array(rows, csv_filename):
+	dtype = calculate_dtype_for_python_list(rows)
+	shape = calculate_shape_for_python_list(rows)
+	result = numpy.memmap(
+		filename = calculate_filename_for_backing_file(csv_filename), 
+		dtype = dtype,
+		mode = "w+",
+		shape = shape)
+	result[:] = rows[:]
+	return result
+
+def calculate_filename_for_backing_file(filename):
+	basename = os.path.basename(filename)
+	result = "temp_scratch_files/" + basename
+	return result
+
+def calculate_dtype_for_python_list(x):
+	if (isinstance(x, float)):
+		return float
+	if (isinstance(x, list)):
+		return calculate_dtype_for_python_list(x[0])
+	message = f"Cannot determine dtype for {x}.\n Type = {type(x)}"
+	raise Exception(message)
+
+def calculate_shape_for_python_list(x):
+	scalars = (float)
+	if (isinstance(x[0], scalars)):
+		return (len(x))
+	if (isinstance(x[0][0], scalars)):
+		return (len(x), len(x[0]))
+	message = f"Cannot determine shape for {x}.\n Type = {type(x)}"
+	raise Exception(message)
 
 # Copied from input_output.py
 def load_yaml(filein):
@@ -86,8 +159,20 @@ def load_yaml(filein):
                 yml[new_key] = value
     return yml
 
+def calculate_is_in_memory(filename):
+	file_size = os.stat(filename).st_size
+	memory_cutoff = 300000
+	return file_size < memory_cutoff
+
 def load_time_series_data(param, filename, ext):
+	is_in_memory = calculate_is_in_memory(filename)
 	if ext == "csv":
-		return CsvTimeSeriesData(param, filename)
+		if is_in_memory:
+			return CsvTimeSeriesData(param, filename)
+		else:
+			return CsvTimeSeriesData_File_Backed(param, filename)
 	elif ext == "yml":
-		return YamlTimeSeriesData(param, filename)
+		if is_in_memory:
+			return YamlTimeSeriesData(param, filename)
+		else:
+			return YamlTimeSeriesData_File_Backed(param, filename)
