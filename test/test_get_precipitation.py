5a104d68aeba01d7230de7835088b1aa4ef5c48e
Chris Foley <chris@nice-code.co.uk>
2023-07-10 17:51:08 +0100

    Move calculations to numpy. 6

--- test/test_get_precipitation.py
+++ test/test_get_precipitation.py
@@ -17,6 +17,13 @@ rainfall_zone_mapping = [
 	[3, 7, 13,],
 	[5, 17, 23,],
 ]
+
+expected_precipitation = numpy.array([
+	[2*1, 2*2, 2*3, 2*4, 2*5],
+	[7*2, 7*4, 7*6, 7*8, 7*10],
+	[17*4, 17*8, 17*12, 17*16, 17*20],
+], dtype=float)
+
 class Test_Get_Precipitation(unittest.TestCase):
 	def test_get_precipitation(self):
 		input_data = {
@@ -27,15 +34,10 @@ class Test_Get_Precipitation(unittest.TestCase):
 				"rainfall_zone_mapping": rainfall_zone_mapping
 			},
 		}
-		expected = numpy.array([
-			[2*1, 2*2, 2*3, 2*4, 2*5],
-			[7*2, 7*4, 7*6, 7*8, 7*10],
-			[17*4, 17*8, 17*12, 17*16, 17*20],
-		], dtype=float)
 		actual_oracle = oracle_get_precipitation(input_data)
 		actual_numpy = numpy_get_precipitation(input_data)
-		numpy.testing.assert_array_equal(expected, actual_oracle)
-		numpy.testing.assert_array_equal(expected, actual_numpy)
+		numpy.testing.assert_array_equal(expected_precipitation, actual_oracle)
+		numpy.testing.assert_array_equal(expected_precipitation, actual_numpy)
 
 def oracle_get_precipitation(data):
 	result = []
