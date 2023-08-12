8f6fcf2f926e61ec386c2e64f9152fa9ce917dea
Chris Foley <chris@nice-code.co.uk>
2023-08-12 11:32:32 +0100

    Limit size of model for testing. 8

--- swacmod/feature_flags.py
+++ swacmod/feature_flags.py
@@ -2,5 +2,5 @@ import os
 
 use_perf_features = "True" == os.environ.get("SWAc_use_perf_features", "False")
 
-max_node_count_override = 1
-max_time_period_override = 1
+max_node_count_override = 610
+max_time_period_override = 1000
