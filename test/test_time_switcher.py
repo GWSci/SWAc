7cc61c58301f363443b2b5c877119ba471a9329a
Chris Foley <chris@nice-code.co.uk>
2023-08-12 17:55:33 +0100

    Iterative performance optimisation. 32

--- test/test_time_switcher.py
+++ test/test_time_switcher.py
@@ -2,35 +2,40 @@ import unittest
 import swacmod.timer as timer
 
 class test_time_switcher(unittest.TestCase):
-    def test_time_switcher_initially_has_an_empty_report(self):
-        time_switcher = timer.make_time_switcher()
-        actual = timer.time_switcher_report(time_switcher)
-        expected = []
-        self.assertEqual(expected, actual)
+	def test_time_switcher_initially_has_an_empty_report(self):
+		time_switcher = timer.make_time_switcher()
+		actual = timer.time_switcher_report(time_switcher)
+		expected = []
+		self.assertEqual(expected, actual)
 
-    def test_time_switcher_calling_switch_off_when_not_started_makes_no_change(self):
-        time_switcher = timer.make_time_switcher()
-        timer.switch_off(time_switcher)
-        actual = timer.time_switcher_report(time_switcher)
-        expected = []
-        self.assertEqual(expected, actual)
+	def test_time_switcher_calling_switch_off_when_not_started_makes_no_change(self):
+		time_switcher = timer.make_time_switcher()
+		timer.switch_off(time_switcher)
+		actual = timer.time_switcher_report(time_switcher)
+		expected = []
+		self.assertEqual(expected, actual)
 
-    def test_time_switcher_calling_switch_to_does_not_immediately_update_the_report(self):
-        time = mock_time([2, 3])
-        time_switcher = timer.make_time_switcher()
-        timer.switch_to(time_switcher, "aardvark")
-        actual = timer.time_switcher_report(time_switcher)
-        expected = []
-        self.assertEqual(expected, actual)
+	def test_time_switcher_calling_switch_to_does_not_immediately_update_the_report(self):
+		time = mock_time([2, 3])
+		time_switcher = timer.make_time_switcher()
+		timer.switch_to(time_switcher, "aardvark")
+		actual = timer.time_switcher_report(time_switcher)
+		expected = []
+		self.assertEqual(expected, actual)
 
-    def test_time_switcher_has_one_row_after_starting_and_stopping(self):
-        time = mock_time([2, 3])
-        time_switcher = timer.make_time_switcher()
-        timer.switch_to(time_switcher, "aardvark")
-        timer.switch_off(time_switcher)
-        actual = timer.time_switcher_report(time_switcher)
-        expected = [{"message": "aardvark", "elapsed_seconds": 1}]
-        self.assertEqual(expected, actual)
+	def test_time_switcher_has_one_row_after_starting_and_stopping(self):
+		time = mock_time([2, 3])
+		time_switcher = timer.make_time_switcher()
+		timer.switch_to(time_switcher, "aardvark")
+		timer.switch_off(time_switcher)
+		actual = timer.time_switcher_report(time_switcher)
+		expected = [{"message": "aardvark", "elapsed_seconds": 1}]
+		self.assertEqual(expected, actual)
+
+	def assert_report(self, time_switcher, expected_report):
+		actual = timer.time_switcher_report(time_switcher)
+		self.assertEqual(expected_report, actual)
+
 
 class mock_time():
 	def __init__(self, times):
