084213dd879c42b7b729c529a53ac670fb4a2e44
Chris Foley <chris@nice-code.co.uk>
2023-09-18 12:10:53 +0100

    Write performance to log file when --debug flag is on. 24

--- swacmod/performance_logging.py
+++ swacmod/performance_logging.py
@@ -1,8 +1,6 @@
-import datetime
 import logging
 
+# This has been extracted as performance logging should be done at debug level normally
+# but possibly at a higher level, or even printed to the console when working on performance.
 def log_performance(message):
-	timestamp = datetime.datetime.now()
-	line = f"{timestamp} : {message}"
-	print(line)
 	logging.debug(message)
