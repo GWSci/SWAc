b90c423acef0cbecf8a0f4e7acdb1390818f4375
Chris Foley <chris@nice-code.co.uk>
2023-09-18 06:34:34 +0100

    File-backed array location is configurable. 5

--- swacmod/no_timer.py
+++ swacmod/no_timer.py
@@ -2,12 +2,33 @@ import datetime
 import numpy
 import time
 
+def make_time_switcher():
+	pass
+
+def switch_to(time_switcher, message, time=time):
+	pass
+
+def switch_off(time_switcher, time=time):
+	pass
+
+def time_switcher_report(time_switcher):
+	pass
+
+def print_time_switcher_report(time_switcher):
+	pass
+
+def make_accumulation_timer(message):
+	pass
+
 def report_time(message, function):
 	result = function()
 	return result
 
 def start_timing(message):
 	return {}
+
+def continue_timing(token, time=time):
+	pass
 
 def stop_timing(token):
 	pass
