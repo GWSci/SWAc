0699b672a629b065193b83a79ea748aa496734f0
Chris Foley <chris@nice-code.co.uk>
2023-09-18 07:24:13 +0100

    File-backed array location is configurable. 9

--- test/test_filename_for_backing_file.py
+++ test/test_filename_for_backing_file.py
@@ -12,3 +12,11 @@ class Test_Filename_For_Backing_File(unittest.TestCase):
 		actual = ts.calculate_filename_for_backing_file(base_path, filename, shape)
 		expected = f"aardvark{sep}cat.csv.dog.swacmod_array"
 		self.assertEqual(expected, actual)
+
+	def test_backing_filename_converts_forward_slashes_to_current_operating_system(self):
+		base_path = "aardvark/bat/"
+		filename = "cat.csv"
+		shape = "dog"
+		actual = ts.calculate_filename_for_backing_file(base_path, filename, shape)
+		expected = f"aardvark{sep}bat{sep}cat.csv.dog.swacmod_array"
+		self.assertEqual(expected, actual)
