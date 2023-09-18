75b2e3fd39dee06b93f77443c17d9e92a401497f
Chris Foley <chris@nice-code.co.uk>
2023-09-18 07:22:55 +0100

    File-backed array location is configurable. 8

--- test/test_filename_for_backing_file.py
+++ test/test_filename_for_backing_file.py
@@ -1,11 +1,14 @@
-import unittest
 import swacmod.time_series_data as ts
+import unittest
+import os
+
+sep = os.sep
 
 class Test_Filename_For_Backing_File(unittest.TestCase):
-	def test_x(self):
-		base_path = "aardvark/bat/"
+	def test_backing_filename_combines_base_directory_filename_and_meta_data(self):
+		base_path = "aardvark/"
 		filename = "cat.csv"
 		shape = "dog"
 		actual = ts.calculate_filename_for_backing_file(base_path, filename, shape)
-		expected = "aardvark/bat/cat.csv.dog.swacmod_array"
+		expected = f"aardvark{sep}cat.csv.dog.swacmod_array"
 		self.assertEqual(expected, actual)
