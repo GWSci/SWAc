49889537ca643ec3b6047b2a57ccd99928789e88
Chris Foley <chris@nice-code.co.uk>
2023-09-20 10:57:19 +0100

    Reducing feature flags.

--- swacmod/feature_flags.py
+++ swacmod/feature_flags.py
@@ -9,11 +9,11 @@ _skip_validation = False
 
 # Set master flag from environment variable
 
-_use_perf_features = "True" == os.environ.get("SWAc_use_perf_features", "False")
+_use_experimental_perf_features = "True" == os.environ.get("SWAc_use_perf_features", "False")
 
 # Flags to query in code
 
-use_node_count_override = _node_count_override and _use_perf_features
+use_node_count_override = _node_count_override and _use_experimental_perf_features
 max_node_count_override = _max_node_count_override if use_node_count_override else 10000000
-disable_multiprocessing = _disable_multiprocessing and _use_perf_features
-skip_validation = _skip_validation and _use_perf_features 
+disable_multiprocessing = _disable_multiprocessing and _use_experimental_perf_features
+skip_validation = _skip_validation and _use_experimental_perf_features 
