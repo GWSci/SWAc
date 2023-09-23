52753bf6669bbbb95d205a192435c00b57acddae
Chris Foley <chris@nice-code.co.uk>
2023-09-23 22:24:03 +0100

    Preparing to merge into old master. 40

--- swacmod/cymodel.pyx
+++ swacmod/cymodel.pyx
@@ -757,13 +757,11 @@ def get_recharge(data, output, node):
                                      output['pond_atten'][0]))
 
         for num in range(1, length):
-            recharge[num] = (recharge_store_input[num-1] +
-                             col_recharge_store[num-1] -
-                             (col_combined_recharge[num-1] -
-                              ((1.0 - pond_area) *
-                               macropore_dir[num-1])
-                               - (pond_area * (output['pond_direct'][num-1] +
-                                               output['pond_atten'][num-1]))))
+            recharge[num] = (recharge_store_input[num-1]
+                             + col_recharge_store[num-1]
+                             - col_combined_recharge[num-1]
+                             + ((1.0 - pond_area) * macropore_dir[num-1])
+                             + (pond_area * (output['pond_direct'][num-1] + output['pond_atten'][num-1])))
 
             col_recharge_store[num] = recharge[num]
             col_combined_recharge[num] = (min((recharge[num] * rlp), rll) +
@@ -852,7 +850,6 @@ def get_mf6rch_file(data, rchrate):
                     spd[per][i] = ((0, irch[i, 0] - 1),
                                    rchrate[(nodes * per) + i + 1] * fac)
                 else:
-
                     spd[per][i] = ((irch[i, 0] - 1,),
                                    rchrate[(nodes * per) + i + 1] * fac)
 
