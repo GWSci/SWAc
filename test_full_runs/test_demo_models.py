38605653ac6dcc100dd13897140b9733a71f07a1
Chris Foley <chris@nice-code.co.uk>
2023-09-24 15:41:44 +0100

    Preparing to merge into natproc. 55

--- test_full_runs/test_demo_models.py
+++ test_full_runs/test_demo_models.py
@@ -1,5 +1,12 @@
 import unittest
+from pathlib import Path
 
 class Test_Demo_Models(unittest.TestCase):
-	def test_x(self):
+	def test_demo_model(self):
+		reference_output_folder = "test/reference_output/"
+		output_folder = "output_files/"
+
+		for f in Path(output_folder).iterdir():
+			f.unlink()
+
 		self.assertEqual(1, 1)
