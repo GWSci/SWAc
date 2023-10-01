f8df47c69a19986f586842bc7be9504bd567f01d
Chris Foley <chris@nice-code.co.uk>
2023-10-01 11:48:51 +0100

    Nitrate. 5

--- test/test_load_params_from_yaml.py
+++ test/test_load_params_from_yaml.py
@@ -3,8 +3,10 @@ import swacmod.input_output as input_output
 
 class Test_Load_Params_From_Yaml(unittest.TestCase):
 	def test_nitrate_process_is_read_from_input_file(self):
-		input = input_output.load_params_from_yaml(
-			input_file="./test/resources/loading_params/input.yml", 
-			input_dir="./test/resources/loading_params/")
-		params = input["params"]
-		self.assertEqual("enabled", params["nitrate_process"])
+		self.assertEqual("enabled", load_params()["nitrate_process"])
+
+def load_params():
+	input = input_output.load_params_from_yaml(
+		input_file="./test/resources/loading_params/input.yml", 
+		input_dir="./test/resources/loading_params/")
+	return input["params"]
