e5dac21db2b535b368ff50fd9475e0e2518df7c2
Chris Foley <chris@nice-code.co.uk>
2023-10-01 12:06:34 +0100

    Nitrate. 6

--- test/test_load_params_from_yaml.py
+++ test/test_load_params_from_yaml.py
@@ -5,6 +5,14 @@ class Test_Load_Params_From_Yaml(unittest.TestCase):
 	def test_nitrate_process_is_read_from_input_file(self):
 		self.assertEqual("enabled", load_params()["nitrate_process"])
 
+	def test_nitrate_depth_to_water_is_read_from_input_file(self):
+		expected = {
+			1: [110],
+			2: [120],
+			3: [130],
+		}
+		self.assertEqual(expected, load_params()["nitrate_depth_to_water"])
+
 def load_params():
 	input = input_output.load_params_from_yaml(
 		input_file="./test/resources/loading_params/input.yml", 
