ce4eb1142cbe62a9ae17eced5bbb1500b6413d32
Chris Foley <chris@nice-code.co.uk>
2023-10-01 17:50:38 +0100

    Nitrate. 21

--- test/test_nitrate.py
+++ test/test_nitrate.py
@@ -29,6 +29,20 @@ class Test_Nitrate(unittest.TestCase):
 		self.assertAlmostEqual(0.7025, testee(200.0))
 		self.assertAlmostEqual(0.95, testee(310.0))
 
+	def test_cumulative_fraction_leaked_per_day(self):
+		her_at_5_percent = 10.0
+		her_at_50_percent = 110.0
+		her_at_95_percent = 310.0
+
+		testee = lambda her: cumulative_fraction_leaked_per_day(
+				her_at_5_percent, her_at_50_percent, her_at_95_percent, her)
+
+		self.assertAlmostEqual(0.05, testee(10.0 / 365.25))
+		self.assertAlmostEqual(0.32, testee(70.0 / 365.25))
+		self.assertAlmostEqual(0.5, testee(110.0 / 365.25))
+		self.assertAlmostEqual(0.7025, testee(200.0 / 365.25))
+		self.assertAlmostEqual(0.95, testee(310.0 / 365.25))
+
 def cumulative_fraction_leaked_per_year(
 		her_at_5_percent,
 		her_at_50_percent,
@@ -36,6 +50,22 @@ def cumulative_fraction_leaked_per_year(
 		her):
 	x = her
 	is_below_50_percent = her < her_at_50_percent
+	upper = her_at_50_percent if is_below_50_percent else her_at_95_percent
+	lower = her_at_5_percent if is_below_50_percent else her_at_50_percent
+	# y = mx + c
+	m = 0.45 / (upper - lower)
+	c = 0.5 - (her_at_50_percent * m)
+	y = (m * x) + c
+	return y
+
+def cumulative_fraction_leaked_per_day(
+		her_at_5_percent,
+		her_at_50_percent,
+		her_at_95_percent,
+		her_per_day):
+	her_per_year = 365.25 * her_per_day
+	x = her_per_year
+	is_below_50_percent = her_per_year < her_at_50_percent
 	upper = her_at_50_percent if is_below_50_percent else her_at_95_percent
 	lower = her_at_5_percent if is_below_50_percent else her_at_50_percent
 	# y = mx + c
