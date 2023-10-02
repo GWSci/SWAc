85e7596a452b16c84508bd68aba25eccd84a198e
Chris Foley <chris@nice-code.co.uk>
2023-10-02 16:19:46 +0100

    Nitrate. 40

--- swacmod/nitrate.py
+++ swacmod/nitrate.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 # + Derive a continuous relationship between annual HER and % leached nitrate
 #   for each model cell, based on the 5, 50 and 95% values given by NEAP-N (see
 #   bullet below for the csv of this).
@@ -33,3 +35,67 @@
 
 def _calculate_her_mm_per_day(data, output, node):
 	return output["rainfall_ts"] - output["ae"]
+
+def _calculate_m0_kg_per_day(data, output, node, her_array_mm_per_day):
+	params = data["params"]
+	cell_area_m_sq = params["node_areas"][node][0]
+	days = data["series"]["date"]
+
+	nitrate_leaching = params["nitrate_leaching"][node]
+	max_load_per_year_kg_per_hectare = nitrate_leaching[3]
+	her_at_5_percent = nitrate_leaching[4]
+	her_at_50_percent = nitrate_leaching[5]
+	her_at_95_percent = nitrate_leaching[6]
+
+	hectare_area_m_sq = 10000
+	max_load_per_year_kg_per_cell = max_load_per_year_kg_per_hectare * cell_area_m_sq / hectare_area_m_sq
+
+	m0_array_kg_per_day = _calculate_total_mass_leached_from_cell_on_days(
+		max_load_per_year_kg_per_cell,
+		her_at_5_percent,
+		her_at_50_percent,
+		her_at_95_percent,
+		days,
+		her_array_mm_per_day)
+	return m0_array_kg_per_day
+
+def _calculate_total_mass_leached_from_cell_on_days(
+		max_load_per_year_kg_per_cell,
+		her_at_5_percent,
+		her_at_50_percent,
+		her_at_95_percent,
+		days,
+		her_per_day):
+	length = len(days)
+	result = np.zeros(length)
+	remaining_for_year = max_load_per_year_kg_per_cell
+	for i in range(length):
+		day = days[i]
+		her = her_per_day[i]
+		if (day.month == 10) and (day.day == 1):
+			remaining_for_year = max_load_per_year_kg_per_cell
+		fraction_leached = _cumulative_fraction_leaked_per_day(her_at_5_percent,
+			her_at_50_percent,
+			her_at_95_percent,
+			her)
+		mass_leached_for_day = min(remaining_for_year, max_load_per_year_kg_per_cell * fraction_leached)
+		remaining_for_year -= mass_leached_for_day
+		result[i] = mass_leached_for_day
+	return result
+
+def _cumulative_fraction_leaked_per_day(
+		her_at_5_percent,
+		her_at_50_percent,
+		her_at_95_percent,
+		her_per_day):
+	days_in_year = 365.25
+	her_per_year = days_in_year * her_per_day
+	x = her_per_year
+	is_below_50_percent = her_per_year < her_at_50_percent
+	upper = her_at_50_percent if is_below_50_percent else her_at_95_percent
+	lower = her_at_5_percent if is_below_50_percent else her_at_50_percent
+	# y = mx + c
+	m = 0.45 / (upper - lower)
+	c = 0.5 - (her_at_50_percent * m)
+	y = (m * x) + c
+	return y / days_in_year
--- test/test_nitrate.py
+++ test/test_nitrate.py
@@ -35,7 +35,7 @@ class Test_Nitrate(unittest.TestCase):
 		her_at_50_percent = 110.0
 		her_at_95_percent = 310.0
 
-		testee = lambda her: cumulative_fraction_leaked_per_day(
+		testee = lambda her: nitrate._cumulative_fraction_leaked_per_day(
 				her_at_5_percent, her_at_50_percent, her_at_95_percent, her)
 
 		self.assertAlmostEqual(0.05 / 365.25, testee(10.0 / 365.25))
@@ -94,33 +94,9 @@ class Test_Nitrate(unittest.TestCase):
 		max_load_per_cell_per_year = 10000 * 365.25
 		expected = [0.6 * max_load_per_cell_per_year, 0.4 * max_load_per_cell_per_year, 0, 0.6 * max_load_per_cell_per_year, 0.4 * max_load_per_cell_per_year, 0]
 
-		actual = calculate_m0_kg_per_day(data, output, node, her_array_mm_per_day)
+		actual = nitrate._calculate_m0_kg_per_day(data, output, node, her_array_mm_per_day)
 
-		np.testing.assert_array_equal(expected, actual)
-
-def calculate_m0_kg_per_day(data, output, node, her_array_mm_per_day):
-	params = data["params"]
-	cell_area_m_sq = params["node_areas"][node][0]
-	days = data["series"]["date"]
-
-	nitrate_leaching = params["nitrate_leaching"][node]
-	max_load_per_year_kg_per_hectare = nitrate_leaching[3]
-	her_at_5_percent = nitrate_leaching[4]
-	her_at_50_percent = nitrate_leaching[5]
-	her_at_95_percent = nitrate_leaching[6]
-
-	hectare_area_m_sq = 10000
-	max_load_per_year_kg_per_cell = max_load_per_year_kg_per_hectare * cell_area_m_sq / hectare_area_m_sq
-	
-	m0_array_kg_per_day = calculate_total_mass_leached_from_cell_on_days(
-		max_load_per_year_kg_per_cell,
-		her_at_5_percent,
-		her_at_50_percent,
-		her_at_95_percent,
-		days,
-		her_array_mm_per_day)
-	return m0_array_kg_per_day
-		
+		np.testing.assert_array_equal(expected, actual)		
 
 def calculate_total_mass_leached_for_test(days, her_per_day):
 		max_load_per_year = 10000 * 365.25
@@ -128,37 +104,13 @@ def calculate_total_mass_leached_for_test(days, her_per_day):
 		her_at_50_percent = 50 * 365.25
 		her_at_95_percent = 95 * 365.25
 
-		return calculate_total_mass_leached_from_cell_on_days(
+		return nitrate._calculate_total_mass_leached_from_cell_on_days(
 			max_load_per_year,
 			her_at_5_percent,
 			her_at_50_percent,
 			her_at_95_percent,
 			days,
 			her_per_day)
-
-def calculate_total_mass_leached_from_cell_on_days(
-		max_load_per_year_kg_per_cell,
-		her_at_5_percent,
-		her_at_50_percent,
-		her_at_95_percent,
-		days,
-		her_per_day):
-	length = len(days)
-	result = np.zeros(length)
-	remaining_for_year = max_load_per_year_kg_per_cell
-	for i in range(length):
-		day = days[i]
-		her = her_per_day[i]
-		if (day.month == 10) and (day.day == 1):
-			remaining_for_year = max_load_per_year_kg_per_cell
-		fraction_leached = cumulative_fraction_leaked_per_day(her_at_5_percent,
-			her_at_50_percent,
-			her_at_95_percent,
-			her)
-		mass_leached_for_day = min(remaining_for_year, max_load_per_year_kg_per_cell * fraction_leached)
-		remaining_for_year -= mass_leached_for_day
-		result[i] = mass_leached_for_day
-	return result
 	
 def cumulative_fraction_leaked_per_year(
 		her_at_5_percent,
@@ -174,20 +126,3 @@ def cumulative_fraction_leaked_per_year(
 	c = 0.5 - (her_at_50_percent * m)
 	y = (m * x) + c
 	return y
-
-def cumulative_fraction_leaked_per_day(
-		her_at_5_percent,
-		her_at_50_percent,
-		her_at_95_percent,
-		her_per_day):
-	days_in_year = 365.25
-	her_per_year = days_in_year * her_per_day
-	x = her_per_year
-	is_below_50_percent = her_per_year < her_at_50_percent
-	upper = her_at_50_percent if is_below_50_percent else her_at_95_percent
-	lower = her_at_5_percent if is_below_50_percent else her_at_50_percent
-	# y = mx + c
-	m = 0.45 / (upper - lower)
-	c = 0.5 - (her_at_50_percent * m)
-	y = (m * x) + c
-	return y / days_in_year
