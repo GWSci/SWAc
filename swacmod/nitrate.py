eead2b5782025a7c851203400d8470684cf54d43
Chris Foley <chris@nice-code.co.uk>
2023-10-05 11:29:50 +0100

    Nitrate. 100

--- swacmod/nitrate.py
+++ swacmod/nitrate.py
@@ -12,7 +12,7 @@ def calculate_nitrate(data, output, node):
 	daily_proportion_reaching_water_table = _calculate_daily_proportion_reaching_water_table_arr(data, output, node)
 	total_mass_on_day_kg = _calculate_total_mass_on_day_kg(daily_proportion_reaching_water_table, mi_kg_per_day)
 	recharge_concentration_kg_per_m3 = _calculate_recharge_concentration_kg_per_m3(data, output, node, total_mass_on_day_kg)
-	
+
 
 def _calculate_her_mm_per_day(data, output, node):
 	return output["rainfall_ts"] - output["ae"]
@@ -138,11 +138,11 @@ def _calculate_daily_proportion_reaching_water_table_arr(data, output, node):
 def _calculate_daily_proportion_reaching_water_table(DTW, t):
 	f_t = _calculate_cumulative_proportion_reaching_water_table(DTW, t)
 	f_t_prev = _calculate_cumulative_proportion_reaching_water_table(DTW, t - 1)
-	return -(f_t - f_t_prev)
+	return f_t - f_t_prev
 
 def _calculate_cumulative_proportion_reaching_water_table(DTW, t):
 	if (t <= 0):
-		return 1
+		return 0
 
 	a = 1.38
 	μ = 1.58
@@ -151,7 +151,7 @@ def _calculate_cumulative_proportion_reaching_water_table(DTW, t):
 	numerator = math.log((1.7/0.0029) * (DTW/t), a) - μ
 	denominator = σ * math.sqrt(2)
 
-	result = 0.5 * math.erfc(- numerator / denominator)
+	result = 0.5 * (1 + math.erf(- numerator / denominator))
 	return result
 
 def _calculate_total_mass_on_day_kg(daily_proportion_reaching_water_table, mi_kg_per_day):
