20213252b1926bb44d8a0f40d2e994948beff7dc
Chris Foley <chris@nice-code.co.uk>
2023-10-05 07:20:21 +0100

    Nitrate. 71

--- test/test_nitrate_cumulative_distribution_function.py
+++ test/test_nitrate_cumulative_distribution_function.py
@@ -2,26 +2,26 @@ import math
 import unittest
 
 class Test_Nitrate_Cumulative_Distribution_Function(unittest.TestCase):
-	def test_sum_of_n_prop_i_should_equal_1(self):
+	def test_sum_of_daily_proportion_should_equal_1(self):
 		DTW = 100
 		sum = 0
 		for t in range(1, 1000000):
-			sum += n_prop_i(DTW, t)
+			sum += calculate_daily_proportion_reaching_water_table(DTW, t)
 		self.assertAlmostEqual(1, sum, places=2)
 
-	def test_maximum_of_daily_nitrate_should_appear_after_several_years(self):
+	def test_maximum_of_daily_proportion_should_appear_after_several_years(self):
 		DTW = 100
 		t = 0
 		previous_nitrate = 0
 		while True:
-			nitrate = n_prop_i(DTW, t + 1)
+			nitrate = calculate_daily_proportion_reaching_water_table(DTW, t + 1)
 			if nitrate < previous_nitrate:
 				break
 			t += 1
 			previous_nitrate = nitrate
 		self.assertEqual(6927, t)
 
-def n_prop_i(DTW, t):
+def calculate_daily_proportion_reaching_water_table(DTW, t):
 	f_t = calculate_cumulative_proportion_reaching_water_table(DTW, t)
 	f_t_prev = calculate_cumulative_proportion_reaching_water_table(DTW, t - 1)
 	return -(f_t - f_t_prev)
