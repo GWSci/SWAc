2495d38365d770ff7494cb5c0b1ebe3928e66bea
Chris Foley <chris@nice-code.co.uk>
2023-10-05 09:36:46 +0100

    Nitrate. 94

--- swacmod/nitrate.py
+++ swacmod/nitrate.py
@@ -9,6 +9,8 @@ def calculate_nitrate(data, output, node):
 	m2_arr_kg_per_day = _calculate_m2_kg_per_day(data, output, node, her_array_mm_per_day, m0_array_kg_per_day)
 	m3_kg_per_day = _calculate_m3_kg_per_day(data, output, node, her_array_mm_per_day, m0_array_kg_per_day)
 	mi_kg_per_day = _calculate_mi_kg_per_day(m1a_arr_kg_per_day, m2_arr_kg_per_day)
+	daily_proportion_reaching_water_table = _calculate_daily_proportion_reaching_water_table_arr(data, output, node)
+	total_mass_on_day_kg = _calculate_total_mass_on_day_kg(daily_proportion_reaching_water_table, mi_kg_per_day)
 
 def _calculate_her_mm_per_day(data, output, node):
 	return output["rainfall_ts"] - output["ae"]
