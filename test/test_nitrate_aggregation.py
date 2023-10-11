c8175c6ac5ffd67cd672ef8bee3c6c012291b41f
Chris Foley <chris@nice-code.co.uk>
2023-10-11 16:51:24 +0100

    Nitrate. 141

--- test/test_nitrate_aggregation.py
+++ test/test_nitrate_aggregation.py
@@ -5,7 +5,9 @@ class Test_Nitrate_Aggregation(unittest.TestCase):
 	def test_nitrate_aggregation_for_empty_data(self):
 		data = {
 			"params" : {
-				"node_areas" : {},
+				"node_areas" : {
+					0: [10.0]
+				},
 				"time_periods" : {}
 			}
 		}
@@ -13,10 +15,10 @@ class Test_Nitrate_Aggregation(unittest.TestCase):
 			"nitrate_reaching_water_table_array_tons_per_day" : np.array([]),
 			"combined_recharge" : np.array([]),
 		}
-		node = None
+		node = 0
 
 		actual = aggregate_nitrate(data, output, node)
-		expected = np.zeros(shape = (0, 0))
+		expected = np.zeros(shape = (0, 1))
 		np.testing.assert_array_equal(expected, actual)
 
 	def test_nitrate_aggregation_for_one_day(self):
@@ -46,9 +48,10 @@ def aggregate_nitrate(data, output, node):
 	nitrate_reaching_water_table_array_tons_per_day = output["nitrate_reaching_water_table_array_tons_per_day"]
 	combined_recharge_mm = output["combined_recharge"]
 	combined_recharge_m = _convert_mm_to_m(combined_recharge_mm)
+	combined_recharge_m_cubed = combined_recharge_m * node_areas[node][0]
 	shape = (len(time_periods), num_nodes)
 	result = np.zeros(shape = shape)
-	if (num_nodes > 0):
+	if (num_nodes > 0) and (len(time_periods) > 0):
 		result[0, 0] = nitrate_reaching_water_table_array_tons_per_day[0] / (combined_recharge_m[0] * node_areas[node][0])
 	return result
 
