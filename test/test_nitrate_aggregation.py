593fb831a6fa3d43f758278f1af0983c4ffc0d86
Chris Foley <chris@nice-code.co.uk>
2023-10-11 16:53:21 +0100

    Nitrate. 143

--- test/test_nitrate_aggregation.py
+++ test/test_nitrate_aggregation.py
@@ -44,14 +44,13 @@ class Test_Nitrate_Aggregation(unittest.TestCase):
 def aggregate_nitrate(data, output, node):
 	time_periods = data["params"]["time_periods"]
 	node_areas = data["params"]["node_areas"]
-	num_nodes = len(node_areas)
 	nitrate_reaching_water_table_array_tons_per_day = output["nitrate_reaching_water_table_array_tons_per_day"]
 	combined_recharge_mm = output["combined_recharge"]
 	combined_recharge_m = _convert_mm_to_m(combined_recharge_mm)
 	combined_recharge_m_cubed = combined_recharge_m * node_areas[node][0]
-	shape = (len(time_periods), num_nodes)
+	shape = (len(time_periods), len(node_areas))
 	result = np.zeros(shape = shape)
-	if (num_nodes > 0) and (len(time_periods) > 0):
+	if (len(node_areas) > 0) and (len(time_periods) > 0):
 		result[0, 0] = nitrate_reaching_water_table_array_tons_per_day[0] / combined_recharge_m_cubed[0]
 	return result
 
