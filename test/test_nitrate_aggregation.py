32d619ccc2a4428bd6684372d4350fb9f1483631
Chris Foley <chris@nice-code.co.uk>
2023-10-12 05:20:41 +0100

    Nitrate. 181

--- test/test_nitrate_aggregation.py
+++ test/test_nitrate_aggregation.py
@@ -102,17 +102,15 @@ def aggregate_nitrate(aggregation, data, output, node):
 		time_period = time_periods[time_period_index]
 		sum_of_nitrate_tons = 0.0
 		sum_of_recharge_m_cubed = 0.0
-		for i in range(time_period[0] - 1, time_period[1]):
+		for i in range(time_period[0] - 1, time_period[1] - 1):
 			day = i + 1
 			if day == time_periods[time_period_index][1]:
-				aggregation[time_period_index, node] += sum_of_nitrate_tons / sum_of_recharge_m_cubed
 				if day == max_day:
 					break
-				sum_of_nitrate_tons = 0.0
-				sum_of_recharge_m_cubed = 0.0
 				time_period_index += 1
 			sum_of_nitrate_tons += nitrate_reaching_water_table_array_tons_per_day[i]
 			sum_of_recharge_m_cubed += combined_recharge_m_cubed[i]
+		aggregation[time_period_index, node] += sum_of_nitrate_tons / sum_of_recharge_m_cubed
 
 	return aggregation
 
