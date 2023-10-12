b592492cd8b2afabd49a42697337abc7a2b86fde
Chris Foley <chris@nice-code.co.uk>
2023-10-12 07:28:58 +0100

    Nitrate. 209

--- test/test_make_nitrate_csv_output.py
+++ test/test_make_nitrate_csv_output.py
@@ -45,6 +45,24 @@ class Test_Make_Nitrate_Csv_Output(unittest.TestCase):
 		actual = make_nitrate_csv_output(nitrate_aggregation)
 		self.assertEqual(expected, actual)
 
+	def test_make_nitrate_csv_output_for_multiple_nodes_and_stress_periods(self):
+		nitrate_aggregation = np.array([
+			[ 2.0,  3.0],
+			[ 5.0,  7.0],
+			[11.0, 13.0],
+		])
+		expected = [
+			["Stress Period", "Node", "Recharge Concentration (metric tons/m³)"],
+			[1, 1,  2.0],
+			[1, 2,  3.0],
+			[2, 1,  5.0],
+			[2, 2,  7.0],
+			[3, 1, 11.0],
+			[3, 2, 13.0],
+		]
+		actual = make_nitrate_csv_output(nitrate_aggregation)
+		self.assertEqual(expected, actual)
+
 def make_nitrate_csv_output(nitrate_aggregation):
 	result = []
 	result.append(["Stress Period", "Node", "Recharge Concentration (metric tons/m³)"])
