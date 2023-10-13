1bbec884e0cc7735eadee970bd9c9f75c2be6bed
Chris Foley <chris@nice-code.co.uk>
2023-10-13 09:31:22 +0100

    Nitrate. 231

--- test/test_nitrate.py
+++ test/test_nitrate.py
@@ -52,11 +52,8 @@ class Test_Nitrate(unittest.TestCase):
 		self.assert_cumulative_fraction_leaked_per_year(10.0, 110.0, 310.0, 376.666666, 1.1)
 
 	def assert_cumulative_fraction_leaked_per_year(self, her_at_5_percent, her_at_50_percent, her_at_95_percent, her, expected):
-		testee = lambda her: nitrate._cumulative_fraction_leaked_per_year(
+		actual = nitrate._cumulative_fraction_leaked_per_year(
 				her_at_5_percent, her_at_50_percent, her_at_95_percent, her)
-
-		actual = testee(her)
-
 		self.assertAlmostEqual(expected, actual)
 
 	def test_cumulative_fraction_leaked_per_day(self):
