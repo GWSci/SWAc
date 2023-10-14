edc1f512506349ceb2c9e2f8431bd6a15d8a0ff2
Chris Foley <chris@nice-code.co.uk>
2023-10-14 08:54:55 +0100

    Nitrate performance. 22

--- test/test_make_repeated_array_offset.py
+++ test/test_make_repeated_array_offset.py
@@ -14,7 +14,22 @@ class Test_Make_Repeated_Array_Offset(unittest.TestCase):
 		actual = make_repeated_array_offset(input_array)
 		np.testing.assert_array_equal(expected, actual)
 	
+	def test_making_repeated_array_offset_from_length_2(self):
+		input_array = np.array([2, 3])
+		expected = np.array([[2, 3, 0], [0, 2, 3]])
+		actual = make_repeated_array_offset(input_array)
+		np.testing.assert_array_equal(expected, actual)
+	
 def make_repeated_array_offset(array):
 	length = len(array)
-	result = np.broadcast_to(array, shape=(length, length))
+	if length == 2:
+		padded_length = 3
+		padded_array = np.zeros(3)
+		padded_array[0:length] = array
+	else:
+		padded_array = array
+		padded_length = length
+	result = np.broadcast_to(padded_array, shape=(length, padded_length))
+	r, c = np.ogrid[:result.shape[0], :result.shape[1]]
+	result = result[r, c - r]
 	return result
