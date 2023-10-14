cb02ecab17ad45813bb40b984096fbe86610360a
Chris Foley <chris@nice-code.co.uk>
2023-10-14 09:00:07 +0100

    Nitrate performance. 28

--- test/test_make_repeated_array_offset.py
+++ test/test_make_repeated_array_offset.py
@@ -7,25 +7,31 @@ class Test_Make_Repeated_Array_Offset(unittest.TestCase):
 		expected = np.empty(shape=(0, 0))
 		actual = make_repeated_array_offset(input_array)
 		np.testing.assert_array_equal(expected, actual)
-	
+
 	def test_making_repeated_array_offset_from_length_1(self):
 		input_array = np.array([2])
 		expected = np.array([[2]])
 		actual = make_repeated_array_offset(input_array)
 		np.testing.assert_array_equal(expected, actual)
-	
+
 	def test_making_repeated_array_offset_from_length_2(self):
 		input_array = np.array([2, 3])
 		expected = np.array([[2, 3, 0], [0, 2, 3]])
 		actual = make_repeated_array_offset(input_array)
 		np.testing.assert_array_equal(expected, actual)
-	
+
 	def test_making_repeated_array_offset_from_length_3(self):
 		input_array = np.array([2, 3, 5])
 		expected = np.array([[2, 3, 5, 0, 0], [0, 2, 3, 5, 0], [0, 0, 2, 3, 5]])
 		actual = make_repeated_array_offset(input_array)
 		np.testing.assert_array_equal(expected, actual)
-	
+
+	def test_making_repeated_array_offset_from_length_4(self):
+		input_array = np.array([2, 3, 5, 7])
+		expected = np.array([[2, 3, 5, 7, 0, 0, 0], [0, 2, 3, 5, 7, 0, 0], [0, 0, 2, 3, 5, 7, 0], [0, 0, 0, 2, 3, 5, 7]])
+		actual = make_repeated_array_offset(input_array)
+		np.testing.assert_array_equal(expected, actual)
+
 def make_repeated_array_offset(array):
 	length = len(array)
 	if length == 0:
