e08a76a216106326e7ff33242880c96c04865364
Chris Foley <chris@nice-code.co.uk>
2023-10-14 08:29:36 +0100

    Nitrate performance. 20

--- test/test_make_repeated_array_offset.py
+++ test/test_make_repeated_array_offset.py
@@ -3,10 +3,17 @@ import unittest
 
 class Test_Make_Repeated_Array_Offset(unittest.TestCase):
 	def test_making_repeated_array_offset_from_empty_array(self):
-		input_array = np.empty(shape=(0, 0))
+		input_array = np.empty(shape=(0))
 		expected = np.empty(shape=(0, 0))
 		actual = make_repeated_array_offset(input_array)
 		np.testing.assert_array_equal(expected, actual)
 	
+	# def test_making_repeated_array_offset_from_length_1(self):
+	# 	input_array = np.array([2])
+	# 	expected = np.array([[2]])
+	# 	actual = make_repeated_array_offset(input_array)
+	# 	np.testing.assert_array_equal(expected, actual)
+	
 def make_repeated_array_offset(array):
-	return np.zeros_like(array)
+	length = len(array)
+	return np.zeros(shape=(length, length))
