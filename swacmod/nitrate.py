f4ba82423b94d59ab0fa8068ace576103bb19813
Chris Foley <chris@nice-code.co.uk>
2023-10-15 08:34:23 +0100

    Nitrate performance. 35

--- swacmod/nitrate.py
+++ swacmod/nitrate.py
@@ -260,7 +260,9 @@ def _calculate_mass_reaching_water_table_array_kg_per_day(data, output, node, pr
 		timer.switch_to(time_switcher, "Nitrate: _calculate_mass... > add to result")
 		result_kg[result_start:result_end] += np.array(mass_reaching_water_table_array_kg)
 	timer.switch_to(time_switcher, "Nitrate: _calculate_mass... > 2D solution")
-	x = _make_repeated_array_offset(proportion_reaching_water_table_array_per_day)
+	timer.switch_to(time_switcher, "Nitrate: _calculate_mass... > 2D solution > make repeated_array_offset")
+	x = _make_repeated_array_offset2(proportion_reaching_water_table_array_per_day, time_switcher)
+	timer.switch_to(time_switcher, "Nitrate: _calculate_mass... > 2D solution > mass_reaching_water_table_2d_array_kg")
 	mass_reaching_water_table_2d_array_kg = _convert_repeating_proportions_to_mass_reaching_water_table_2d_array_kg(x, mi_array_kg_per_day)
 	result_kg = _sum_columns(mass_reaching_water_table_2d_array_kg)
 	timer.switch_to(time_switcher, "Nitrate: _calculate_mass... > return")
@@ -277,6 +279,24 @@ def _make_repeated_array_offset(array):
 		padded_array[0:length] = array
 	result = np.broadcast_to(padded_array, shape=(length, padded_length))
 	r, c = np.ogrid[:result.shape[0], :result.shape[1]]
+	result = result[r, c - r]
+	return result
+
+def _make_repeated_array_offset2(array, time_switcher):
+	timer.switch_to(time_switcher, "Nitrate: _calculate_mass... > 2D solution > make repeated_array_offset > lengths")
+	length = len(array)
+	if length == 0:
+		padded_array = array
+		padded_length = length
+	else:
+		padded_length = length + length - 1
+		padded_array = np.zeros(padded_length)
+		padded_array[0:length] = array
+	timer.switch_to(time_switcher, "Nitrate: _calculate_mass... > 2D solution > make repeated_array_offset > broadcast")
+	result = np.broadcast_to(padded_array, shape=(length, padded_length))
+	timer.switch_to(time_switcher, "Nitrate: _calculate_mass... > 2D solution > make repeated_array_offset > r c")
+	r, c = np.ogrid[:result.shape[0], :result.shape[1]]
+	timer.switch_to(time_switcher, "Nitrate: _calculate_mass... > 2D solution > make repeated_array_offset > result")
 	result = result[r, c - r]
 	return result
 
