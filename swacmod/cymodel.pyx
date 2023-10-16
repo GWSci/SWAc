24c5cbdac762725071536f8906201d82d281cb3a
Chris Foley <chris@nice-code.co.uk>
2023-10-16 06:46:17 +0100

    Nitrate performance. 63

--- swacmod/cymodel.pyx
+++ swacmod/cymodel.pyx
@@ -1517,7 +1517,7 @@ def calculate_mass_reaching_water_table_array_kg_per_day(data, output, size_t no
 
     time_switcher = data["time_switcher"]
     timer.switch_to(time_switcher, "Nitrate: _calculate_mass... > init variables")
-    length = proportion_reaching_water_table_array_per_day.size
+    length = proportion.size
     result_kg = np.zeros(length)
     timer.switch_to(time_switcher, "Nitrate: _calculate_mass... > for day_nitrate_was_leached")
     for day_nitrate_was_leached in range(length):
@@ -1526,7 +1526,7 @@ def calculate_mass_reaching_water_table_array_kg_per_day(data, output, size_t no
         timer.switch_to(time_switcher, "Nitrate: _calculate_mass... > mass_leached_on_day_kg")
         mass_leached_on_day_kg = mi_array_kg_per_day[day_nitrate_was_leached]
         timer.switch_to(time_switcher, "Nitrate: _calculate_mass... > mass_reaching_water_table_array_kg")
-        mass_reaching_water_table_array_kg = proportion_reaching_water_table_array_per_day[:result_end] * mass_leached_on_day_kg
+        mass_reaching_water_table_array_kg = proportion[:result_end] * mass_leached_on_day_kg
         timer.switch_to(time_switcher, "Nitrate: _calculate_mass... > add to result")
         result_kg[day_nitrate_was_leached:] += mass_reaching_water_table_array_kg
 
