2f16090ff8cc5613f8ce2ca5256160c420a7c327
Chris Foley <chris@nice-code.co.uk>
2023-11-29 16:10:36 +0000

    Open utf8-bom csv files. 2

--- swacmod/csv_resource.py
+++ swacmod/csv_resource.py
@@ -4,7 +4,7 @@ import csv
 @contextmanager
 def reader_for(path):
 	try:
-		with open(path, "r") as csv_file:
+		with open(path, "r", encoding = "utf-8-sig") as csv_file:
 			reader = csv.reader(csv_file)
 			yield reader
 	finally:
--- test/test_csv_resource.py
+++ test/test_csv_resource.py
@@ -8,3 +8,10 @@ class Test_Csv_Resource(unittest.TestCase):
 			actual = [[int(cell) for cell in row] for row in testee]
 		expected = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]]
 		self.assertEqual(expected, actual)
+
+	def test_parse_utf8_bom_file(self):
+		csv_file="./test/resources/csv_file_utf8_bom/example_utf8_bom_csv_file.csv"
+		with(csv_resource.reader_for(csv_file)) as testee:
+			actual = [[int(cell) for cell in row] for row in testee]
+		expected = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]]
+		self.assertEqual(expected, actual)
