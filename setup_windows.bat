668b205cfd9c36859b4d6299ef4a66a433141db7
Chris Foley <chris@nice-code.co.uk>
2023-12-01 12:31:34 +0000

    Useful error message in setup_windows.bat when python is not present. 2

--- setup_windows.bat
+++ setup_windows.bat
@@ -1,3 +1,11 @@
+@echo off
+where python
+if %errorlevel% neq 0 (
+    echo Cannot find Python. Please install it from www.python.org and rerun this script.
+    exit /b
+)
+
+@echo on
 python -m venv env || exit /b
 
 call env/Scripts/activate.bat
