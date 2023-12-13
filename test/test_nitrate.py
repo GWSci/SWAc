12b881cc896673ef97d3a6239647f49bebc79056
Chris Foley <chris@nice-code.co.uk>
2023-12-13 09:31:05 +0000

    Mass balance includes soil moisture deficit. 11

--- test/test_nitrate.py
+++ test/test_nitrate.py
@@ -256,21 +256,7 @@ class Test_Nitrate(unittest.TestCase):
 	def test_calculate_M4_and_M4out_arrays_mm_per_day_for_zero_days(self):
 		data = None
 		output = {
-			"tawtew": np.array([]),
-		}
-		node = None
-		dSMD_array_mm_per_day = np.array([])
-		her_array_mm_per_day = np.array([])
-		actual_M4, actual_M4out = nitrate._calculate_M4_and_M4out_arrays_mm_per_day_for_zero_days(
-			data, output, node, dSMD_array_mm_per_day, her_array_mm_per_day)
-		expected_M4 = np.array([])
-		expected_M4out = np.array([])
-		np.testing.assert_array_almost_equal(expected_M4, actual_M4)
-		np.testing.assert_array_almost_equal(expected_M4out, actual_M4out)
-		
-	def test_calculate_M4_and_M4out_arrays_mm_per_day_for_zero_days(self):
-		data = None
-		output = {
+			"smd" : np.array([]),
 			"tawtew": np.array([]),
 		}
 		node = None
