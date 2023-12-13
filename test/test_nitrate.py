628b7b8ef4da19bbd46dc9d50c7c7d65a76e3be6
Chris Foley <chris@nice-code.co.uk>
2023-12-13 10:36:39 +0000

    Mass balance includes soil moisture deficit. 25

--- test/test_nitrate.py
+++ test/test_nitrate.py
@@ -262,21 +262,12 @@ class Test_Nitrate(unittest.TestCase):
 		np.testing.assert_array_almost_equal(expected, actual)
 
 	def test_calculate_M4_array_mm_per_day_for_one_day_zero_dSMD(self):
-		data = None
-		output = {
-			"smd" : np.array([1.0]),
-			"tawtew": np.array([8.0]),
-		}
-		node = None
 		dSMD_array_mm_per_day = np.array([0.0])
 		her_array_mm_per_day = np.array([2.0])
 		m0_array_kg_per_day = np.array([5.0])
-		actual_M4, actual_M4out = nitrate._calculate_M4_and_M4out_arrays_mm_per_day_for_zero_days(
-			data, output, node, dSMD_array_mm_per_day, her_array_mm_per_day, m0_array_kg_per_day)
-		expected_M4 = np.array([0.0])
-		expected_M4out = np.array([0.0])
-		np.testing.assert_array_almost_equal(expected_M4, actual_M4)
-		np.testing.assert_array_almost_equal(expected_M4out, actual_M4out)
+		actual = nitrate._calculate_M4_array_mm_per_day_for_zero_days(dSMD_array_mm_per_day, her_array_mm_per_day, m0_array_kg_per_day)
+		expected = np.array([0.0])
+		np.testing.assert_array_almost_equal(expected, actual)
 
 	def test_calculate_M4_array_mm_per_day_for_one_day_positive_dSMD(self):
 		data = None
