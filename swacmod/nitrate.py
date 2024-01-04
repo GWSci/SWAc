6422c36bca93881b02fed29994d20b8400ec4761
Chris Foley <chris@nice-code.co.uk>
2024-01-04 14:41:27 +0000

    M3 uses a proportion of HER. 7

--- swacmod/nitrate.py
+++ swacmod/nitrate.py
@@ -128,11 +128,6 @@ def _calculate_p_non(data, output, node, her_array_mm_per_day):
 	return p_non
 
 def _calculate_m2_array_kg_per_day(data, output, node, her_array_mm_per_day, m0_array_kg_per_day, p_non):
-	runoff_recharge_mm_per_day = output["runoff_recharge"]
-	macropore_att_mm_per_day = output["macropore_att"]
-	macropore_dir_mm_per_day = output["macropore_dir"]
-	macropore_mm_per_day = macropore_att_mm_per_day + macropore_dir_mm_per_day
-	p_non = _divide_arrays((runoff_recharge_mm_per_day + macropore_mm_per_day), her_array_mm_per_day)
 	m2_kg_per_day = m0_array_kg_per_day * p_non
 	return m2_kg_per_day
 
