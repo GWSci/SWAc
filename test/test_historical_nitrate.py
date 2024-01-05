f1463432c2bb5b968537a459023593516aef259a
Chris Foley <chris@nice-code.co.uk>
2024-01-05 07:46:17 +0000

    Historical nitrate. 3

--- test/test_historical_nitrate.py
+++ test/test_historical_nitrate.py
@@ -1,4 +1,5 @@
 from datetime import date
+import numpy as np
 import swacmod.nitrate as nitrate
 import unittest
 
@@ -15,4 +16,5 @@ class Test_Historical_Nitrate(unittest.TestCase):
 		node = 3
 		output = nitrate.get_historical_nitrate(data, output, node)
 		actual = output["historical_nitrate_reaching_water_table_array_tons_per_day"]
-		self.assertEqual(1, 1)
+		expected = np.zeros(2)
+		np.testing.assert_array_almost_equal(expected, actual)
