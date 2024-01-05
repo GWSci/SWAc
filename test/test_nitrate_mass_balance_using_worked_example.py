ed682b9a2f300f54b2a84833b8366a1446fad7ad
Chris Foley <chris@nice-code.co.uk>
2024-01-05 14:58:36 +0000

    Troubleshoot Nitrate Mass Balance. 5

--- test/test_nitrate_mass_balance_using_worked_example.py
+++ test/test_nitrate_mass_balance_using_worked_example.py
@@ -50,10 +50,16 @@ class Test_Nitrate_Mass_Balance_Using_Worked_Example(unittest.TestCase):
 		self.output = {
 			"rainfall_ts" : np.array([self.Precipitation]),
 			"ae" : np.array(self.AE),
+			"smd" : np.array([self.SMD]),
 		}
 		self.node = 0
 
 	def test_worked_example_HER(self):
 		expected = [self.HER_mm_per_d]
 		actual = nitrate._calculate_her_array_mm_per_day(self.data, self.output, self.node)
+		np.testing.assert_array_almost_equal(expected, actual)
+
+	def test_worked_example_dSMD(self):
+		expected = [self.dSMD_mm]
+		actual = nitrate._calculate_dSMD_array_mm_per_day(self.data, self.output, self.node)
 		np.testing.assert_array_almost_equal(expected, actual)
