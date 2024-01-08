9db5e6272d6afde110636f50bb59e1bc1eb785ce
Chris Foley <chris@nice-code.co.uk>
2024-01-08 17:25:52 +0000

    Troubleshoot Nitrate Mass Balance. 74

--- swacmod/nitrate.py
+++ swacmod/nitrate.py
@@ -35,7 +35,6 @@ def calculate_nitrate(data, output, node, logging = logging):
 
 		her_array_mm_per_day = _calculate_her_array_mm_per_day(data, output, node)
 		m0_array_kg_per_day = _calculate_m0_array_kg_per_day(data, output, node, her_array_mm_per_day)
-		pp = _calculate_pp(data, output, node, her_array_mm_per_day)
 		Psoilperc = _calculate_Psoilperc(data, output, node)
 		Pherperc = _calculate_Pherperc(data, output, node, her_array_mm_per_day)
 		dSMD_array_mm_per_day = _calculate_dSMD_array_mm_per_day(data, output, node)
