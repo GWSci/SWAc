1b892451836587584155154727de377f390782f4
Chris Foley <chris@nice-code.co.uk>
2024-01-08 17:28:39 +0000

    Troubleshoot Nitrate Mass Balance. 77

--- swacmod/nitrate.py
+++ swacmod/nitrate.py
@@ -47,6 +47,7 @@ def calculate_nitrate(data, output, node, logging = logging):
 		m2_array_kg_per_day = _calculate_m2_array_kg_per_day(m0_array_kg_per_day, p_non)
 		Pro = _calculate_Pro(her_array_mm_per_day, p_non, Pherperc, Psmd)
 		m3_array_kg_per_day = _calculate_m3_array_kg_per_day(m0_array_kg_per_day, Pro)
+		m4_array_kg_per_day = _calculate_M4_array_mm_per_day_new(M_soil_tot_kg, m1_array_kg_per_day)
 		m4_array_kg_per_day = _calculate_M4_array_mm_per_day(dSMD_array_mm_per_day, her_array_mm_per_day, m0_array_kg_per_day, Psmd)
 		m4out_array_kg_per_day = _calculate_M4out_array_mm_per_day(data, output, node, dSMD_array_mm_per_day, m4_array_kg_per_day)
 		mi_array_kg_per_day = _calculate_mi_array_kg_per_day(m1a_array_kg_per_day, m2_array_kg_per_day)
