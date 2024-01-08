b00690161df66e30dda5bf394e379030cb34d54e
Chris Foley <chris@nice-code.co.uk>
2024-01-08 08:10:48 +0000

    Troubleshoot Nitrate Mass Balance. 33

--- test/test_nitrate_mass_balance_using_worked_example.py
+++ test/test_nitrate_mass_balance_using_worked_example.py
@@ -64,6 +64,7 @@ class Test_Nitrate_Mass_Balance_Using_Worked_Example(unittest.TestCase):
 		self.Psmd_array = np.array([self.Psmd])
 		self.Pherperc_array = np.array([self.Pherperc])
 		self.dSMD_array_mm_per_day = np.array([self.dSMD_mm])
+		self.p_non_array = np.array([self.Pnon])
 
 	def test_worked_example_HER(self):
 		expected = self.her_array_mm_per_day
@@ -93,4 +94,9 @@ class Test_Nitrate_Mass_Balance_Using_Worked_Example(unittest.TestCase):
 	def test_worked_example_Pnon(self):
 		expected = [self.Pnon]
 		actual = nitrate._calculate_p_non(self.data, self.output, self.node, self.her_array_mm_per_day)
+		np.testing.assert_array_almost_equal(expected, actual)
+
+	def test_worked_example_M2(self):
+		expected = [self.M2_kg]
+		actual = nitrate._calculate_m2_array_kg_per_day(self.m0_array_kg_per_day, self.p_non_array)
 		np.testing.assert_array_almost_equal(expected, actual)
