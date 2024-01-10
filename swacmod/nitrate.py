b0cbb247403cb58efe485f3e8f2b7d4c95678d8e
MalTGraham <70013516+MalTGraham@users.noreply.github.com>
2024-01-10 09:05:23 +0000

    Add files via upload

--- swacmod/nitrate.py
+++ swacmod/nitrate.py
@@ -47,7 +47,7 @@ def calculate_nitrate(data, output, node, logging = logging):
 		m2_array_kg_per_day = _calculate_m2_array_kg_per_day(m0_array_kg_per_day, p_non)
 		Pro = _calculate_Pro(her_array_mm_per_day, p_non, Pherperc, Psmd)
 		m3_array_kg_per_day = _calculate_m3_array_kg_per_day(m0_array_kg_per_day, Pro)
-		m4_array_kg_per_day = _calculate_M4_array_mm_per_day(M_soil_tot_kg, m1_array_kg_per_day)
+		m4_array_kg_per_day = _calculate_M4_array_mm_per_day(M_soil_in_kg, m1_array_kg_per_day)
 		mi_array_kg_per_day = _calculate_mi_array_kg_per_day(m1a_array_kg_per_day, m2_array_kg_per_day)
 		total_NO3_to_receptors_kg = _calculate_total_NO3_to_receptors_kg(m1_array_kg_per_day, m2_array_kg_per_day, m3_array_kg_per_day, m4_array_kg_per_day)
 		mass_balance_error_kg = _calculate_mass_balance_error_kg(m0_array_kg_per_day, total_NO3_to_receptors_kg)
@@ -170,8 +170,8 @@ def _calculate_dSMD_array_mm_per_day(data, output, node):
 	p_smd = output["p_smd"]
 	return smd - np.maximum(0, p_smd)
 
-def _calculate_M4_array_mm_per_day(M_soil_tot_kg, m1_array_kg_per_day):
-	return M_soil_tot_kg - m1_array_kg_per_day
+def _calculate_M4_array_mm_per_day(M_soil_in_kg, m1_array_kg_per_day):
+	return M_soil_in_kg - m1_array_kg_per_day
 
 def _calculate_Psmd(her_array_mm_per_day, dSMD_array_mm_per_day):
 	Psmd = _divide_arrays(
