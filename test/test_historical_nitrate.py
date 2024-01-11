79ee2ba13db1337f59685a889c0d3ed66f565469
Chris Foley <chris@nice-code.co.uk>
2024-01-11 13:59:20 +0000

    Historical nitrate. 202

--- test/test_historical_nitrate.py
+++ test/test_historical_nitrate.py
@@ -85,11 +85,16 @@ class Test_Historical_Nitrate(unittest.TestCase):
 		blackboard = historical_nitrate.HistoricalNitrateBlackboard()
 		blackboard.truncated_historical_nitrate_dates = [date(2023, 1, 1), date(2023, 1, 2), ]
 		blackboard.historical_mi_array_kg_per_day = np.array([10.0, 20.0])
-
+		truncated_historical_nitrate_dates = [date(2023, 1, 1), date(2023, 1, 2), ]
+		historical_mi_array_kg_per_day = [10.0, 20.0]
 		actual = historical_nitrate._calculate_truncated_historical_mi_array_kg_per_day(blackboard)
 
 		expected = np.array([10.0, 20.0])
 		np.testing.assert_array_equal(expected, actual)
+		self.assert_calculate_truncated_historical_mi_array_kg_per_day(
+			expected,
+			truncated_historical_nitrate_dates,
+			historical_mi_array_kg_per_day)
 
 	def test_calculate_truncated_historical_mi_array_kg_per_day_does_not_truncate_when_dates_are_longer_than_mi(self):
 		expected = [10.0, 20.0]
