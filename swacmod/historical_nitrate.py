ec8baed633207d5a7abdbacd8c884dbdc36fd5c5
Chris Foley <chris@nice-code.co.uk>
2024-01-11 12:32:05 +0000

    Historical nitrate. 195

--- swacmod/historical_nitrate.py
+++ swacmod/historical_nitrate.py
@@ -14,6 +14,7 @@ def get_historical_nitrate(data, output, node):
 	}
 
 def _calculate_historical_nitrate(blackboard):
+	blackboard.truncated_historical_nitrate_dates = _calculate_truncated_historical_nitrate_date(blackboard)
 	return blackboard
 
 def _calculate_truncated_historical_nitrate_date(blackboard):
