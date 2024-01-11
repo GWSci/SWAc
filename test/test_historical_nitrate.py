f335896498f3af9c7cd80428648a352d81f2d65b
Chris Foley <chris@nice-code.co.uk>
2024-01-11 12:00:31 +0000

    Historical nitrate. 178

--- test/test_historical_nitrate.py
+++ test/test_historical_nitrate.py
@@ -33,3 +33,5 @@ class Test_Historical_Nitrate(unittest.TestCase):
 		input_historical_nitrate_date = [date(2023, 1, 1), date(2023, 1, 2), ]
 		input_date = [date(2023, 1, 3), date(2023, 1, 4), ]
 		blackboard = historical_nitrate.HistoricalNitrateBlackboard()
+		blackboard.date = input_date
+		blackboard.historical_nitrate_date = input_historical_nitrate_date
