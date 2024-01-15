5b7d398cea21262c276991cb0af815fcd3027018
Chris Foley <chris@nice-code.co.uk>
2024-01-15 08:42:48 +0000

    Historical nitrate. 285

--- swacmod/historical_nitrate.py
+++ swacmod/historical_nitrate.py
@@ -51,9 +51,6 @@ def get_historical_nitrate(data, output, node):
 		}
 
 def _calculate_aggregate_mi_unpacking(blackboard):
-	
-	print(f"historical_mi_array_kg_per_time_period = {blackboard.historical_mi_array_kg_per_time_period}")
-	print(f"blackboard.node = {blackboard.node}")
 	result = np.array(blackboard.historical_mi_array_kg_per_time_period[blackboard.node])
 	blackboard.historical_mi_array_kg_per_day = result
 	return blackboard
