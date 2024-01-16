9df5cbca6dd7b6c4373c29f26047b0c0e163c90d
Chris Foley <chris@nice-code.co.uk>
2024-01-16 08:45:31 +0000

    Historical nitrate. 348

--- swacmod/finalization.py
+++ swacmod/finalization.py
@@ -50,9 +50,8 @@ def fin_historical_start_date(data, name):
     new_date = str(params["historical_start_date"])
     fields = re.findall(r"^(\d{4})-(\d{2})-(\d{2})$", new_date)
     if not fields:
-        msg = ""
-    #     msg = ("start_date has to be in the format YYYY-MM-DD "
-    #            + "(e.g. 1980-12-21)")
+        msg = ("start_date has to be in the format YYYY-MM-DD "
+               + "(e.g. 1980-12-21)")
         raise u.ValidationError(msg)
     params[name] = datetime.datetime(
         int(fields[0][0]), int(fields[0][1]), int(fields[0][2])
