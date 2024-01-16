66255be904952b11f8e77669c088a00f3770963d
Chris Foley <chris@nice-code.co.uk>
2024-01-16 08:23:22 +0000

    Historical nitrate. 334

--- test/test_historical_hitrate_finalization.py
+++ test/test_historical_hitrate_finalization.py
@@ -3,19 +3,6 @@ import unittest
 import swacmod.finalization as finalization
 
 class Test_Historical_Nitrate_Finalization(unittest.TestCase):
-	def test_fin_historical_start_date_when_valid(self):
-		data = {
-			"params" : {
-				"historical_start_date" : "2024-01-16",
-			}
-		}
-		name = "historical_start_date"
-		finalization.fin_historical_start_date(data, name)
-
-		actual = data["params"]["historical_start_date"]
-		expected = datetime.datetime(2024, 1, 16)
-		self.assertEqual(expected, actual)
-
 	def test_finalize_param_converts_historical_start_date_when_valid(self):
 		data = {
 			"params" : {
