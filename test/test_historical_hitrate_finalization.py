82d352d3b11aef3fde96796808ca9bd029b8cdd3
Chris Foley <chris@nice-code.co.uk>
2024-01-17 07:27:07 +0000

    Historical nitrate. 371

--- test/test_historical_hitrate_finalization.py
+++ test/test_historical_hitrate_finalization.py
@@ -68,7 +68,11 @@ class Test_Historical_Nitrate_Finalization(unittest.TestCase):
 		finalize_params_and_series(data)
 
 		actual = data["series"]["date"]
-		expected = [datetime.datetime(2024, 1, 16), datetime.datetime(2024, 1, 17), datetime.datetime(2024, 1, 18), datetime.datetime(2024, 1, 19)]
+		expected = [
+			datetime.datetime(2024, 1, 16),
+			datetime.datetime(2024, 1, 17),
+			datetime.datetime(2024, 1, 18),
+			datetime.datetime(2024, 1, 19)]
 		self.assertEqual(expected, actual)
 
 def finalize_params_and_series(data):
