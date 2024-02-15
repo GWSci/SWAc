Fix mm to m calculation. 3

test/test_nitrate.py
@@ -580,7 +580,7 @@ class Test_Nitrate(unittest.TestCase):
 	def test_convert_mm_to_m(self):
 		np.testing.assert_array_almost_equal(
 			np.array([0.0, 1.0, 0.001]),
-			nitrate._convert_mm_to_m_B(np.array([0.0, 1000.0, 1.0])))
+			nitrate._convert_mm_to_m(np.array([0.0, 1000.0, 1.0])))
 
 	def assert_output_file_path(self, expected_filename, make_filename_function):
 		data = {