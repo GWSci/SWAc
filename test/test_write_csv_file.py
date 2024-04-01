2d99becb3f8054f67e160c3e90f8f15f96eaf7f6
Chris Foley <chris@nice-code.co.uk>
2024-04-01 15:10:06 +0100

    Refactor duplicate nitrate code. 8

--- test/test_write_csv_file.py
+++ test/test_write_csv_file.py
@@ -1,5 +1,6 @@
 import unittest
 import numpy as np
+import swacmod.time_series_data as time_series_data
 import swacmod.utils as utils
 import swacmod.nitrate as nitrate
 import tempfile
@@ -59,6 +60,42 @@ class Test_Write_Csv_File(unittest.TestCase):
 		}
 
 		filename = nitrate.write_stream_nitrate_csv(data, nitrate_aggregation)
+		self.files_to_delete.append(filename)
+		return filename
+
+	def test_writing_an_mi_file_produces_a_file_with_the_expected_contents(self):
+		filename = self.write_mi_csv_return_filename()
+		actual = slurp(filename)
+		expected = "2.1,3,5\r\n7,11,13\r\n"
+		self.assertEqual(expected, actual)
+
+	def test_reading_an_mi_filename_reproduces_input_data(self):
+		filename = self.write_mi_csv_return_filename()
+
+		base_path = self.tempdir
+		param = None
+		ext = "csv"
+		file_contents = time_series_data.load_time_series_data(base_path, param, filename, ext)
+
+		expected = np.array([
+			[2.1, 3.0, 5.0],
+			[7.0, 11.0, 13.0],
+		])
+		np.testing.assert_allclose(expected, file_contents)
+
+	def write_mi_csv_return_filename(self):
+		nitrate_mi_aggregation = np.array([
+			[2.1, 3.0, 5.0],
+			[7.0, 11.0, 13.0],
+		])
+
+		data = {
+			"params" : {
+				"run_name" : "aardvark"
+			}
+		}
+
+		filename = nitrate.write_mi_csv(data, nitrate_mi_aggregation)
 		self.files_to_delete.append(filename)
 		return filename
 
