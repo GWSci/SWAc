8e70a9035f2d0f9022fb4a521db58a2070a4695c
Chris Foley <chris@nice-code.co.uk>
2024-04-02 08:36:24 +0100

    Refactor duplicate nitrate code. 19

--- test/test_get_str_file_and_get_str_nitrate.py
+++ test/test_get_str_file_and_get_str_nitrate.py
@@ -32,3 +32,37 @@ class Test_Get_Str_File_And_Get_Str_Nitrate(unittest.TestCase):
 			np.array([[0.0, -2.0, 0.0, -2.0, 1.0, 1.0, 0.0, 2.0, 1.0, 0.0, 1.0, 1.0, 111.111, 222.222]]),
 			str.stress_period_data.get_dataframe())
 		self.assertEqual({0: [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]}, str.segment_data)
+
+	def test_get_str_file_for_3_nodes_and_2_sp(self):
+		data = {
+			"params": {
+				"node_areas" : {1: 100.0, 2: 200.0, 3: 300.0},
+				"run_name": "aardvark",
+				"time_periods": {1: [1, 2], 2: [2, 5]},
+				"num_nodes": 3,
+				"mf96_lrc": [1, 1, 3],
+				"routing_topology": {1 : [1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 2 : [1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 3 : [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]},
+				"istcb1": None,
+				"istcb2": None,
+			}
+		}
+		runoff = np.array([100.0, 200.0, 300.0, 50.0, 60.0, 70.0, 80.0])
+
+		with warnings.catch_warnings():
+			warnings.filterwarnings("ignore", category=DeprecationWarning)
+			str = m.get_str_file(data, runoff)
+
+		self.assertEqual(3, str.mxacts)
+		self.assertEqual(3, str.nss)
+		self.assertEqual(8, str.ntrib)
+		self.assertEqual(0, str.ipakcb)
+		self.assertIsNone(str.istcb2)
+		np.testing.assert_array_almost_equal(
+			np.array([[ -1.      ,  -1.      ,   0.      ,  -3.      ,   6.      , 3.      ,   0.      ,   6.      ,   3.      ,   0.      , 3.      ,   3.      , 333.333008, 666.666016]]),
+			str.stress_period_data.get_dataframe())
+		self.assertEqual(
+			{
+				0: [[2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]],
+				1: [[2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]],
+			},
+			str.segment_data)
