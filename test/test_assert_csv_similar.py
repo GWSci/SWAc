910954ff82f28862a86970f7b46df7f340db321b
Chris Foley <chris@nice-code.co.uk>
2024-05-01 11:13:04 +0100

    All tests working. 32

--- test/test_assert_csv_similar.py
+++ test/test_assert_csv_similar.py
@@ -1,3 +1,4 @@
+import io
 import csv
 import unittest
 
@@ -11,7 +12,8 @@ class Test_Assert_Csv_Similar(unittest.TestCase):
 		self.assert_failure_message("Difference in row=0, col=0. Expected: x Actual: b", self.get_assertion_result("x", "b"))
 
 	def test_csv_files_reports_the_column_number(self):
-		self.assert_failure_message("Difference in row=0, col=0. Expected: a Actual: x", self.get_assertion_result("a,b,c", "x,b,c"))
+		self.assert_failure_message("Difference in row=0, col=0. Expected: a Actual: x", self.get_assertion_result("a,b,c\n", "x,b,c\n"))
+		self.assert_failure_message("Difference in row=0, col=1. Expected: b Actual: y", self.get_assertion_result("a,b,c\n", "a,y,c\n"))
 
 	def assert_passes(self, actual_assertion_result):
 		self.assertTrue(actual_assertion_result.is_pass)
@@ -36,13 +38,14 @@ def assert_csv_equal(test_case, expected, actual):
 	expected_grid = _read_csv(expected)
 	actual_grid = _read_csv(actual)
 	if (len(expected_grid) > 0):
-		message = f"Difference in row=0, col=0. Expected: {expected_grid[0][0]} Actual: {actual_grid[0][0]}"
-		test_case.assertEqual(expected_grid, actual_grid, message)
+		for col_index in range(len(expected_grid[0])):
+			message = f"Difference in row=0, col={col_index}. Expected: {expected_grid[0][col_index]} Actual: {actual_grid[0][col_index]}"
+			test_case.assertEqual(expected_grid[0][col_index], actual_grid[0][col_index], message)
 
 def _read_csv(file_contents):
-	csv_reader = csv.reader(file_contents)
+	file = io.StringIO(file_contents)
+	csv_reader = csv.reader(file)
 	result = []
 	for row in csv_reader:
 		result.append(row)
-	print(f"result = {result}")
 	return result
