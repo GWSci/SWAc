b92a5f85f18c1814800f217e4ac602154efc2537
Chris Foley <chris@nice-code.co.uk>
2024-05-01 08:21:48 +0100

    All tests working. 22

--- test/test_assert_csv_similar.py
+++ test/test_assert_csv_similar.py
@@ -2,10 +2,20 @@ import unittest
 
 class Test_Assert_Csv_Similar(unittest.TestCase):
 	def test_empty_csv_files_are_equal(self):
-		assert_testee_passes("", "")
-	
-def assert_testee_passes(expected, actual):
-	assert_csv_equal(expected, actual)
+		self.assert_testee_passes("", "")
 
-def assert_csv_equal(expected, actual):
-	pass
+	def test_csv_files_with_different_text_are_not_equal(self):
+		self.assert_testee_fails("a", "b")
+
+	def assert_testee_passes(self, expected, actual):
+		assert_csv_equal(self, expected, actual)
+
+	def assert_testee_fails(self, expected, actual):
+		try:
+			assert_csv_equal(self, expected, actual)
+		except AssertionError as e:
+			return
+		self.fail()
+
+def assert_csv_equal(test_case, expected, actual):
+	test_case.assertEqual(expected, actual)
