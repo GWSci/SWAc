3a1adabbb3e6ecc68d1de8a44ec648c024b0ad28
Chris Foley <chris@nice-code.co.uk>
2024-05-01 08:33:35 +0100

    All tests working. 23

--- test/test_assert_csv_similar.py
+++ test/test_assert_csv_similar.py
@@ -5,7 +5,11 @@ class Test_Assert_Csv_Similar(unittest.TestCase):
 		self.assert_testee_passes("", "")
 
 	def test_csv_files_with_different_text_are_not_equal(self):
-		self.assert_testee_fails("a", "b")
+		actual = self.assert_testee_fails("a", "b")
+		self.assert_failure_message("Difference in row=0, col=0. Expected: a Actual: b", actual)
+
+	def assert_failure_message(self, expected_message, actual_assertion_result):
+		self.assertFalse(actual_assertion_result.is_pass)
 
 	def assert_testee_passes(self, expected, actual):
 		assert_csv_equal(self, expected, actual)
@@ -14,8 +18,13 @@ class Test_Assert_Csv_Similar(unittest.TestCase):
 		try:
 			assert_csv_equal(self, expected, actual)
 		except AssertionError as e:
-			return
+			return AssertionResult(False, "")
 		self.fail()
+
+class AssertionResult:
+	def __init__(self, is_pass, message):
+		self.is_pass = is_pass
+		self.message = message
 
 def assert_csv_equal(test_case, expected, actual):
 	test_case.assertEqual(expected, actual)
