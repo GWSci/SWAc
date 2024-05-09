813dbb659f4737b033d2a9fe546fbbf3bb445bde
Chris Foley <chris@nice-code.co.uk>
2024-05-09 13:58:56 +0100

    Cleaning tests. 8

--- test.sh
+++ test.sh
@@ -1,5 +1,15 @@
 discovery_root="test"
 
+for arg in "$@"
+do
+	if [[ "$arg" == "--all" ]]; then
+		discovery_root="."
+	else
+		echo "Arg not recognised: $arg"
+		exit 1
+	fi
+done
+
 source env/bin/activate
 python3.11 -c "import swacmod.compile_model"
 env TQDM_DISABLE=true coverage run -m unittest discover -s $discovery_root
