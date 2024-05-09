4004c1dafec4dd27c77139ee62ae932a67488a46
Chris Foley <chris@nice-code.co.uk>
2024-05-09 07:13:07 +0100

    Cleaning tests. 2

--- test_full_runs.sh
+++ test_full_runs.sh
@@ -3,5 +3,6 @@ python3.11 -c "import swacmod.compile_model"
 env TQDM_DISABLE=true coverage run -m unittest discover -s .
 exit_status=$?
 coverage report -m
+pip list --outdated
 deactivate
 exit $exit_status
