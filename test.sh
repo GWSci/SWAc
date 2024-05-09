a6e457bfda25b4259e99e3289a6b031c3d3c67eb
Chris Foley <chris@nice-code.co.uk>
2024-05-09 13:52:24 +0100

    Cleaning tests. 7

--- test.sh
+++ test.sh
@@ -1,6 +1,8 @@
+discovery_root="test"
+
 source env/bin/activate
 python3.11 -c "import swacmod.compile_model"
-env TQDM_DISABLE=true coverage run -m unittest discover -s test
+env TQDM_DISABLE=true coverage run -m unittest discover -s $discovery_root
 exit_status=$?
 coverage report -m
 deactivate
