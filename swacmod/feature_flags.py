51f7c12fa983731fd980101efaaaf82ac68cd5e2
Chris Foley <chris@nice-code.co.uk>
2024-05-10 11:48:56 +0100

    Cleaning up feature flags. 3

--- swacmod/feature_flags.py
+++ swacmod/feature_flags.py
@@ -1,18 +1,5 @@
-import os
-
-# Flags values when experimental performance features are enabled.
-
-_node_count_override = False
-_max_node_count_override = 4181
-
-# Set master flag from environment variable
-
-_use_experimental_perf_features = "True" == os.environ.get("SWAc_use_perf_features", "False")
-
 # Flags to query in code
 
-use_node_count_override = _node_count_override and _use_experimental_perf_features
-max_node_count_override = _max_node_count_override if use_node_count_override else 10000000
 disable_multiprocessing = True
 
 use_natproc = True
