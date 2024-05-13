a3da4f641178655a45d4d8a0feceab706b435e9d
Chris Foley <chris@nice-code.co.uk>
2024-05-13 07:11:57 +0100

    Flopy Adaptor. 7

--- swacmod/flopy_adaptor.py
+++ swacmod/flopy_adaptor.py
@@ -10,14 +10,15 @@ def mf_model(sim, path):
 def mf_gwf_disv(model):
 	return flopy.mf6.modflow.mfgwfdisv.ModflowGwfdisv(model)
 
-def mf_gwf_disu(model, nodes, njag):
+def mf_gwf_disu(model, nodes, njag, area=None):
 	return flopy.mf6.modflow.mfgwfdisu.ModflowGwfdisu(
 		model,
 		nodes=nodes,
 		ja=np.zeros((njag), dtype=int),
 		nja=njag,
 		ihc=[1],
-		iac=[1])
+		iac=[1],
+		area=area)
 
 def mf_gwf_disu_with_area(model, nodes, njag, area):
 	return flopy.mf6.modflow.mfgwfdisu.ModflowGwfdisu(
