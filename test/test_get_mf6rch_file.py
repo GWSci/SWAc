330feeb748d82e3cd0566d29ebd75fcf09ad1367
Chris Foley <chris@nice-code.co.uk>
2024-06-24 04:14:10 +0100

    Flopy Adaptor. 42

--- test/test_get_mf6rch_file.py
+++ test/test_get_mf6rch_file.py
@@ -41,3 +41,41 @@ END period  2
 
 """
 		self.assertEqual(expected, actual)
+
+	def test_write_rch_with_disu_and_no_node_mapping(self):
+		data = {
+			"params" : {
+				"run_name" : "run-bat",
+				"recharge_node_mapping" : None,
+				"time_periods" : [-1, -1], # Only the length is used as nper
+				"num_nodes" : 3,
+				"disv" : False,
+			}
+		}
+		# rchrate is 1-based, accessed as rchrate[(nodes * per) + i + 1]
+		rchrate = [-1, 5, 7, 11, 13, 17, 19]
+		rch_out = m.get_mf6rch_file(data, rchrate)
+		flopy_adaptor.write_mf_gwf_rch(rch_out)
+
+		actual = file_test_helpers.slurp_without_first_line("output_files/run-bat.rch")
+		expected = """BEGIN options
+END options
+
+BEGIN dimensions
+  MAXBOUND  3
+END dimensions
+
+BEGIN period  1
+  1       0.00500000
+  2       0.00700000
+  
+END period  1
+
+BEGIN period  2
+  1       0.01300000
+  2       0.01700000
+  
+END period  2
+
+"""
+		self.assertEqual(expected, actual)
