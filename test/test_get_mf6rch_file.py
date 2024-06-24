878c98e5f74652823b7f662dee18658c28cb2d2a
Chris Foley <chris@nice-code.co.uk>
2024-06-24 04:27:12 +0100

    Flopy Adaptor. 43

--- test/test_get_mf6rch_file.py
+++ test/test_get_mf6rch_file.py
@@ -5,21 +5,10 @@ import test.file_test_helpers as file_test_helpers
 
 class Test_Get_Mf6Rch_File(unittest.TestCase):
 	def test_write_rch_with_disv_and_no_node_mapping(self):
-		data = {
-			"params" : {
-				"run_name" : "run-aardvark",
-				"recharge_node_mapping" : None,
-				"time_periods" : [-1, -1], # Only the length is used as nper
-				"num_nodes" : 3,
-				"disv" : True,
-			}
-		}
-		# rchrate is 1-based, accessed as rchrate[(nodes * per) + i + 1]
-		rchrate = [-1, 5, 7, 11, 13, 17, 19]
-		rch_out = m.get_mf6rch_file(data, rchrate)
-		flopy_adaptor.write_mf_gwf_rch(rch_out)
-
-		actual = file_test_helpers.slurp_without_first_line("output_files/run-aardvark.rch")
+		run_name = "run-aardvark"
+		is_disv = True
+		recharge_node_mapping = None
+		filename = "output_files/run-aardvark.rch"
 		expected = """BEGIN options
 END options
 
@@ -40,24 +29,13 @@ BEGIN period  2
 END period  2
 
 """
-		self.assertEqual(expected, actual)
+		self.assert_rch_file(run_name, is_disv, recharge_node_mapping, filename, expected)
 
 	def test_write_rch_with_disu_and_no_node_mapping(self):
-		data = {
-			"params" : {
-				"run_name" : "run-bat",
-				"recharge_node_mapping" : None,
-				"time_periods" : [-1, -1], # Only the length is used as nper
-				"num_nodes" : 3,
-				"disv" : False,
-			}
-		}
-		# rchrate is 1-based, accessed as rchrate[(nodes * per) + i + 1]
-		rchrate = [-1, 5, 7, 11, 13, 17, 19]
-		rch_out = m.get_mf6rch_file(data, rchrate)
-		flopy_adaptor.write_mf_gwf_rch(rch_out)
-
-		actual = file_test_helpers.slurp_without_first_line("output_files/run-bat.rch")
+		run_name = "run-bat"
+		is_disv = False
+		recharge_node_mapping = None
+		filename = "output_files/run-bat.rch"
 		expected = """BEGIN options
 END options
 
@@ -78,4 +56,22 @@ BEGIN period  2
 END period  2
 
 """
+		self.assert_rch_file(run_name, is_disv, recharge_node_mapping, filename, expected)
+
+	def assert_rch_file(self, run_name, is_disv, recharge_node_mapping, filename, expected):
+		data = {
+			"params" : {
+				"run_name" : run_name,
+				"recharge_node_mapping" : recharge_node_mapping,
+				"time_periods" : [-1, -1], # Only the length is used as nper
+				"num_nodes" : 3,
+				"disv" : is_disv,
+			}
+		}
+		# rchrate is 1-based, accessed as rchrate[(nodes * per) + i + 1]
+		rchrate = [-1, 5, 7, 11, 13, 17, 19]
+		rch_out = m.get_mf6rch_file(data, rchrate)
+		flopy_adaptor.write_mf_gwf_rch(rch_out)
+
+		actual = file_test_helpers.slurp_without_first_line(filename)
 		self.assertEqual(expected, actual)
