fe86a3a03c6d19e4c25ddf80f5bd32884691213e
Chris Foley <chris@nice-code.co.uk>
2024-07-01 12:33:23 +0100

    SW Attenuation Process. 23

--- test/test_get_flows.py
+++ test/test_get_flows.py
@@ -49,14 +49,15 @@ class Test_Get_Flows(unittest.TestCase):
 			2 : make_routing_parameters(downstr = 0, str_flag = 1),
 		}
 		self.assert_get_flows(sorted_by_ca, [2, 0], [0, 0])
-		# TODO I think the result should be [2, 5]
+		# TODO I think the result should be [2, 3]
 
-	def test_get_flows_for_a_model_size_2_and_2_stream_cells_and_downstream_connections_is_empty(self):
+	def test_get_flows_for_node_count_2_str_count_2_connections_on(self):
 		sorted_by_ca = {
 			1 : make_routing_parameters(downstr = 2, str_flag = 1),
 			2 : make_routing_parameters(downstr = 1, str_flag = 1),
 		}
 		self.assert_get_flows(sorted_by_ca, [2, 0], [0, 0])
+		# TODO I think the result should be [2, 5]
 
 	def test_get_flows_for_a_model_size_3_and_3_stream_cells_and_downstream_connections_accumulates_flow(self):
 		sorted_by_ca = {
