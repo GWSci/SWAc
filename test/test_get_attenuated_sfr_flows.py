ecce4f0fd195a89e6c6832cfcc24584370710510
Chris Foley <chris@nice-code.co.uk>
2024-07-03 13:29:07 +0100

    SW Attenuation Process. 32

---
+++ test/test_get_attenuated_sfr_flows.py
@@ -0,0 +1,48 @@
+import unittest
+import numpy as np
+
+class Test_Get_Attenuated_Sfr_Flows(unittest.TestCase):
+	def test_get_flows_for_an_empty_model_is_empty(self):
+		self.assert_get_flows({}, [], [])
+
+	def test_get_flows_for_node_count_1_str_count_0(self):
+		sorted_by_ca = {1 : make_routing_parameters(downstr = 0)}
+		self.assert_get_flows(sorted_by_ca, [], [])
+
+	def assert_get_flows(self, sorted_by_ca, expected_A, expected_B):
+		actual_A, actual_B = get_flows_adaptor(sorted_by_ca)
+		np.testing.assert_array_almost_equal(expected_A, actual_A)
+		np.testing.assert_array_almost_equal(expected_B, actual_B)
+
+def make_routing_parameters(
+	downstr = -1, # swac node downstream of this one
+	str_flag = 0, # 1 = stream cell, 0 = *not* stream cell
+	node_mf = -1, # modflow node number of this node 
+	RCHLEN = -1, # the modflow SFR variable RCHLEN
+	ca = -1, # the upstream contributing area 
+	STRTOP = -1, # the modflow SFR variable STRTOP
+	STRTHICK = -1, # the modflow SFR variable STRTHICK
+	STRHC1 = -1, # the modflow SFR variable STRHC1
+	DEPTH2 = -1, # the modflow SFR variable DEPTH2
+	WIDTH2 = -1, # the modflow SFR variable WIDTH2
+):
+	return [downstr, str_flag, node_mf, RCHLEN, ca, STRTOP, STRTHICK, STRHC1, DEPTH2, WIDTH2]
+
+def get_flows_adaptor(sorted_by_ca):
+	swac_seg_dic = {}
+	stream_index = 1
+	for node_number, params in sorted_by_ca.items():
+		if (params[1] == 1):
+			swac_seg_dic[node_number] = stream_index
+			stream_index += 1
+
+	nodes = len(sorted_by_ca)
+	nss = len(list(filter(lambda x : x[1] == 1, sorted_by_ca.values())))
+	long_list_for_source = [pow(2, x) for x in range(nodes)]
+	source = [-1000, -2000, -3000] + long_list_for_source[:nodes]
+	index_offset = 3
+	actual_A, actual_B = get_attenuated_sfr_flows(sorted_by_ca, swac_seg_dic, nodes, nss, source, index_offset)
+	return actual_A, actual_B
+
+def get_attenuated_sfr_flows(sorted_by_ca, swac_seg_dic, nodes, nss, source, index_offset, explain = False):
+	return [], []
