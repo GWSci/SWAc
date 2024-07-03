c312fc8f298e84836351063add5de53837e7a639
Chris Foley <chris@nice-code.co.uk>
2024-07-03 13:45:38 +0100

    SW Attenuation Process. 38

--- test/test_get_attenuated_sfr_flows.py
+++ test/test_get_attenuated_sfr_flows.py
@@ -5,16 +5,16 @@ class Test_Get_Attenuated_Sfr_Flows(unittest.TestCase):
 	def test_get_flows_for_an_empty_model_is_empty(self):
 		sfr_store_init = []
 		release_proportion = []
-		self.assert_get_flows({}, sfr_store_init, release_proportion, [], [])
+		self.assert_get_flows({}, sfr_store_init, release_proportion, [], [], [])
 
 	def test_get_flows_for_node_count_1_str_count_0(self):
 		sorted_by_ca = {1 : make_routing_parameters(downstr = 0)}
 		sfr_store_init = []
 		release_proportion = []
-		self.assert_get_flows(sorted_by_ca, sfr_store_init, release_proportion, [], [])
+		self.assert_get_flows(sorted_by_ca, sfr_store_init, release_proportion, [], [], [])
 
-	def assert_get_flows(self, sorted_by_ca, sfr_store_init, release_proportion, expected_A, expected_B):
-		actual_A, actual_B = get_flows_adaptor(sorted_by_ca, sfr_store_init, release_proportion)
+	def assert_get_flows(self, sorted_by_ca, sfr_store_init, release_proportion, expected_A, expected_B, expected_sfr_store_total):
+		actual_A, actual_B, actual_sfr_total = get_flows_adaptor(sorted_by_ca, sfr_store_init, release_proportion)
 		np.testing.assert_array_almost_equal(expected_A, actual_A)
 		np.testing.assert_array_almost_equal(expected_B, actual_B)
 
@@ -25,13 +25,13 @@ class Test_Get_Attenuated_Sfr_Flows(unittest.TestCase):
 		}
 		sfr_store_init = []
 		release_proportion = []
-		self.assert_get_flows(sorted_by_ca, sfr_store_init, release_proportion, [], [])
+		self.assert_get_flows(sorted_by_ca, sfr_store_init, release_proportion, [], [], [])
 
 	def test_get_flows_for_a_node_count_1_str_count_1(self):
 		sorted_by_ca = {1 : make_routing_parameters(downstr = 0, str_flag = 1)}
 		sfr_store_init = [0]
 		release_proportion = [1]
-		self.assert_get_flows(sorted_by_ca, sfr_store_init, release_proportion, [0], [0])
+		self.assert_get_flows(sorted_by_ca, sfr_store_init, release_proportion, [0], [0], [0])
 
 def make_routing_parameters(
 	downstr = -1, # swac node downstream of this one
@@ -60,10 +60,10 @@ def get_flows_adaptor(sorted_by_ca, sfr_store_init, release_proportion):
 	long_list_for_source = [pow(2, x) for x in range(nodes)]
 	source = [-1000, -2000, -3000] + long_list_for_source[:nodes]
 	index_offset = 3
-	actual_A, actual_B = get_attenuated_sfr_flows(sorted_by_ca, swac_seg_dic, nodes, nss, source, index_offset, sfr_store_init, release_proportion)
-	return actual_A, actual_B
+	actual_A, actual_B, sfr_store = get_attenuated_sfr_flows(sorted_by_ca, swac_seg_dic, nodes, nss, source, index_offset, sfr_store_init, release_proportion)
+	return actual_A, actual_B, sfr_store
 
 def get_attenuated_sfr_flows(sorted_by_ca, swac_seg_dic, nodes, nss, source, index_offset, sfr_store_init, release_proportion):
 	runoff_result = np.zeros(nss)
 	flows_result = np.zeros(nss)
-	return runoff_result, flows_result
+	return runoff_result, flows_result, []
