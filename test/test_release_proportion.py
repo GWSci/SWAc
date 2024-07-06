426025e4b1c3c8a4de57dd6f09c3d415bddcf18f
Chris Foley <chris@nice-code.co.uk>
2024-07-06 16:37:03 +0100

    SW Attenuation Process. 73

--- test/test_release_proportion.py
+++ test/test_release_proportion.py
@@ -5,21 +5,21 @@ class Test_Release_Proportion(unittest.TestCase):
 	def test_release_proportion_with_no_zones(self):
 		data = {
 			"params": {
-				"sfr_flow_zones" : {}
-			},
-			"sfr_flow_monthly_proportions" : {
-				1: [],
-				2: [],
-				3: [],
-				4: [],
-				5: [],
-				6: [],
-				7: [],
-				8: [],
-				9: [],
-				10: [],
-				11: [],
-				12: [],
+				"sfr_flow_zones" : {},
+				"sfr_flow_monthly_proportions" : {
+					1: [],
+					2: [],
+					3: [],
+					4: [],
+					5: [],
+					6: [],
+					7: [],
+					8: [],
+					9: [],
+					10: [],
+					11: [],
+					12: [],
+				},
 			},
 		}
 		stream_ca_order = []
@@ -33,28 +33,32 @@ class Test_Release_Proportion(unittest.TestCase):
 			"params": {
 				"sfr_flow_zones" : {
 					1: [1]
-				}
-			},
-			"sfr_flow_monthly_proportions" : {
-				1: [0.01],
-				2: [0.02],
-				3: [0.03],
-				4: [0.04],
-				5: [0.05],
-				6: [0.06],
-				7: [0.07],
-				8: [0.08],
-				9: [0.09],
-				10: [0.1],
-				11: [0.11],
-				12: [0.12],
+				},
+				"sfr_flow_monthly_proportions" : {
+					1: [0.01],
+					2: [0.02],
+					3: [0.03],
+					4: [0.04],
+					5: [0.05],
+					6: [0.06],
+					7: [0.07],
+					8: [0.08],
+					9: [0.09],
+					10: [0.1],
+					11: [0.11],
+					12: [0.12],
+				},
 			},
 		}
 		stream_ca_order = [(0, 0, 0)]
 		self.assertEqual([0.01], extract_release_proportion(data, stream_ca_order, [1, 2]))
+		self.assertEqual([0.02], extract_release_proportion(data, stream_ca_order, [32, 2]))
 
 def extract_release_proportion(data, stream_ca_order, time_period):
 	result = []
+	month_key = 1
+	if (time_period[0] == 32):
+		month_key = 2
 	for node_index, stream_index, downstream_stream_index in stream_ca_order:
-		result.append(0.01)
+		result.append(data["params"]["sfr_flow_monthly_proportions"][month_key][0])
 	return result
