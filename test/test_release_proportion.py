3c484171bb67052a4c2413b4cb93cfc26c4a785c
Chris Foley <chris@nice-code.co.uk>
2024-07-06 16:27:41 +0100

    SW Attenuation Process. 71

--- test/test_release_proportion.py
+++ test/test_release_proportion.py
@@ -22,12 +22,42 @@ class Test_Release_Proportion(unittest.TestCase):
 				12: [],
 			},
 		}
-		date = datetime.datetime(2024, 1, 1)
 		stream_ca_order = []
 		time_period = [1, 2]
 		actual = extract_release_proportion(data, stream_ca_order, time_period)
 		expected = []
 		self.assertEqual(expected, actual)
 
+	def test_release_proportion_with_one_zone_and_one_node(self):
+		data = {
+			"params": {
+				"sfr_flow_zones" : {
+					1: [1]
+				}
+			},
+			"sfr_flow_monthly_proportions" : {
+				1: [0.01],
+				2: [0.02],
+				3: [0.03],
+				4: [0.04],
+				5: [0.05],
+				6: [0.06],
+				7: [0.07],
+				8: [0.08],
+				9: [0.09],
+				10: [0.1],
+				11: [0.11],
+				12: [0.12],
+			},
+		}
+		stream_ca_order = [(0, 0, 0)]
+		time_period = [1, 2]
+		actual = extract_release_proportion(data, stream_ca_order, time_period)
+		expected = [0.01]
+		self.assertEqual(expected, actual)
+
 def extract_release_proportion(data, stream_ca_order, time_period):
-	return []
+	result = []
+	for node_index, stream_index, downstream_stream_index in stream_ca_order:
+		result.append(0.01)
+	return result
