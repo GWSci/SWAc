9e0c675c96300e20f7bac4e1ed19309c5c41e584
Chris Foley <chris@nice-code.co.uk>
2024-07-24 23:00:02 +0100

    h5py adaptor. 3

--- swacmod/h5py_adaptor.py
+++ swacmod/h5py_adaptor.py
@@ -1,5 +1,14 @@
 import h5py
+import numpy as np
 
 def write_h5py(file_path, dataset_name, data):
     with h5py.File(file_path, "w") as outfile:
         outfile.create_dataset(dataset_name, data=data, compression="gzip")
+
+def read_h5py(path):
+	result = {}
+	with h5py.File(path, "r") as in_file:
+		for dataset_name in in_file.keys():
+			dataset = in_file[dataset_name]
+			result[dataset_name] = np.array(dataset)
+	return result
