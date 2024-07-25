b934d0894446715f38a9e2eb5411441f5e6bbad9
Chris Foley <chris@nice-code.co.uk>
2024-07-25 21:14:45 +0100

    Adaptor for networkx. 56

--- test/test_do_swrecharge_mask_original.py
+++ test/test_do_swrecharge_mask_original.py
@@ -4,21 +4,25 @@ import numpy as np
 
 class Test_Do_Swrecharge_Mask_Original(unittest.TestCase):
 	def test_Do_Swrecharge_Mask_Original_for_empty_model_leaves_runoff_and_recharge_unchanged(self):
-		input_data = {
-			"params": {
-				'num_nodes': 0,
-				'ror_prop': np.array([[]]),
-				'ror_limit': np.array([[]]),
-				'swrecharge_zone_mapping': {1: 1},
-				'routing_topology': {}
-			},
-			"series": {
-				'date': [],
-				'months': []
-			},
-		}
-		input_runoff = "some runoff"
-		input_recharge = "some recharge"
+		input_data, input_runoff, input_recharge = make_model(0)
 		runoff, recharge = m.do_swrecharge_mask_original(input_data, input_runoff, input_recharge)
 		self.assertEqual("some runoff", runoff)
 		self.assertEqual("some recharge", recharge)
+
+def make_model(node_count):
+	input_data = {
+		"params": {
+			'num_nodes': node_count,
+			'ror_prop': np.array([[]]),
+			'ror_limit': np.array([[]]),
+			'swrecharge_zone_mapping': {1: 1},
+			'routing_topology': {}
+		},
+		"series": {
+			'date': [],
+			'months': []
+		},
+	}
+	input_runoff = "some runoff"
+	input_recharge = "some recharge"
+	return input_data, input_runoff, input_recharge
