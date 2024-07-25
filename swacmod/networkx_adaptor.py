30abc8aa8594f7e56453d32ac8bd41ffe37dbcd0
Chris Foley <chris@nice-code.co.uk>
2024-07-25 16:24:22 +0100

    Adaptor for networkx. 49

--- swacmod/networkx_adaptor.py
+++ swacmod/networkx_adaptor.py
@@ -2,19 +2,15 @@ import networkx as nx
 
 def make_directed_graph(nodes, edges):
 	G = nx.DiGraph()
-	for node, attr in nodes.items():
-		G.add_node(node, **attr)
-
-	for e in edges:
-		G.add_edge(e[0], e[1])
-	return G
+	return _populate_graph(G, nodes, edges)
 
 def make_undirected_graph(nodes, edges):
 	G = nx.Graph()
+	return _populate_graph(G, nodes, edges)
 
+def _populate_graph(G, nodes, edges):
 	for node, attr in nodes.items():
 		G.add_node(node, **attr)
-
 	for e in edges:
 		G.add_edge(e[0], e[1])
 	return G
