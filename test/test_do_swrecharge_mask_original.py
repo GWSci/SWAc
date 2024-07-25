46c9a5181865c170aef2a390068bc0a74c150043
Chris Foley <chris@nice-code.co.uk>
2024-07-25 21:12:26 +0100

    Adaptor for networkx. 55

---
+++ test/test_do_swrecharge_mask_original.py
@@ -0,0 +1,24 @@
+import unittest
+import swacmod.model as m
+import numpy as np
+
+class Test_Do_Swrecharge_Mask_Original(unittest.TestCase):
+	def test_Do_Swrecharge_Mask_Original_for_empty_model_leaves_runoff_and_recharge_unchanged(self):
+		input_data = {
+			"params": {
+				'num_nodes': 0,
+				'ror_prop': np.array([[]]),
+				'ror_limit': np.array([[]]),
+				'swrecharge_zone_mapping': {1: 1},
+				'routing_topology': {}
+			},
+			"series": {
+				'date': [],
+				'months': []
+			},
+		}
+		input_runoff = "some runoff"
+		input_recharge = "some recharge"
+		runoff, recharge = m.do_swrecharge_mask_original(input_data, input_runoff, input_recharge)
+		self.assertEqual("some runoff", runoff)
+		self.assertEqual("some recharge", recharge)
