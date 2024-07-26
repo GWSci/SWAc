15055921c0e253937ede071e8aae4c309af04b68
Chris Foley <chris@nice-code.co.uk>
2024-07-26 11:49:55 +0100

    Remove constraint on Python 11. 5

--- test_full_runs.bat
+++
@@ -1,4 +0,0 @@
-call env/Scripts/activate.bat
-python -c "import swacmod.compile_model"
-python -m unittest discover -s .
-call env\Scripts\deactivate.bat
--- test_full_runs.sh
+++
@@ -1,8 +0,0 @@
-source env/bin/activate
-python3.11 -c "import swacmod.compile_model"
-env TQDM_DISABLE=true coverage run -m unittest discover -s .
-exit_status=$?
-coverage report -m
-pip list --outdated
-deactivate
-exit $exit_status
