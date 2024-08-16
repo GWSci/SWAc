07be62574b44b809fe427e3968290135d94a29b4
Chris Foley <chris@nice-code.co.uk>
2024-08-16 11:34:29 +0100

    Compile Cython in build script. 16

--- compile_model.py
+++ compile_model.py
@@ -8,26 +8,19 @@ import logging
 def compile_model():
     """Compile Cython model."""
     is_compile_required = calculate_is_compile_required()
-    code_directory = calculate_code_directory()
     if is_compile_required:
-        arch = struct.calcsize('P') * 8
-        print('cymodel.pyx modified, recompiling for %d-bit' % arch)
-        proc = sp.Popen([sys.executable, 'setup.py', 'build_ext', '--inplace'],
-                        cwd=code_directory,
-                        stdout=sp.PIPE,
-                        stderr=sp.PIPE)
-        proc.wait()
+        proc = compile_model()
         if_errors_report_and_exit(proc)
-
-def calculate_code_directory():
-    current_directory = os.path.dirname(os.path.abspath(__file__))
-    return os.path.join(current_directory, 'swacmod')
 
 def calculate_is_compile_required():
     code_directory = calculate_code_directory()
     mod_c = get_modified_time(os.path.join(code_directory, 'cymodel.c'))
     mod_pyx = get_modified_time(os.path.join(code_directory, 'cymodel.pyx'))
     return mod_pyx >= mod_c
+
+def calculate_code_directory():
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    return os.path.join(current_directory, 'swacmod')
 
 def get_modified_time(path):
     """Get the datetime a file was modified."""
@@ -40,6 +33,17 @@ def get_modified_time(path):
                         path)
         mod = datetime.datetime(1901, 1, 1, 0, 0, 0)
     return mod
+
+def compile_model():
+    arch = struct.calcsize('P') * 8
+    code_directory = calculate_code_directory()
+    print('cymodel.pyx modified, recompiling for %d-bit' % arch)
+    proc = sp.Popen([sys.executable, 'setup.py', 'build_ext', '--inplace'],
+                    cwd=code_directory,
+                    stdout=sp.PIPE,
+                    stderr=sp.PIPE)
+    proc.wait()
+    return proc
 
 def if_errors_report_and_exit(proc):
     if proc.returncode != 0:
