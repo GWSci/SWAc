b80407bebdac781d707eaf12f3660902cb08216d
Chris Foley <chris@nice-code.co.uk>
2024-09-10 07:13:15 +0100

    Compile Cython in build script. 20

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 source env/bin/activate
 python3 "compile_model.py"
-pyinstaller --add-data ./swacmod/specs.yml:./swacmod/ --hidden-import swacmod.snow_melt --hidden-import swacmod.networkx_adaptor swacmod_run.py
+pyinstaller --clean --noconfirm --add-data ./swacmod/specs.yml:./swacmod/ --hidden-import swacmod.snow_melt --hidden-import swacmod.networkx_adaptor --onefile swacmod_run.py
 deactivate
