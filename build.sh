4964c8f627fe3c1b9b031138b1dea1cf8ea8830c
Chris Foley <chris@nice-code.co.uk>
2024-09-11 10:46:09 +0100

    User documentation. 4

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+rm -rf build/
+rm -rf dist/
 source env/bin/activate
 python3 "compile_model.py"
 pyinstaller --clean --noconfirm --add-data ./swacmod/specs.yml:./swacmod/ --hidden-import swacmod.snow_melt --hidden-import swacmod.networkx_adaptor --onefile swacmod_run.py
