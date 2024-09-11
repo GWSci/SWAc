4457f24852fa97258f81a2778c654f060120f5f1
Chris Foley <chris@nice-code.co.uk>
2024-09-11 11:10:44 +0100

    User documentation. 6

--- build.bat
+++ build.bat
@@ -1,3 +1,6 @@
+rmdir build /s /q
+rmdir dist /s /q
+
 call env/Scripts/activate.bat
 python "compile_model.py"
 pyinstaller --clean --noconfirm --add-data .\swacmod\specs.yml:.\swacmod\ --hidden-import swacmod.snow_melt --hidden-import swacmod.networkx_adaptor --onefile swacmod_run.py
