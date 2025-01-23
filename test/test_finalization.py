41e195ece2aa126807f58cb10778b43a4eceabf0
Chris Foley <chris@nice-code.co.uk>
2025-01-23 14:57:39 +0000

    #117 Function get_recharge_path not defined. 15

--- test/test_finalization.py
+++ test/test_finalization.py
@@ -8,6 +8,9 @@ class Test_Finalization(unittest.TestCase):
     def test_fin_run_name_does_not_convert_upper_case_letters(self):
         self.assertEqual("AARDVARK", fin_run_name_adaptor("AARDVARK"))
 
+    def test_fin_run_name_does_not_convert_digits(self):
+        self.assertEqual("1234567890", fin_run_name_adaptor("1234567890"))
+
 def fin_run_name_adaptor(input_run_name):
     data = {"params": {"run_name": input_run_name}}
     finalization.fin_run_name(data, "run_name")
