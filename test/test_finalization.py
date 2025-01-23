6d65ade3fe9535be1b212dd056fd383e6df04a02
Chris Foley <chris@nice-code.co.uk>
2025-01-23 15:00:44 +0000

    #117 Function get_recharge_path not defined. 19

--- test/test_finalization.py
+++ test/test_finalization.py
@@ -20,6 +20,9 @@ class Test_Finalization(unittest.TestCase):
     def test_fin_run_name_converts_spaces_to_underscores(self):
         self.assertEqual("__", fin_run_name_adaptor("  "))
 
+    def test_fin_run_name_converts_non_strings_into_a_string(self):
+        self.assertEqual("123", fin_run_name_adaptor(123))
+
 def fin_run_name_adaptor(input_run_name):
     data = {"params": {"run_name": input_run_name}}
     finalization.fin_run_name(data, "run_name")
