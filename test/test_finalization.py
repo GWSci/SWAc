79413b5a432a77e8dd225f832f14fbbb828a5fc7
Chris Foley <chris@nice-code.co.uk>
2025-01-23 14:56:57 +0000

    #117 Function get_recharge_path not defined. 14

--- test/test_finalization.py
+++ test/test_finalization.py
@@ -2,8 +2,11 @@ import unittest
 import swacmod.finalization as finalization
 
 class Test_Finalization(unittest.TestCase):
-    def test_fin_run_name_when_params_has_ordinary_string_for_run_name(self):
+    def test_fin_run_name_does_not_convert_lower_case_letters(self):
         self.assertEqual("aardvark", fin_run_name_adaptor("aardvark"))
+
+    def test_fin_run_name_does_not_convert_upper_case_letters(self):
+        self.assertEqual("AARDVARK", fin_run_name_adaptor("AARDVARK"))
 
 def fin_run_name_adaptor(input_run_name):
     data = {"params": {"run_name": input_run_name}}
