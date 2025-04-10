dcdf3da363d150a609491c5f14e838d7dd36a270
Charalampos Ntigkakis <99178328+hntig@users.noreply.github.com>
2025-04-10 12:12:30 +0100

    #128: refactored tests. 2

--- test/test_fin_pe_ts.py
+++ test/test_fin_pe_ts.py
@@ -6,50 +6,39 @@ import shutil
 from swacmod.input_output import load_and_validate
 from test.change_input_file import change_input_file
 
-specs_file = u.CONSTANTS['SPECS_FILE']
-
 class Test_pe_ts_Finalisation(unittest.TestCase):
     def test_canopy_and_fao_are_disabled(self):
         with tempfile.TemporaryDirectory() as temp_dir:
-            shutil.copytree(u.CONSTANTS['TEST_INPUT_DIR'], temp_dir, dirs_exist_ok=True)
-            test_input_file = os.path.join(temp_dir, 'input.yml')
             pairs = [['fao_process', 'disabled'], ['canopy_process', 'disabled']]
-            for p in pairs: change_input_file(test_input_file, p[0], p[1])
-            try:
-                data = load_and_validate(specs_file=specs_file,
-                                        input_file=test_input_file,
-                                        input_dir=temp_dir)
-                actual = 0
-            except:
-                actual = 1
+            test_input_file = create_and_change_test_input_file(temp_dir, pairs)
+            actual = load_test_input_file(temp_dir, test_input_file)
         self.assertEqual(0, actual)
-    
+
     def test_canopy_is_disabled_and_fao_is_enabled(self):
         with tempfile.TemporaryDirectory() as temp_dir:
-            shutil.copytree(u.CONSTANTS['TEST_INPUT_DIR'], temp_dir, dirs_exist_ok=True)
-            test_input_file = os.path.join(temp_dir, 'input.yml')
             pairs = [['fao_process', 'enabled'], ['canopy_process', 'disabled']]
-            for p in pairs: change_input_file(test_input_file, p[0], p[1])
-            try:
-                data = load_and_validate(specs_file=specs_file,
-                                        input_file=test_input_file,
-                                        input_dir=temp_dir)
-                actual = 0
-            except:
-                actual = 1
+            test_input_file = create_and_change_test_input_file(temp_dir, pairs)
+            actual = load_test_input_file(temp_dir, test_input_file)
         self.assertEqual(0, actual)
     
     def test_canopy_is_enabled_and_fao_is_disabled(self):
         with tempfile.TemporaryDirectory() as temp_dir:
-            shutil.copytree(u.CONSTANTS['TEST_INPUT_DIR'], temp_dir, dirs_exist_ok=True)
-            test_input_file = os.path.join(temp_dir, 'input.yml')
             pairs = [['fao_process', 'disabled'], ['canopy_process', 'enabled']]
-            for p in pairs: change_input_file(test_input_file, p[0], p[1])
-            try:
-                data = load_and_validate(specs_file=specs_file,
-                                        input_file=test_input_file,
-                                        input_dir=temp_dir)
-                actual = 0
-            except:
-                actual = 1
+            test_input_file = create_and_change_test_input_file(temp_dir, pairs)
+            actual = load_test_input_file(temp_dir, test_input_file)
         self.assertEqual(0, actual)
+
+def create_and_change_test_input_file(temp_dir, pairs):
+    test_input_file = os.path.join(temp_dir, 'input.yml')
+    shutil.copytree(u.CONSTANTS['TEST_INPUT_DIR'], temp_dir, dirs_exist_ok=True)
+    for p in pairs: change_input_file(test_input_file, p[0], p[1])
+    return test_input_file
+
+def load_test_input_file(temp_dir, test_input_file):
+    try:
+        data = load_and_validate(specs_file=u.CONSTANTS['SPECS_FILE'],
+                                input_file=test_input_file,
+                                input_dir=temp_dir)
+        return 0
+    except:
+        return 1
