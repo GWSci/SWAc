db5ff46ce6164ec1af01f568bf1e9f45917bffc4
Chris Foley <chris@nice-code.co.uk>
2025-04-21 06:47:31 +0100

    Isssue #120. Translate platform-specific scripts to Python. 6

--- build.py
+++ build.py
@@ -8,4 +8,21 @@ if (os.path.exists("dist/")):
     shutil.rmtree("dist/")
 
 python_binary = "env/bin/python3"
+
 subprocess.run([python_binary, "compile_model.py"])
+
+subprocess.run([
+    python_binary,
+    "-m",
+    "PyInstaller",
+    "--clean",
+    "--noconfirm",
+    "--add-data",
+    "./swacmod/specs.yml:./swacmod/",
+    "--hidden-import",
+    "swacmod.snow_melt",
+    "--hidden-import",
+    "swacmod.networkx_adaptor",
+    "--onefile",
+    "swacmod_run.py",
+])
