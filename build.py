4eea899bc146ea8d0fc8e7f73df49c8cbf6a861b
Chris Foley <chris@nice-code.co.uk>
2025-04-21 06:36:04 +0100

    Isssue #120. Translate platform-specific scripts to Python. 5

--- build.py
+++ build.py
@@ -1,7 +1,11 @@
 import shutil
 import os.path
+import subprocess
 
 if (os.path.exists("build/")):
     shutil.rmtree("build/")
 if (os.path.exists("dist/")):
     shutil.rmtree("dist/")
+
+python_binary = "env/bin/python3"
+subprocess.run([python_binary, "compile_model.py"])
