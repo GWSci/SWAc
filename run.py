869f5c1337e8640956f10968906bae9c85b1da25
Chris Foley <chris@nice-code.co.uk>
2025-04-21 06:21:50 +0100

    Isssue #120. Translate platform-specific scripts to Python. 2

--- run.py
+++ run.py
@@ -1,1 +1,9 @@
-print("hello, World!")
+import subprocess
+import sys
+
+python_binary = "env/bin/python3"
+
+args = sys.argv[1:]
+
+subprocess.run([python_binary, "compile_model.py"])
+subprocess.run([python_binary, "swacmod_run.py"] + args)
