45b9e2de3b643b127898a3c925edcb29949ef52b
Chris Foley <chris@nice-code.co.uk>
2025-04-21 07:15:47 +0100

    Isssue #120. Translate platform-specific scripts to Python. 11

--- test.py
+++ test.py
@@ -31,9 +31,14 @@ for arg in args:
 subprocess.run([python_binary, "compile_model.py"])
 
 if use_coverage:
-    pass
-    # env TQDM_DISABLE=true coverage run -m unittest discover --durations 10 -s $discovery_root
+    subprocess.run(
+        [coverage_binary, "run", "-m", "unittest", "discover", "--durations", "10", "-s", discovery_root],
+        env={"TQDM_DISABLE": "true"})
 else:
     subprocess.run(
         [python_binary, "-m", "unittest", "discover", "--durations", "10", "--s", discovery_root],
         env={"TQDM_DISABLE": "true"})
+
+if use_coverage:
+    subprocess.run([coverage_binary, "report", "-m"])
+    subprocess.run([coverage_binary, "html"])
