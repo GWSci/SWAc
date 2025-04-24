d9303adb72153e642d69dd1c0f196e8545f543a7
Chris Foley <chris@nice-code.co.uk>
2025-04-24 11:57:54 +0100

    Issue #144. Architecture for backward compatibility of input files. 36

--- swacmod_run.py
+++ swacmod_run.py
@@ -846,13 +846,17 @@ def run(test=False, debug=False, file_format=None, reduced=False, skip=False, en
 
 def scrape_run_name_and_start_logging(debug, env, input_file):
     level = logging.DEBUG if debug else logging.INFO
-    params = input_data_v1.load_yaml(input_file)
-    run_name = params["run_name"]
+    run_name = scrape_run_name(input_file)
     log_path = io.start_logging(env, level=level, run_name=run_name)
 
     env.print('\nStart "%s"' % run_name)
     logging.info("Start SWAcMod run")
     return level,log_path
+
+def scrape_run_name(input_file):
+    params = input_data_v1.load_yaml(input_file)
+    run_name = params["run_name"]
+    return run_name
 
 def run_main():
     # Parser for command line arguments
