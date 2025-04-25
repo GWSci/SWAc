eb9ce948e61e7a807645b2c10d1185bc6cbaf05f
Chris Foley <chris@nice-code.co.uk>
2025-04-25 06:45:29 +0100

    Issue 142. Split original end to end test test into individual tests. 15

--- test/tests/tests.py
+++ test/tests/tests.py
@@ -13,14 +13,18 @@ from swacmod import utils as u
 from swacmod.input_files.input_files_version_1 import validation as v
 from swacmod.input_files.input_files_version_1 import input_data as input_data
 
-class EndToEndTests(unittest.TestCase):
-    """Test suite for the SWAcMod project."""
-
+def load_data():
     specs_file = u.CONSTANTS['SPECS_FILE']
     input_file = u.CONSTANTS['TEST_INPUT_FILE']
     input_dir = u.CONSTANTS['TEST_INPUT_DIR']
 
     data = input_data.load_and_validate(specs_file, input_file, input_dir)
+    return data
+
+class EndToEndTests(unittest.TestCase):
+    """Test suite for the SWAcMod project."""
+
+    data = load_data()
 
     def test_val_num_nodes(self):
         """Test for val_num_nodes() function."""
@@ -39,11 +43,3 @@ class EndToEndTests(unittest.TestCase):
         self.data['params'][name] = 1.0
         self.assertRaises(u.ValidationError, v.val_start_date, self.data, name)
         self.data['params'][name] = old
-
-def load_data():
-    specs_file = u.CONSTANTS['SPECS_FILE']
-    input_file = u.CONSTANTS['TEST_INPUT_FILE']
-    input_dir = u.CONSTANTS['TEST_INPUT_DIR']
-
-    data = input_data.load_and_validate(specs_file, input_file, input_dir)
-    return data
