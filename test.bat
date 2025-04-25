19166d1360678a84e626df22ef5ce8d43551dea7
Charalampos Ntigkakis <99178328+hntig@users.noreply.github.com>
2025-04-25 16:02:08 +0100

    #120 updated test.bat

--- test.bat
+++ test.bat
@@ -1,6 +1,1 @@
-call env/Scripts/activate.bat
-python "compile_model.py"
-setlocal
-set "TQDM_DISABLE=true" && python -m unittest discover -s test
-endlocal
-call env\Scripts\deactivate.bat
+python run_test.py %*
