65baf6e2b2cde35258742d333ad6b16733e03d3c
Chris Foley <chris@nice-code.co.uk>
2025-04-25 06:59:29 +0100

    Issue 142. Split original end to end test test into individual tests. 28

--- test/tests/tests.py
+++ test/tests/tests.py
@@ -3,32 +3,24 @@ from swacmod import utils as u
 from swacmod.input_files.input_files_version_1 import validation as v
 from swacmod.input_files.input_files_version_1 import input_data as input_data
 
-def load_data():
-    specs_file = u.CONSTANTS['SPECS_FILE']
-    input_file = u.CONSTANTS['TEST_INPUT_FILE']
-    input_dir = u.CONSTANTS['TEST_INPUT_DIR']
-
-    data = input_data.load_and_validate(specs_file, input_file, input_dir)
-    return data
-
 class EndToEndTests(unittest.TestCase):
     def test_val_num_nodes_with_wrong_type(self):
-        data = {'params': {}, 'specs': {}}
         name = 'num_nodes'
+        data = {'params': {}, 'specs': {}}
         data['params'][name] = 1.0
         data['specs'][name] = {'required': True, 'type': [int]}
         self.assertRaises(u.ValidationError, v.val_num_nodes, data, name)
 
     def test_val_num_nodes_when_out_of_range(self):
-        data = {'params': {}, 'specs': {}}
         name = 'num_nodes'
+        data = {'params': {}, 'specs': {}}
         data['specs'][name] = {'required': True, 'type': [int]}
         data['params'][name] = -1
         self.assertRaises(u.ValidationError, v.val_num_nodes, data, name)
 
     def test_val_start_date(self):
-        data = {'params': {}, 'specs': {}}
         name = 'start_date'
+        data = {'params': {}, 'specs': {}}
         data['specs'][name] = {'required': True, 'type': [int]}
         data['params'][name] = 1.0
         self.assertRaises(u.ValidationError, v.val_start_date, data, name)
