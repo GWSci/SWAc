a683cdfb2c3f3f4c30ea01500e93217c53ce0dc3
Chris Foley <chris@nice-code.co.uk>
2025-04-25 12:24:17 +0100

    Issue #106. Error message for invalid YAML syntax. 6

--- test/input_file_reader/input_files_version_2/test_input_data_v2.py
+++ test/input_file_reader/input_files_version_2/test_input_data_v2.py
@@ -10,3 +10,12 @@ class Test_Input_Files_v2(unittest.TestCase):
         file_opener = lambda filename: io.StringIO(mock_file_system[filename])
         actual = input_data.load_yaml("aardvark.yaml", file_opener=file_opener)
         self.assertIsNone(actual)
+
+    def test_load_yaml_reads_yaml_file_with_one_key(self):
+        mock_file_system = {
+            "aardvark.yaml": "bat: cat",
+        }
+        file_opener = lambda filename: io.StringIO(mock_file_system[filename])
+        expected = {"bat": "cat"}
+        actual = input_data.load_yaml("aardvark.yaml", file_opener=file_opener)
+        self.assertEqual(expected, actual)
