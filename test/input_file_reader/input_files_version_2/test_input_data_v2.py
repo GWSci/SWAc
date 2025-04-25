90c1e7c539d6c59f6573be5c6f9b8e294d38dde6
Chris Foley <chris@nice-code.co.uk>
2025-04-25 12:26:33 +0100

    Issue #106. Error message for invalid YAML syntax. 7

--- test/input_file_reader/input_files_version_2/test_input_data_v2.py
+++ test/input_file_reader/input_files_version_2/test_input_data_v2.py
@@ -4,10 +4,9 @@ import swacmod.input_files.input_files_version_2.input_data as input_data
 
 class Test_Input_Files_v2(unittest.TestCase):
     def test_load_yaml_reads_empty_yaml_file(self):
-        mock_file_system = {
+        file_opener = make_mock_file_opener({
             "aardvark.yaml": "",
-        }
-        file_opener = lambda filename: io.StringIO(mock_file_system[filename])
+        })
         actual = input_data.load_yaml("aardvark.yaml", file_opener=file_opener)
         self.assertIsNone(actual)
 
@@ -19,3 +18,6 @@ class Test_Input_Files_v2(unittest.TestCase):
         expected = {"bat": "cat"}
         actual = input_data.load_yaml("aardvark.yaml", file_opener=file_opener)
         self.assertEqual(expected, actual)
+
+def make_mock_file_opener(filenames_to_contents):
+    return lambda filename: io.StringIO(filenames_to_contents[filename])
