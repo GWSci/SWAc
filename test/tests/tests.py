3c13a317e401c7a91e717fadae82ec9528dc585c
Chris Foley <chris@nice-code.co.uk>
2025-04-25 07:01:25 +0100

    Issue 142. Split original end to end test test into individual tests. 31

--- test/tests/tests.py
+++ test/tests/tests.py
@@ -6,15 +6,21 @@ from swacmod.input_files.input_files_version_1 import input_data as input_data
 class EndToEndTests(unittest.TestCase):
     def test_val_num_nodes_with_wrong_type(self):
         name = 'num_nodes'
-        data = {'params': {name: 1.0}, 'specs': {name: {'required': True, 'type': [int]}}}
+        data = {
+            'params': {name: 1.0},
+            'specs': {name: {'required': True, 'type': [int]}}}
         self.assertRaises(u.ValidationError, v.val_num_nodes, data, name)
 
     def test_val_num_nodes_when_out_of_range(self):
         name = 'num_nodes'
-        data = {'params': {name: -1}, 'specs': {name: {'required': True, 'type': [int]}}}
+        data = {
+            'params': {name: -1},
+            'specs': {name: {'required': True, 'type': [int]}}}
         self.assertRaises(u.ValidationError, v.val_num_nodes, data, name)
 
     def test_val_start_date(self):
         name = 'start_date'
-        data = {'params': {name: 1.0}, 'specs': {name :{'required': True, 'type': [int]}}}
+        data = {
+            'params': {name: 1.0},
+            'specs': {name :{'required': True, 'type': [int]}}}
         self.assertRaises(u.ValidationError, v.val_start_date, data, name)
