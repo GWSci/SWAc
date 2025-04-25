36db917cc549dee868c6aaa4b533b3f7b0bb8bac
Chris Foley <chris@nice-code.co.uk>
2025-04-25 12:41:01 +0100

    Issue #106. Error message for invalid YAML syntax. 11

--- test/input_file_reader/input_files_version_2/test_input_data_v2.py
+++ test/input_file_reader/input_files_version_2/test_input_data_v2.py
@@ -8,7 +8,6 @@ class Test_Input_Files_v2(unittest.TestCase):
             "aardvark.yaml": "",
         })
         actual = input_data.load_yaml("aardvark.yaml", file_opener=file_opener)
-        self.assertIsNone(actual)
         self.assertEqual(None, actual)
 
     def test_load_yaml_reads_yaml_file_with_one_key(self):
