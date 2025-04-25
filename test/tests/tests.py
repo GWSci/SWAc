c8664cc5fad9e30cbd36278487504231be0f993c
Chris Foley <chris@nice-code.co.uk>
2025-04-25 07:01:32 +0100

    Issue 142. Split original end to end test test into individual tests. 32

--- test/tests/tests.py
+++ test/tests/tests.py
@@ -1,7 +1,6 @@
 import unittest
 from swacmod import utils as u
 from swacmod.input_files.input_files_version_1 import validation as v
-from swacmod.input_files.input_files_version_1 import input_data as input_data
 
 class EndToEndTests(unittest.TestCase):
     def test_val_num_nodes_with_wrong_type(self):
