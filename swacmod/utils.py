edfd8c618fd8e56b3b0f3d8ce78ea2d01d70a4de
Chris Foley <chris@nice-code.co.uk>
2025-04-25 07:04:35 +0100

    Issue 142. Split original end to end test test into individual tests. 34

--- swacmod/utils.py
+++ swacmod/utils.py
@@ -27,8 +27,6 @@ CONSTANTS['TEST_INPUT_DIR'] = os.path.join(CONSTANTS['TEST_DIR'],
                                            'input_files')
 CONSTANTS['TEST_INPUT_FILE'] = os.path.join(CONSTANTS['TEST_INPUT_DIR'],
                                             'input.yml')
-CONSTANTS['TEST_RESULTS_FILE'] = os.path.join(CONSTANTS['TEST_DIR'],
-                                              'results.csv')
 
 def col_order():
     if ff.use_natproc:
