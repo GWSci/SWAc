8c33c67d0175f8cf5e9e192d10c3a6d931508341
Chris Foley <chris@nice-code.co.uk>
2025-04-28 11:01:43 +0100

    Issue #129: Revised input architecture. 30

--- test/input_file_reader/test_parsed_input_data.py
+++ test/input_file_reader/test_parsed_input_data.py
@@ -1,5 +1,9 @@
 import unittest
+from swacmod.input_files.parsed_input_data import ParsedInputData
 
 class Test_Parsed_Input_Data(unittest.TestCase):
     def test_x(self):
+        testee = ParsedInputData(None, [], [])
+        actual = ""
+        testee.print()
         self.assertEqual(1, 1)
