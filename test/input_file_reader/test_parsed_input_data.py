d97b53ac5c5e2a661e29c62e4da215753f1a7257
Chris Foley <chris@nice-code.co.uk>
2025-04-28 11:01:56 +0100

    Issue #129: Revised input architecture. 31

--- test/input_file_reader/test_parsed_input_data.py
+++ test/input_file_reader/test_parsed_input_data.py
@@ -5,5 +5,5 @@ class Test_Parsed_Input_Data(unittest.TestCase):
     def test_x(self):
         testee = ParsedInputData(None, [], [])
         actual = ""
-        testee.print()
+        testee.print(printer = None)
         self.assertEqual(1, 1)
