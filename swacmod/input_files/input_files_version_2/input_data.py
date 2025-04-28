9c5ff664813d8c066c4a3888e9876430b2295ad1
Chris Foley <chris@nice-code.co.uk>
2025-04-28 08:10:38 +0100

    Issue #129: Revised input architecture. 18

--- swacmod/input_files/input_files_version_2/input_data.py
+++ swacmod/input_files/input_files_version_2/input_data.py
@@ -36,6 +36,32 @@ class Validation_Result:
     errors: list
     warnings: list
 
+def load_and_validate(specs_file, input_file, input_dir):
+    """Load, finalize and validate model parameters and time series."""
+    data = fast_load(specs_file, input_file)
+    f.finalize_required_params(data)
+    c.check_required(data)
+    data = load_params_from_yaml(specs_file, input_file, input_dir, tqdm)
+
+    f.finalize_params(data)
+    f.finalize_series(data)
+    c.check_required(data)
+    v.validate_params(data)
+    v.validate_series(data)
+
+    return data
+
+def fast_load(specs_file, input_file):
+    "Load the main input file, without loading the files the parameters point to"
+    specs = load_yaml(specs_file)
+    params = load_yaml(input_file)
+    for key in specs:
+        if key not in params:
+            params[key] = None
+    params, series = _get_series_from_params(params)
+    data = {"specs": specs, "series": series, "params": params}
+    return data
+
 def _default_file_open(filename):
     return open(filename, "r")
 
@@ -95,10 +121,28 @@ def load_params_from_yaml(specs_file, input_file, input_dir, tqdm, file_opener=_
 
     return data
 
-def _supply_null_values_for_missing_fields(specs, params):
-    for key in specs:
-        if key not in params:
-            params[key] = None
+def validate(params, input_file):
+    errors = []
+    warnings = []
+    required_fields = specs.required_field_names(specs.make_specs())
+    for field in required_fields:
+        if (not field in params):
+            errors.append(f'Error: The file "{input_file}" is missing the required field "{field}".')
+    return Validation_Result(errors, warnings)
+
+def validate_no_extra_params(specs, params, input_file):
+    valid_keys = set(specs.keys())
+    found_keys = set(params.keys())
+    unrecognised_keys = sorted(found_keys.difference(valid_keys))
+    errors = []
+
+    valid_keys_string = ", ".join([f'"{k}"' for k in sorted(valid_keys)])
+    for key in unrecognised_keys:
+        errors.append(f'Error: Found the key "{key}" in the file "{input_file}". This key is not valid. A full list of valid keys is: [{valid_keys_string}].')
+
+    if len(errors) > 0:
+        message = "\n".join(errors)
+        raise Exception(message)
 
 def _load_alt_formats(input_dir, tqdm, file_opener, specs, params, no_list):
     for param in tqdm(params, desc="SWAcMod load params     "):
@@ -113,6 +157,19 @@ def _load_alt_formats(input_dir, tqdm, file_opener, specs, params, no_list):
                 load_csv_alt_format(params, no_list, param, absolute)
             elif ext == "yml":
                 load_yml_alt_format(params, param, absolute, file_opener)
+
+def _use_time_series_data(param):
+    return param in [
+        "historical_mi_array_kg_per_time_period",
+        "interflow_decay_ts",
+        "percolation_rejection_ts",
+        "rainfall_ts",
+        "temperature_ts",
+        "tmax_c_ts",
+        "tmin_c_ts",
+        "pe_ts",
+        "windsp_ts",
+    ]
 
 def load_temp_file_backed_array(params, param, absolute, ext):
     base_path = params["temp_file_backed_array_directory"]
@@ -139,6 +196,17 @@ def load_csv_alt_format(params, no_list, param, absolute):
     except Exception as err:
         msg = "Could not import %s: %s" % (param, err)
         raise u.InputOutputError(msg)
+
+def _use_array_directly(param):
+    return (param.endswith("_ts")
+            or param == "time_periods"
+            or param == "historical_time_periods"
+            or param == "historical_mi_array_kg_per_time_period")
+
+def _supply_null_values_for_missing_fields(specs, params):
+    for key in specs:
+        if key not in params:
+            params[key] = None
 
 def load_yml_alt_format(params, param, absolute, file_opener):
     parsed_yaml = load_yaml_file_contents(param, absolute, file_opener)
@@ -174,71 +242,3 @@ def extract_keys_or_empty_list(something_that_might_have_keys):
         return something_that_might_have_keys.keys()
     except Exception:
         return []
-
-def _use_time_series_data(param):
-    return param in [
-        "historical_mi_array_kg_per_time_period",
-        "interflow_decay_ts",
-        "percolation_rejection_ts",
-        "rainfall_ts",
-        "temperature_ts",
-        "tmax_c_ts",
-        "tmin_c_ts",
-        "pe_ts",
-        "windsp_ts",
-    ]
-
-def _use_array_directly(param):
-    return (param.endswith("_ts")
-            or param == "time_periods"
-            or param == "historical_time_periods"
-            or param == "historical_mi_array_kg_per_time_period")
-
-def fast_load(specs_file, input_file):
-    "Load the main input file, without loading the files the parameters point to"
-    specs = load_yaml(specs_file)
-    params = load_yaml(input_file)
-    for key in specs:
-        if key not in params:
-            params[key] = None
-    params, series = _get_series_from_params(params)
-    data = {"specs": specs, "series": series, "params": params}
-    return data
-
-def load_and_validate(specs_file, input_file, input_dir):
-    """Load, finalize and validate model parameters and time series."""
-    data = fast_load(specs_file, input_file)
-    f.finalize_required_params(data)
-    c.check_required(data)
-    data = load_params_from_yaml(specs_file, input_file, input_dir, tqdm)
-
-    f.finalize_params(data)
-    f.finalize_series(data)
-    c.check_required(data)
-    v.validate_params(data)
-    v.validate_series(data)
-
-    return data
-
-def validate(params, input_file):
-    errors = []
-    warnings = []
-    required_fields = specs.required_field_names(specs.make_specs())
-    for field in required_fields:
-        if (not field in params):
-            errors.append(f'Error: The file "{input_file}" is missing the required field "{field}".')
-    return Validation_Result(errors, warnings)
-
-def validate_no_extra_params(specs, params, input_file):
-    valid_keys = set(specs.keys())
-    found_keys = set(params.keys())
-    unrecognised_keys = sorted(found_keys.difference(valid_keys))
-    errors = []
-
-    valid_keys_string = ", ".join([f'"{k}"' for k in sorted(valid_keys)])
-    for key in unrecognised_keys:
-        errors.append(f'Error: Found the key "{key}" in the file "{input_file}". This key is not valid. A full list of valid keys is: [{valid_keys_string}].')
-
-    if len(errors) > 0:
-        message = "\n".join(errors)
-        raise Exception(message)
