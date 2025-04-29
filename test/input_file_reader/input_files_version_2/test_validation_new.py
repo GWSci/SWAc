ebb997dbe39ffc6b7ab1785b792eb2d745c286eb
Chris Foley <chris@nice-code.co.uk>
2025-04-29 08:38:29 +0100

    Issue #129: Revised input architecture. 108

--- test/input_file_reader/input_files_version_2/test_validation_new.py
+++ test/input_file_reader/input_files_version_2/test_validation_new.py
@@ -39,6 +39,7 @@ class Test_Validation_New(unittest.TestCase):
             "output_individual": "x",
             "irchcb": "x",
             "nodes_per_line": "x",
+            "nodes_per_line": -5,
         }
         spec_maps = specs_module.make_specs_dictionary(specs_module.make_specs())
         for k, v in invalid_pairs.items():
