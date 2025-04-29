f2e0fe5c3f22448ecda433c732c5b1ec02ba11a9
Chris Foley <chris@nice-code.co.uk>
2025-04-29 10:16:31 +0100

    Issue #129: Revised input architecture. 118

--- test/input_file_reader/input_files_version_2/test_validation_new.py
+++ test/input_file_reader/input_files_version_2/test_validation_new.py
@@ -76,7 +76,7 @@ def make_valid_params():
         "reporting_zone_mapping": "reporting_zone_mapping.yml",
         "reporting_zone_names": {1:"b", 2:"d"},
         "rainfall_zone_mapping": "rainfall_zone_mapping.yml",
-        "rainfall_zone_names": {},
+        "rainfall_zone_names": {1:"b", 2:"d"},
         "pe_zone_mapping": "pe_zone_mapping.yml",
         "pe_zone_names": "pe_zone_names.yml",
         "output_individual": set([1, 2, 3]),
