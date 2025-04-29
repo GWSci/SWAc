3cf92f13049a858655e43c658b9b233f1c5cf0c1
Chris Foley <chris@nice-code.co.uk>
2025-04-29 06:54:00 +0100

    Issue #129: Revised input architecture. 81

--- test/input_file_reader/input_files_version_2/test_validation_new.py
+++ test/input_file_reader/input_files_version_2/test_validation_new.py
@@ -3,8 +3,10 @@ import swacmod.input_files.input_files_version_2.validation_new as validation_ne
 import swacmod.input_files.input_files_version_2.specs as specs_module
 
 class Test_Validation_New(unittest.TestCase):
-    def test_x(self):
+    def test_validation_converts_exceptions_to_error_list(self):
         spec_maps = specs_module.make_specs_dictionary(specs_module.make_specs())
         params = {"run_name": 5}
         actual = validation_new.validate(params, spec_maps)
-        self.assertEqual("---> Validation failed: Parameter \"run_name\" has to be a string, found a <class 'int'> instead", actual[0])
+        self.assertEqual(
+            "---> Validation failed: Parameter \"run_name\" has to be a string, found a <class 'int'> instead",
+            actual[0])
