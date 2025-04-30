89354330a31b4a2e880d4035e4148cf884755628
Chris Foley <chris@nice-code.co.uk>
2025-04-30 07:37:16 +0100

    Issue #129: Revised input architecture. 168

--- swacmod/input_files/input_files_version_2/input_data.py
+++ swacmod/input_files/input_files_version_2/input_data.py
@@ -90,12 +90,6 @@ def _make_no_list(params):
     
     return no_list
 
-def validate_keys(specs, params, input_file):
-    result = ParsedInputData(params, [], [])
-    result.update(validate_required_fields(params, input_file))
-    result.update(validate_no_extra_params(specs_module.make_specs_dictionary(specs), params, input_file))
-    return result
-
 def validate_required_fields(params, input_file):
     errors = []
     warnings = []
