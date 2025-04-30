4c1198fbda64be03e7cf601329de88ec04d954b7
Chris Foley <chris@nice-code.co.uk>
2025-04-30 08:03:05 +0100

    Issue #129: Revised input architecture. 182

--- swacmod/input_files/input_files_version_2/input_data.py
+++ swacmod/input_files/input_files_version_2/input_data.py
@@ -92,19 +92,23 @@ def _make_no_list(params):
 
 def _load_alt_formats(input_dir, tqdm, file_opener, specs, params, no_list):
     for param in tqdm(params, desc="SWAcMod load params     "):
-        should_load_alt_format = False
-        might_be_alt_format = isinstance(params[param], str) and "alt_format" in specs[param]
-        if might_be_alt_format:
-            ext = params[param].split(".")[-1]
-            is_not_alt_format = ext not in specs[param]["alt_format"] and ext != "numpydumpy"
-            is_alt_format = not is_not_alt_format
-            if is_alt_format:
-                should_load_alt_format = True
+        should_load_alt_format = f_is_alt_format(specs, params, param)
         
         if should_load_alt_format:
             absolute = os.path.join(input_dir, params[param])
             ext = params[param].split(".")[-1]
             load_alt_format(file_opener, params, no_list, param, absolute, ext)
+
+def f_is_alt_format(specs, params, param):
+    should_load_alt_format = False
+    might_be_alt_format = isinstance(params[param], str) and "alt_format" in specs[param]
+    if might_be_alt_format:
+        ext = params[param].split(".")[-1]
+        is_not_alt_format = ext not in specs[param]["alt_format"] and ext != "numpydumpy"
+        is_alt_format = not is_not_alt_format
+        if is_alt_format:
+            should_load_alt_format = True
+    return should_load_alt_format
 
 def load_alt_format(file_opener, params, no_list, param, absolute, ext):
     if _use_time_series_data(param):
