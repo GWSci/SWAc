0338940e61fc535c207cb945c0b87830b103d169
Charalampos Ntigkakis <99178328+hntig@users.noreply.github.com>
2025-05-08 14:43:21 +0100

    #134 renaming nitrate to solute 76

--- test_full_runs/test_solute_cumulative_distribution_function_Slow.py
+++ test_full_runs/test_solute_cumulative_distribution_function_Slow.py
@@ -1,7 +1,7 @@
-import swacmod.solute_proportion_reaching_water_table as nitrate_proportion
+import swacmod.solute_proportion_reaching_water_table as solute_proportion
 import unittest
 
-class Test_Nitrate_Cumulative_Distribution_Function(unittest.TestCase):
+class Test_Solute_Cumulative_Distribution_Function(unittest.TestCase):
 	def test_sum_of_daily_proportion_should_equal_1(self):
 		a = 1.38
 		μ = 1.58
@@ -11,5 +11,5 @@ class Test_Nitrate_Cumulative_Distribution_Function(unittest.TestCase):
 		DTW = 100
 		sum = 0
 		for t in range(1, 1000000):
-			sum += nitrate_proportion._calculate_daily_proportion_reaching_water_table(a, μ, σ, alpha, effective_porosity, DTW, t)
+			sum += solute_proportion._calculate_daily_proportion_reaching_water_table(a, μ, σ, alpha, effective_porosity, DTW, t)
 		self.assertAlmostEqual(1, sum, places=2)
