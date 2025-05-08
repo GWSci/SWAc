8cfb80a7e94d5bcd4333c1554f28b7b5a5341728
Charalampos Ntigkakis <99178328+hntig@users.noreply.github.com>
2025-05-08 10:49:42 +0100

    #134 renaming nitrate to solute 26

--- test/input_file_reader/input_files_version_2/test_input_data_v2_validation.py
+++ test/input_file_reader/input_files_version_2/test_input_data_v2_validation.py
@@ -174,7 +174,7 @@ def make_sample_valid_input_file():
         "historical_solute_process": "disabled",
         "solute_process": "disabled",
         "solute_calibration_a": 1.38,
-        "nitrate_calibration_mu": "mu.csv",
+        "solute_calibration_mu": "mu.csv",
         "nitrate_calibration_sigma": 3.96,
         "nitrate_calibration_alpha": 3906.25,
         "nitrate_calibration_effective_porosity": "effective_porosity.csv",
