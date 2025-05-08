c6408b70af622f78aa722b6576eba90ea66787c5
Charalampos Ntigkakis <99178328+hntig@users.noreply.github.com>
2025-05-08 11:36:26 +0100

    #134 renaming nitrate to solute 40

--- test/input_file_reader/input_files_version_2/test_validation_new.py
+++ test/input_file_reader/input_files_version_2/test_validation_new.py
@@ -287,7 +287,7 @@ def make_valid_params():
         "infiltration_limit_ts": "infiltration_limit_ts.yml",
         "interflow_decay_ts": "interflow_decay_ts.yml",
         "recharge_attenuation_params": "recharge_attenuation_params.yml",
-        "nitrate_calibration_mu": "mu.csv",
+        "solute_calibration_mu": "mu.csv",
         "nitrate_calibration_effective_porosity": "effective_porosity.csv",
         "nitrate_depth_to_water": "Average_DTW.csv",
         "nitrate_loading": "NO3_loading.csv",
