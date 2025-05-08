1b625f882694566201119f574cb659e7360b3e8a
Charalampos Ntigkakis <99178328+hntig@users.noreply.github.com>
2025-05-08 11:44:06 +0100

    #134 renaming nitrate to solute 48

--- swacmod/input_files/input_file_reader.py
+++ swacmod/input_files/input_file_reader.py
@@ -47,7 +47,8 @@ def migrate_v1_to_v2(data):
              ('nitrate_process', 'solute_process'),
              ('nitrate_calibration_a', 'solute_calibration_a'),
              ('nitrate_calibration_mu', 'solute_calibration_mu'),
-             ('nitrate_calibration_sigma', 'solute_calibration_sigma')]
+             ('nitrate_calibration_sigma', 'solute_calibration_sigma'),
+             ('nitrate_calibration_alpha', 'solute_calibration_alpha')]
     for pair in pairs:
         old, new = pair[0] , pair[1]
         if 'params' in result: 
