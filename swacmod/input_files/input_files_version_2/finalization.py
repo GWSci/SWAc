bb1ef1e6bbf30f44d32864e3e8df8b6d6819d9ec
Charalampos Ntigkakis <99178328+hntig@users.noreply.github.com>
2025-05-08 12:17:45 +0100

    #134 renaming nitrate to solute 62

--- swacmod/input_files/input_files_version_2/finalization.py
+++ swacmod/input_files/input_files_version_2/finalization.py
@@ -1147,7 +1147,7 @@ def fin_months(data, name):
     dates = np.array([np.datetime64(str(i.date())) for i in series["date"]])
     series[name] = dates.astype("datetime64[M]").astype(int) % 12
 
-def fin_historical_nitrate_days(data, name):
+def fin_historical_solute_days(data, name):
     if (data["params"]["historical_solute_process"] != "enabled"):
         return
     series, params = data["series"], data["params"]
@@ -1433,7 +1433,7 @@ FUNC_PARAMS = [
 FUNC_SERIES = [
     fin_date,
     fin_months,
-    fin_historical_nitrate_days,
+    fin_historical_solute_days,
     fin_rainfall_ts,
     fin_pe_ts,
     fin_temperature_ts,
