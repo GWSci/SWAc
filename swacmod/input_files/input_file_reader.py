756b1a46ceb9c66ed18d8524c5d40855d8217dd7
Charalampos Ntigkakis <99178328+hntig@users.noreply.github.com>
2025-05-08 10:10:16 +0100

    #134 renaming nitrate to solute 9

--- swacmod/input_files/input_file_reader.py
+++ swacmod/input_files/input_file_reader.py
@@ -43,7 +43,8 @@ def migrate_v1_to_v2(data):
     pairs = [('leakage_process', 'subroot_leakage_process'),
              ('subsoilzone_leakage_fraction', 'subroot_leakage_fraction'),
              ('sw_process_natproc', 'sw_ponding_process'),
-             ('historical_nitrate_process', 'historical_solute_process')]
+             ('historical_nitrate_process', 'historical_solute_process'),
+             ('nitrate_process', 'solute_process')]
     for pair in pairs:
         old, new = pair[0] , pair[1]
         if 'params' in result: 
