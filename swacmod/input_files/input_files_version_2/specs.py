f3bf13679bce8b4843fc3cef771472ab5ee4e904
Charalampos Ntigkakis <99178328+hntig@users.noreply.github.com>
2025-05-08 10:21:50 +0100

    #134 renaming nitrate to solute 18

--- swacmod/input_files/input_files_version_2/specs.py
+++ swacmod/input_files/input_files_version_2/specs.py
@@ -140,7 +140,7 @@ def make_specs():
         Input_Parameter('historical_start_date', required=False, alt_format=[], type=[datetime], constraints=None),
         Input_Parameter('historical_time_periods', required=False, alt_format=['yml', 'csv'], type=[list, list, int], constraints=None),
         Input_Parameter('solute_process', required=True, alt_format=[], type=[str], constraints=['enabled', 'disabled']),
-        Input_Parameter('nitrate_calibration_a', required=False, alt_format=[], type=[float], constraints=None),
+        Input_Parameter('solute_calibration_a', required=False, alt_format=[], type=[float], constraints=None),
         Input_Parameter('nitrate_calibration_mu', required=False, alt_format=['yml', 'csv'], type=[dict, float], constraints=None),
         Input_Parameter('nitrate_calibration_sigma', required=False, alt_format=[], type=[float], constraints=None),
         Input_Parameter('nitrate_calibration_alpha', required=False, alt_format=[], type=[float], constraints=None),
