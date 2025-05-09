dc8fd5506be743072109f5f89944ae07380b6ba2
Charalampos Ntigkakis <99178328+hntig@users.noreply.github.com>
2025-05-09 14:02:38 +0100

    #157 reading inputs from csv creates list 1

---
+++ test/input_file_reader/input_files_version_2/test_input_data_v2_no_list.py
@@ -0,0 +1,34 @@
+import unittest
+import io
+from contextlib import contextmanager
+import csv
+from swacmod.input_files.input_files_version_2.input_data import _make_no_list
+import swacmod.input_files.input_files_version_2.loader as loader
+
+class Test_CSVs_Are_Not_Read_As_lists(unittest.TestCase):
+    def test_init_interflow_store_csv_returns_floats(self):
+        filename = "potato.csv"
+        param = 'init_interflow_store'
+        file_contents = "1,1.0\n2,2.0\n3,3.0"
+        params = {param: filename}
+        no_list = _make_no_list(params)
+        file_opener = make_mock_csv_loader({filename: file_contents})
+        loader.load_csv_alt_format(params, no_list, param, filename, file_opener)
+        for val in params[param].values():
+            self.assertIsInstance(val, float)
+
+
+
+
+
+
+def make_mock_csv_loader(file_contents):
+    @contextmanager
+    def mock_loader(filename):
+        try:
+            with io.StringIO(file_contents[filename]) as csv_file:
+                reader =  csv.reader(csv_file)
+                yield reader
+        finally:
+            pass
+    return mock_loader
