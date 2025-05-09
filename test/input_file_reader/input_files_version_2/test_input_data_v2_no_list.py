4eaa9d7a1a427c6bb334053dfd67539efdd4e9f8
Charalampos Ntigkakis <99178328+hntig@users.noreply.github.com>
2025-05-09 14:05:04 +0100

    #157 reading inputs from csv creates list 3

--- test/input_file_reader/input_files_version_2/test_input_data_v2_no_list.py
+++ test/input_file_reader/input_files_version_2/test_input_data_v2_no_list.py
@@ -17,7 +17,16 @@ class Test_CSVs_Are_Not_Read_As_lists(unittest.TestCase):
         for val in params[param].values():
             self.assertIsInstance(val, float)
 
-
+    def test_init_interflow_store_csv_returns_floats(self):
+        filename = "potato.csv"
+        param = 'interflow_store_bypass'
+        file_contents = "1,1.0\n2,2.0\n3,3.0"
+        params = {param: filename}
+        no_list = _make_no_list(params)
+        file_opener = make_mock_csv_loader({filename: file_contents})
+        loader.load_csv_alt_format(params, no_list, param, filename, file_opener)
+        for val in params[param].values():
+            self.assertIsInstance(val, float)
 
 
 
