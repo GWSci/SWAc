6d8ef60115cb761c8c58e406493a24624896de34
Charalampos Ntigkakis <99178328+hntig@users.noreply.github.com>
2025-05-09 14:02:50 +0100

    #157 reading inputs from csv creates list 2

--- swacmod/input_files/input_files_version_2/input_data.py
+++ swacmod/input_files/input_files_version_2/input_data.py
@@ -80,6 +80,7 @@ def _make_no_list(params):
         "free_throughfall",
         "max_canopy_storage",
         "subroot_leakage_fraction",
+        "init_interflow_store",
     ] + [i for i in params if "zone_names" in i] + [
         i for i in params if ("zone_mapping" in i or "_locs" in i) and i not in
         ["rainfall_zone_mapping", "pe_zone_mapping", "subroot_zone_mapping"]
