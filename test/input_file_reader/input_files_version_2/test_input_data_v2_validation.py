9759d9d85524ef6a61293e7b997fbf34f80ffbde
Charalampos Ntigkakis <99178328+hntig@users.noreply.github.com>
2025-05-13 12:19:49 +0100

    #138 check that filename exists 10

--- test/input_file_reader/input_files_version_2/test_input_data_v2_validation.py
+++ test/input_file_reader/input_files_version_2/test_input_data_v2_validation.py
@@ -61,19 +61,19 @@ class Test_Input_Data_v2_Validation_Through_Input_File_Reader(unittest.TestCase)
         input_file = "some_file.yml"
         input_dir = ""
         params = make_sample_valid_input_file()
+        params['time_periods'] = 'potato.csv'
         input_file_contents = ""
         for k, v in params.items():
             input_file_contents += f"{k}: {v}\n"
         file_opener = make_mock_file_opener({input_file: input_file_contents})
         printer_spy = Printer_Spy()
         mock_dir = make_sample_input_directory()
-        mock_dir.remove("time_periods.csv")
         filename_exists = make_mock_filename_exists(mock_dir)
         try:
             input_file_reader.read_inputs(None, input_file, input_dir, file_opener=file_opener, printer=printer_spy.do_print, filename_exists=filename_exists)
         except:
             pass
-        self.assertEqual('Error: Unknown file name: "time_periods.csv"', printer_spy.result)
+        self.assertEqual('Error: Unknown file name: "potato.csv"', printer_spy.result)
 
 def make_mock_filename_exists(directory):
     return lambda filename: filename in directory
