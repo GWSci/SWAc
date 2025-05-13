9ff0da2d79748a48b53d2f868cecc99a59a115d7
Charalampos Ntigkakis <99178328+hntig@users.noreply.github.com>
2025-05-13 12:02:27 +0100

    #138 check that filename exists 7

--- test/input_file_reader/input_files_version_2/test_input_data_v2_validation.py
+++ test/input_file_reader/input_files_version_2/test_input_data_v2_validation.py
@@ -44,7 +44,7 @@ class Test_Input_Data_v2_Validation_Through_Input_File_Reader(unittest.TestCase)
 
     def test_reading_a_file_when_a_filename_doesnt_exist_throws_an_exception(self):
         input_file = "some_file.yml"
-        input_dir = "some_dir/"
+        input_dir = ""
         params = make_sample_valid_input_file()
         input_file_contents = ""
         for k, v in params.items():
@@ -52,6 +52,7 @@ class Test_Input_Data_v2_Validation_Through_Input_File_Reader(unittest.TestCase)
         file_opener = make_mock_file_opener({input_file: input_file_contents})
         printer = lambda x: None
         mock_dir = make_sample_input_directory()
+        mock_dir.remove("time_periods.csv")
         filename_exists = make_mock_filename_exists(mock_dir)
         # with self.assertRaisesRegex(Exception, "Run has exited with errors."):
         input_file_reader.read_inputs(None, input_file, input_dir, file_opener = file_opener, printer=printer, filename_exists=filename_exists)
