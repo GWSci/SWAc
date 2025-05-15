ad67b983993882ad796c415746f5e69e47279d46
Charalampos Ntigkakis <99178328+hntig@users.noreply.github.com>
2025-05-15 16:50:11 +0100

    #161 swac knows its version 9

--- swacmod/__init__.py
+++ swacmod/__init__.py
@@ -2,11 +2,11 @@
 # -*- coding: utf-8 -*-
 """SWAcMod init."""
 
-VERSION = [1, 1, 0]
-COMMIT_ID = "ed1044f"
+from _version import version
+from _commit_id import commit_id
 
-__version__ = ".".join([str(i) for i in VERSION])
-__commit_id__ = COMMIT_ID
+__version__ = ".".join([str(i) for i in version])
+__commit_id__ = commit_id
 __authors__ = ["Alastair Black", "Marco Lagi"]
 __author_email__ = "a.black@GWScience.co.uk"
 __copyright__ = "Copyright (C) 2016 Groundwater Science Ltd."
