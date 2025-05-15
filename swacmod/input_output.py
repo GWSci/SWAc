d400616c780834924fabbb09d767fe5ebb693af0
Charalampos Ntigkakis <99178328+hntig@users.noreply.github.com>
2025-05-15 13:34:07 +0100

    #161 swac knows its version 3

--- swacmod/input_output.py
+++ swacmod/input_output.py
@@ -14,9 +14,9 @@ import numpy
 
 # Internal modules
 from . import utils as u
-from . import __version__
 from . import feature_flags as ff
 import swacmod.h5py_adaptor as h5py_adaptor
+import swacmod.print_version_information as print_version_information
 
 def start_logging(env, level=logging.INFO, path=None, run_name=None):
     """Start logging output.
@@ -44,7 +44,7 @@ def start_logging(env, level=logging.INFO, path=None, run_name=None):
         name = "%s_%s.log" % (run_name, now)
         path = os.path.join(u.CONSTANTS["OUTPUT_DIR"], name)
         env.set_up_logging(path, "%(message)s", level)
-        logging.info("Version " + __version__)
+        logging.info(print_version_information.format_version_information())
         logging.info(disc)
 
     for handler in logging.root.handlers[:]:
