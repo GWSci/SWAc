b7e9447b6d5aa20f28ff9c3c11ba6772776d0b23
Charalampos Ntigkakis <99178328+hntig@users.noreply.github.com>
2025-05-15 14:59:58 +0100

    #161 swac knows its version 6

--- swacmod/__init__.py
+++ swacmod/__init__.py
@@ -3,8 +3,10 @@
 """SWAcMod init."""
 
 VERSION = (1, 1, 0)
+COMMIT_ID = "ed1044f"
 
 __version__ = ".".join([str(i) for i in VERSION])
+__commit_id__ = COMMIT_ID
 __authors__ = ["Alastair Black", "Marco Lagi"]
 __author_email__ = "a.black@GWScience.co.uk"
 __copyright__ = "Copyright (C) 2016 Groundwater Science Ltd."
--- test/test_version_information.py
+++ test/test_version_information.py
@@ -6,4 +6,4 @@ class Test_Version_Information(unittest.TestCase):
     def test_version_information_has_the_correct_version_number(self):
         expected = __version__
         actual = version_information.format_version_information()
-        self.assertIn(actual, expected)
+        self.assertIn(expected, actual)
