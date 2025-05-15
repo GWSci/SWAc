8533c25a0fdf89a29fae88a8d6606ce31c707e3a
Charalampos Ntigkakis <99178328+hntig@users.noreply.github.com>
2025-05-15 13:07:10 +0100

    #161 swac knows its version 1

--- swacmod/print_version_information.py
+++ swacmod/print_version_information.py
@@ -1,9 +1,8 @@
-import version_information
+from . import __version__
 
 def print_version(print=print):
 	formatted_version_information = format_version_information()
 	print(formatted_version_information)
 
 def format_version_information():
-	version = version_information.version
-	return f"Surface Water Accounting Model - SWAc\nVersion: {version}\n"
+	return f"Surface Water Accounting Model - SWAc\nVersion: {__version__}\n"
