ed1044f4893ae37707ee64b4af543f6ee3820143
Charalampos Ntigkakis <99178328+hntig@users.noreply.github.com>
2025-05-15 14:55:59 +0100

    #161 swac knows its version 5

---
+++ test/test_version_information.py
@@ -0,0 +1,9 @@
+import unittest
+import swacmod.version_information as version_information
+from swacmod import __version__
+
+class Test_Version_Information(unittest.TestCase):
+    def test_version_information_has_the_correct_version_number(self):
+        expected = __version__
+        actual = version_information.format_version_information()
+        self.assertIn(actual, expected)
