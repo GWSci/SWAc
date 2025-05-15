e7ad1969c9dd359441a312f4cab96afb6d6bb18a
Charalampos Ntigkakis <99178328+hntig@users.noreply.github.com>
2025-05-15 09:47:57 +0100

    #146 yaml parser error in the error message 3

--- test/input_file_reader/input_files_version_2/test_yaml_parser_error.py
+++ test/input_file_reader/input_files_version_2/test_yaml_parser_error.py
@@ -3,8 +3,9 @@ from swacmod.input_files.input_files_version_2.loader import load_yaml
 from test.input_file_reader.input_files_version_2.mock_file_resource import MockFileResource
 
 class Test_YAML_Parser_Error(unittest.TestCase):
-    def test_x(self):
+    def test_yaml_file_with_misaligned_keys(self):
         filename = 'recipe.csv'
         file_contents = 'ingredients:\n   sausage: 1\n  potato: 2\n' # missing one space before potato
         mock_file_opener = MockFileResource.make_mock_file_opener({filename: file_contents})
-        load_yaml(filename, mock_file_opener)
+        with self.assertRaisesRegex(Exception, 'Please see guidelines for the YAML syntax'):
+            load_yaml(filename, mock_file_opener)
