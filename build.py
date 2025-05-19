6b9280402431fcf01138429d6914feecfd10b8a6
Charalampos Ntigkakis <99178328+hntig@users.noreply.github.com>
2025-05-19 16:41:42 +0100

    #161 swac knows its version 16

--- build.py
+++ build.py
@@ -10,7 +10,7 @@ args = sys.argv[1:]
 def _default_file_open(filename, how='r'):
         return open(filename, how)
 
-def update_version(filename = '_version.py', file_open = _default_file_open):
+def get_old_version(filename = '_version.py', file_open = _default_file_open):
     with file_open(filename, 'r') as file:
         old_version = file.readlines()[0]
     old_version = old_version.replace('\n', '')
@@ -18,10 +18,7 @@ def update_version(filename = '_version.py', file_open = _default_file_open):
     old_version = old_version.replace('version', '')
     old_version = old_version.replace('=', '')
     old_version = ast.literal_eval(old_version)
-    new_version = old_version.copy()
-    new_version[-1] += 1
-    with file_open(filename, 'w') as file:
-        file.write(f'version = {new_version}')
+    return old_version
 
 def main(args):
     if (os.path.exists("build/")):
--- test/test_version_information.py
+++ test/test_version_information.py
@@ -3,7 +3,7 @@ import swacmod.version_information as version_information
 from _version import version
 from _commit_id import commit_id
 import io
-from build import update_version
+import build
 
 class Test_Version_Information(unittest.TestCase):
     def test_version_information_has_the_correct_version_number(self):
@@ -16,11 +16,12 @@ class Test_Version_Information(unittest.TestCase):
         actual = version_information.format_version_information()
         self.assertIn(expected, actual)
     
-    def test_build_script_updates_the_version(self):
+    def test_build_script_gets_the_old_version(self):
         filename = 'mock_version.py'
-        contents = 'version = [1,0,0]'
+        contents = 'version = [1, 0, 0]'
         file_open = make_mock_file_opener({filename: contents})
-        update_version(filename=filename, file_open=file_open)
+        old_version = build.get_old_version(filename, file_open)
+        self.assertIn(str(old_version), contents)
 
 
 
