f8f29e159cb8b13a5ffe19f7027a2145df03acd3
Charalampos Ntigkakis <99178328+hntig@users.noreply.github.com>
2025-05-19 16:58:39 +0100

    #161 swac knows its version 18

--- build.py
+++ build.py
@@ -10,7 +10,7 @@ args = sys.argv[1:]
 def _default_file_open(filename, how='r'):
         return open(filename, how)
 
-def get_old_version(filename = '_version.py', file_open = _default_file_open):
+def get_old_version(filename='_version.py', file_open=_default_file_open):
     with file_open(filename, 'r') as file:
         old_version = file.readlines()[0]
     old_version = old_version.replace('\n', '')
@@ -25,7 +25,7 @@ def update_version(old_version):
     new_version[-1] += 1
     return new_version
 
-def write_new_version(new_version, filename = '_version.py', file_open = _default_file_open):
+def write_new_version(new_version, filename='_version.py', file_open=_default_file_open):
     with file_open(filename, 'w') as file:
         file.write(f'version = {new_version}')
 
