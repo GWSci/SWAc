642d9603df03055a04dda7c55b4944a4c727d944
Charalampos Ntigkakis <99178328+hntig@users.noreply.github.com>
2025-05-19 14:07:17 +0100

    #161 swac knows its version 12

--- build.py
+++ build.py
@@ -2,8 +2,27 @@ import sys
 import shutil
 import os.path
 import subprocess
+from _version import version
+import ast
 
 args = sys.argv[1:]
+
+def _default_file_open(filename):
+        return open(filename, "r")
+
+def _default_file_write(filename):
+        return open(filename, "w")
+
+def update_version(filename = '_version.py', file_open = _default_file_open, file_write=_default_file_write):
+    with file_open(filename) as file:
+        old_version = file.readlines()[0]
+    old_version = old_version.replace('\n', '')
+    old_version = old_version.replace(' ', '')
+    old_version = old_version.replace('version', '')
+    old_version = old_version.replace('=', '')
+    old_version = ast.literal_eval(old_version)
+    new_version = old_version.copy()
+    new_version[-1] += 1
 
 def main(args):
     if (os.path.exists("build/")):
@@ -13,7 +32,7 @@ def main(args):
 
     if sys.platform == "win32":
         python_binary = "env/Scripts/python"
-        zip_input_files_command = "powershell Compress-Archive input_files\*.* dist\input_files.zip"
+        zip_input_files_command = "powershell Compress-Archive input_files/*.* dist/input_files.zip"
     else:
         python_binary = "env/bin/python3"
         zip_input_files_command = "zip --quiet --recurse-paths dist/input_files.zip input_files/"
--- test/test_version_information.py
+++ test/test_version_information.py
@@ -2,14 +2,33 @@ import unittest
 import swacmod.version_information as version_information
 from _version import version
 from _commit_id import commit_id
+import io
+from build import update_version
 
 class Test_Version_Information(unittest.TestCase):
     def test_version_information_has_the_correct_version_number(self):
-        expected = version
+        expected = f'{version[0]}.{version[1]}.{version[2]}'
         actual = version_information.format_version_information()
         self.assertIn(expected, actual)
     
     def test_version_information_has_the_correct_commit_id(self):
-        expected = commit_id
+        expected = str(commit_id)
         actual = version_information.format_version_information()
         self.assertIn(expected, actual)
+    
+    def test_build_script_updates_the_version(self):
+         filename = 'mock_version.py'
+         contents = 'version = [1,0,0]'
+         file_open = make_mock_file_opener({filename: contents})
+         file_write = make_mock_file_write(filename)
+         update_version(filename=filename, file_open=file_open, file_write=file_write)
+
+
+
+
+
+def make_mock_file_opener(contents):
+        return lambda filename: io.StringIO(contents[filename])
+
+def make_mock_file_write(filename):
+        return lambda filename: io.StringIO(filename)
