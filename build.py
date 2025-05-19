631ded5ad1dbb26824d41de7e4c89f45f4a3da3b
Charalampos Ntigkakis <99178328+hntig@users.noreply.github.com>
2025-05-19 12:47:03 +0100

    #161 swac knows its version 11

--- build.py
+++ build.py
@@ -3,36 +3,42 @@ import shutil
 import os.path
 import subprocess
 
-if (os.path.exists("build/")):
-    shutil.rmtree("build/")
-if (os.path.exists("dist/")):
-    shutil.rmtree("dist/")
+args = sys.argv[1:]
 
-if sys.platform == "win32":
-    python_binary = "env/Scripts/python"
-    zip_input_files_command = "powershell Compress-Archive input_files\*.* dist\input_files.zip"
-else:
-    python_binary = "env/bin/python3"
-    zip_input_files_command = "zip --quiet --recurse-paths dist/input_files.zip input_files/"
+def main(args):
+    if (os.path.exists("build/")):
+        shutil.rmtree("build/")
+    if (os.path.exists("dist/")):
+        shutil.rmtree("dist/")
 
-subprocess.run([python_binary, "compile_model.py"])
+    if sys.platform == "win32":
+        python_binary = "env/Scripts/python"
+        zip_input_files_command = "powershell Compress-Archive input_files\*.* dist\input_files.zip"
+    else:
+        python_binary = "env/bin/python3"
+        zip_input_files_command = "zip --quiet --recurse-paths dist/input_files.zip input_files/"
 
-subprocess.run([
-    python_binary,
-    "-m",
-    "PyInstaller",
-    "--clean",
-    "--noconfirm",
-    "--add-data",
-    "./swacmod/specs.yml:./swacmod/",
-    "--hidden-import",
-    "swacmod.snow_melt",
-    "--hidden-import",
-    "swacmod.networkx_adaptor",
-    "--onefile",
-    "swacmod_run.py",
-])
+    subprocess.run([python_binary, "compile_model.py"])
 
-subprocess.run(["pandoc", "doc/getting-started.md", "-o", "dist/getting-started.html"])
+    subprocess.run([
+        python_binary,
+        "-m",
+        "PyInstaller",
+        "--clean",
+        "--noconfirm",
+        "--add-data",
+        "./swacmod/specs.yml:./swacmod/",
+        "--hidden-import",
+        "swacmod.snow_melt",
+        "--hidden-import",
+        "swacmod.networkx_adaptor",
+        "--onefile",
+        "swacmod_run.py",
+    ])
 
-subprocess.run(zip_input_files_command, shell=True)
+    subprocess.run(["pandoc", "doc/getting-started.md", "-o", "dist/getting-started.html"])
+
+    subprocess.run(zip_input_files_command, shell=True)
+
+if __name__ == '__main__':
+    main(args)
