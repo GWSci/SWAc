75fec72503904c4400f641ec83ab475c76f315ee
Charalampos Ntigkakis <99178328+hntig@users.noreply.github.com>
2025-05-19 12:33:56 +0100

    #161 swac knows its version 10

--- test/test_version_information.py
+++ test/test_version_information.py
@@ -1,15 +1,15 @@
 import unittest
 import swacmod.version_information as version_information
-from swacmod import __version__
-from swacmod import __commit_id__
+from _version import version
+from _commit_id import commit_id
 
 class Test_Version_Information(unittest.TestCase):
     def test_version_information_has_the_correct_version_number(self):
-        expected = __version__
+        expected = version
         actual = version_information.format_version_information()
         self.assertIn(expected, actual)
     
     def test_version_information_has_the_correct_commit_id(self):
-        expected = __commit_id__
+        expected = commit_id
         actual = version_information.format_version_information()
         self.assertIn(expected, actual)
