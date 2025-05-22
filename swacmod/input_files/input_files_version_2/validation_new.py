1d5af89ffafbd40c56eea26878d2d5bddbd38823
Charalampos Ntigkakis <99178328+hntig@users.noreply.github.com>
2025-05-22 09:26:06 +0100

    #154 complete validations for V2 file format 38

--- swacmod/input_files/input_files_version_2/validation_new.py
+++ swacmod/input_files/input_files_version_2/validation_new.py
@@ -2123,7 +2123,7 @@ FUNC_PARAMS = [
     val_sw_activation,
     val_sw_bed_infiltration,
     val_sw_direct_recharge,
-    # val_sw_pe_to_open_water,
+    val_sw_pe_to_open_water,
     val_sw_process,
     val_sw_params,
     # val_swdis_locs,
--- test/input_file_reader/input_files_version_2/test_validation_new.py
+++ test/input_file_reader/input_files_version_2/test_validation_new.py
@@ -305,7 +305,12 @@ class Test_Validation_New(unittest.TestCase):
             "sw_direct_recharge": "x",
             "sw_direct_recharge": [],
             "sw_direct_recharge": {"a":"b","c":"d","e":"f"},
-            "sw_direct_recharge": {1:[],2:[],3:[]}
+            "sw_direct_recharge": {1:[],2:[],3:[]},
+            "sw_pe_to_open_water": 5,
+            "sw_pe_to_open_water": "x",
+            "sw_pe_to_open_water": [],
+            "sw_pe_to_open_water": {"a":"b","c":"d","e":"f"},
+            "sw_pe_to_open_water": {1:[],2:[],3:[]}
         }
         spec_maps = specs_module.make_specs_dictionary(specs_module.make_specs())
         for k, v in invalid_pairs.items():
@@ -438,9 +443,9 @@ def make_valid_params():
         "sw_downstream": {1:[0.4,0.4],2:[0.4,0.4],3:[0.4,0.4],4:[0.4,0.4],5:[0.4,0.4],6:[0.4,0.4],7:[0.4,0.4],8:[0.4,0.4],9:[0.4,0.4],10:[0.4,0.4],11:[0.4,0.4],12:[0.4,0.4]},
         "sw_activation": {1:[1.0,1.0],2:[1.0,1.0],3:[1.0,1.0],4:[1.0,1.0],5:[1.0,1.0],6:[1.0,1.0],7:[1.0,1.0],8:[1.0,1.0],9:[1.0,1.0],10:[1.0,1.0],11:[1.0,1.0],12:[1.0,1.0]},
         "sw_bed_infiltration": {1:[0.4,0.4],2:[0.4,0.4],3:[0.4,0.4],4:[0.4,0.4],5:[0.4,0.4],6:[0.4,0.4],7:[0.4,0.4],8:[0.4,0.4],9:[0.4,0.4],10:[0.4,0.4],11:[0.4,0.4],12:[0.4,0.4]},
-        "sw_direct_recharge": {1:[0.4,0.4],2:[0.4,0.4],3:[0.4,0.4],4:[0.4,0.4],5:[0.4,0.4],6:[0.4,0.4],7:[0.4,0.4],8:[0.4,0.4],9:[0.4,0.4],10:[0.4,0.4],11:[0.4,0.4],12:[0.4,0.4]}, #TODO
+        "sw_direct_recharge": {1:[0.4,0.4],2:[0.4,0.4],3:[0.4,0.4],4:[0.4,0.4],5:[0.4,0.4],6:[0.4,0.4],7:[0.4,0.4],8:[0.4,0.4],9:[0.4,0.4],10:[0.4,0.4],11:[0.4,0.4],12:[0.4,0.4]},
+        "sw_pe_to_open_water": {1:[0.0,1.0],2:[0.0,1.0],3:[0.0,1.0],4:[0.0,1.0],5:[0.0,1.0],6:[0.0,1.0],7:[0.0,1.0],8:[0.0,1.0],9:[0.0,1.0],10:[0.0,1.0],11:[0.0,1.0],12:[0.0,1.0]},
 
-        "sw_pe_to_open_water": "sw_pe_to_open_water.yml", #TODO
         "sw_ponding_area": "sw_ponding_area.yml", #TODO
         "routing_topology": "routing_parameters.csv", #TODO
         "sfr_obs": "gauges_sfr.obs", #TODO
