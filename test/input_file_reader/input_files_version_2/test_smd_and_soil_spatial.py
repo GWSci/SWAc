a34bf41626f686609da79a88e6fd4bebcd9df3d9
Charalampos Ntigkakis <99178328+hntig@users.noreply.github.com>
2025-05-23 10:48:03 +0100

    #131 always validate smd and soil spatial 4

--- test/input_file_reader/input_files_version_2/test_smd_and_soil_spatial.py
+++ test/input_file_reader/input_files_version_2/test_smd_and_soil_spatial.py
@@ -70,24 +70,22 @@ class Test_Input_File_With_Missing_Soil_Spatial(unittest.TestCase):
 
 class Test_Always_Validate_SMD(unittest.TestCase):
     def test_smd_is_validated_when_fao_process_is_enabled(self):
-        filename = 'potato.yml'
         input_file = MockFileResource.make_sample_input_file_with_data()
         input_file['fao_process'] = 'enabled'
         input_file['smd'] = 'sausage'
-        mock_file_open = make_mock_file_open_and_contents(filename, input_file)
-        params = loader.load_yaml(filename, mock_file_open)
+        mock_file_open = make_mock_file_open_and_contents('potato.yml', input_file)
+        params = loader.load_yaml('potato.yml', mock_file_open)
         specs = specs_module.make_specs_dictionary(specs_module.make_specs())
         data = {'params':params, 'specs':specs}
         with self.assertRaises(Exception):
             validation.val_smd(data, 'smd')
 
     def test_smd_is_validated_when_fao_process_is_disabled(self):
-        filename = 'potato.yml'
         input_file = MockFileResource.make_sample_input_file_with_data()
         input_file['fao_process'] = 'disabled'
         input_file['smd'] = 'sausage'
-        mock_file_open = make_mock_file_open_and_contents(filename, input_file)
-        params = loader.load_yaml(filename, mock_file_open)
+        mock_file_open = make_mock_file_open_and_contents('potato.yml', input_file)
+        params = loader.load_yaml('potato.yml', mock_file_open)
         specs = specs_module.make_specs_dictionary(specs_module.make_specs())
         data = {'params':params, 'specs':specs}
         with self.assertRaises(Exception):
