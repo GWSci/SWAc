18e33bda40089e4cf9fbf49189985d2fbaea97bd
Charalampos Ntigkakis <99178328+hntig@users.noreply.github.com>
2025-06-02 16:38:54 +0100

    #129 rewriting tests in v2 4

--- test/test_fin_pe_ts.py
+++ test/test_fin_pe_ts.py
@@ -23,12 +23,18 @@ class Test_pe_ts_Finalisation(unittest.TestCase):
         self.assertFalse(parsed_input_data.has_errors())
 
     def test_canopy_is_disabled_and_fao_is_enabled(self):
-        with tempfile.TemporaryDirectory() as temp_dir:
-            pairs = [['fao_process', 'enabled'], ['canopy_process', 'disabled']]
-            test_input_file = create_and_change_test_input_file(temp_dir, pairs)
-            actual = load_test_input_file(temp_dir, test_input_file)
-        self.assertEqual(0, actual)
-    
+        input_file = MockFileResource.make_sample_input_file_with_data()
+        input_file['fao_process'] = 'enabled'
+        input_file['canopy_process'] = 'disabled'
+        input_file_contents = ""
+        for k, v in input_file.items():
+            input_file_contents += f"{k}: {v}\n"
+        mock_file_open = MockFileResource.make_mock_file_opener({'input.yml': input_file_contents})
+        mock_input_directory = MockFileResource.make_sample_input_directory()
+        mock_filename_exists = MockFileResource.make_mock_filename_exists(mock_input_directory)
+        parsed_input_data = load_and_validate_v2(input_file='input.yml', input_dir='', file_opener=mock_file_open, filename_exists=mock_filename_exists)
+        self.assertFalse(parsed_input_data.has_errors())
+        
     def test_canopy_is_enabled_and_fao_is_disabled(self):
         with tempfile.TemporaryDirectory() as temp_dir:
             pairs = [['fao_process', 'disabled'], ['canopy_process', 'enabled']]
