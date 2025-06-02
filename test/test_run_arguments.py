ab86854e680f6417033bbfe558525f773464df30
Charalampos Ntigkakis <99178328+hntig@users.noreply.github.com>
2025-06-02 12:51:55 +0100

    make input file a required argument 5

--- test/test_run_arguments.py
+++ test/test_run_arguments.py
@@ -11,9 +11,14 @@ class Test_Input_File_Arguments(unittest.TestCase):
         with self.assertRaisesRegex(u.ArgumentError,'No input file specified. Use "-i" or "--input_yml" to specify the path to "input.yml"'):
             swacmod_run.check_arguments(args)
     
-
-
-
+    def test_specifying_input_file_does_not_raise_exception(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument("-i", "--input_yml")
+        args = parser.parse_args(args=["-i", "input.yml"])
+        try:
+            swacmod_run.check_arguments(args)
+        except Exception as err:
+            self.fail(f"Unexpected exception was raised: {err}")
 
 
 
