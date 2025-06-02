f02aaebe58fc1adc35be898cd7c3f44c2f3376f2
Charalampos Ntigkakis <99178328+hntig@users.noreply.github.com>
2025-06-02 15:19:14 +0100

    using the test argument requires a model to be specified 1

--- test/test_run_arguments.py
+++ test/test_run_arguments.py
@@ -18,9 +18,18 @@ class Test_Input_File_Arguments(unittest.TestCase):
         except Exception as err:
             self.fail(f"Unexpected exception was raised: {err}")
 
+class Test_Test_Argument(unittest.TestCase):
+    def test_using_the_test_argument_and_no_input_file_raises_exception(self):
+        parser = make_parser()
+        args = parser.parse_args(args=["-t"])
+        with self.assertRaisesRegex(u.ArgumentError,'No input file specified. Use "-i" or "--input_yml" to specify the path to "input.yml"'):
+            swacmod_run.check_arguments(args)
+
+
 def make_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input_yml")
+    parser.add_argument("-t", "--test", action="store_true")
     return parser
 
 
