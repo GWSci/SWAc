44db72f5f2ae521e09bc06b5e6994379228495ee
Chris Foley <chris@nice-code.co.uk>
2025-06-09 13:57:46 +0100

    Issue #154. Complete validations for V2 file format. 11

--- swacmod/input_files/input_files_version_2/validator.py
+++ swacmod/input_files/input_files_version_2/validator.py
@@ -65,5 +65,8 @@ def extract_keys_or_empty_list(something_that_might_have_keys):
         return []
 
 def validate_matches_spec(specs_object, params, key):
-    errors = [f"Error: No spec was found for the key '{key}'. Please contact support."]
+    if len(specs_object) == 0:
+        errors = [f"Error: No spec was found for the key '{key}'. Please contact support."]
+    else:
+        errors = [f"Error: The required key 'aardvark' could not be found."]
     return ParsedInputData(params, errors, [])
--- test/test_specs.py
+++ test/test_specs.py
@@ -1,5 +1,6 @@
 import unittest
 import swacmod.input_files.input_files_version_2.validator as validator
+from swacmod.input_files.input_files_version_2.specs import Input_Parameter
 
 class Test_Specs(unittest.TestCase):
     def test_validate_matches_spec_returns_error_when_key_is_not_in_spec(self):
@@ -19,6 +20,15 @@ class Test_Specs(unittest.TestCase):
             actual,
             params,
             "Error: No spec was found for the key 'bat'. Please contact support.")
+
+    def test_validate_matches_spec_returns_error_when_required_key_is_missing(self):
+        params = {}
+        specs_object = [Input_Parameter("aardvark", required=True, alt_format=[], type=[str], constraints=None)]
+        actual = validator.validate_matches_spec(specs_object, params, "bat")
+        self.assert_one_error(
+            actual,
+            params,
+            "Error: The required key 'aardvark' could not be found.")
 
     def assert_one_error(self, actual, params, expected_error):
         self.assertEqual(params, actual.data)
