7ded7d0d3a038820607bf31082328f3d80b319eb
Chris Foley <chris@nice-code.co.uk>
2025-06-09 14:46:51 +0100

    Issue #154. Complete validations for V2 file format. 28

--- test/test_specs.py
+++ test/test_specs.py
@@ -78,6 +78,12 @@ class Test_Specs(unittest.TestCase):
         actual = validator.validate_matches_spec(specs_object, params, "bat")
         self.assert_one_error(actual, params, "Error: The field 'bat' must be a string. The value '5.7' is invalid.")
 
+    def test_validate_matches_spec_returns_mo_error_when_string_type_matches(self):
+        params = {"aardvark": "bat"}
+        specs_object = [Input_Parameter("aardvark", required=True, alt_format=[], type=[str], constraints=None)]
+        actual = validator.validate_matches_spec(specs_object, params, "aardvark")
+        self.assert_no_errors(actual, params)
+
     def assert_one_error(self, actual, params, expected_error):
         self.assertEqual(params, actual.data)
         self.assertEqual([], actual.warnings)
