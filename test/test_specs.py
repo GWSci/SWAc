85d53f1dbef869727a142abe17cefd865f0603f9
Chris Foley <chris@nice-code.co.uk>
2025-06-09 14:57:40 +0100

    Issue #154. Complete validations for V2 file format. 34

--- test/test_specs.py
+++ test/test_specs.py
@@ -6,7 +6,7 @@ class Test_Specs(unittest.TestCase):
     def test_validate_matches_spec_returns_error_when_key_is_not_in_spec(self):
         params = {}
         specs_object = []
-        actual = validator.validate_matches_spec(specs_object, params, "aardvark")
+        actual = matches_spec_adaptor(specs_object, params, "aardvark")
         self.assert_one_error(
             actual,
             params,
@@ -15,7 +15,7 @@ class Test_Specs(unittest.TestCase):
     def test_validate_matches_spec_returns_error_when_key_is_not_in_spec_2(self):
         params = {}
         specs_object = []
-        actual = validator.validate_matches_spec(specs_object, params, "bat")
+        actual = matches_spec_adaptor(specs_object, params, "bat")
         self.assert_one_error(
             actual,
             params,
@@ -24,7 +24,7 @@ class Test_Specs(unittest.TestCase):
     def test_validate_matches_spec_returns_error_when_key_is_not_in_spec_3(self):
         params = {}
         specs_object = [Input_Parameter("aardvark", True, [], [str], None)]
-        actual = validator.validate_matches_spec(specs_object, params, "bat")
+        actual = matches_spec_adaptor(specs_object, params, "bat")
         self.assert_one_error(
             actual,
             params,
@@ -33,7 +33,7 @@ class Test_Specs(unittest.TestCase):
     def test_validate_matches_spec_returns_error_when_required_key_is_missing(self):
         params = {}
         specs_object = [Input_Parameter("aardvark", True, [], [str], None)]
-        actual = validator.validate_matches_spec(specs_object, params, "aardvark")
+        actual = matches_spec_adaptor(specs_object, params, "aardvark")
         self.assert_one_error(
             actual,
             params,
@@ -42,7 +42,7 @@ class Test_Specs(unittest.TestCase):
     def test_validate_matches_spec_returns_error_when_required_key_is_missing_2(self):
         params = {}
         specs_object = [Input_Parameter("bat", True, [], [str], None)]
-        actual = validator.validate_matches_spec(specs_object, params, "bat")
+        actual = matches_spec_adaptor(specs_object, params, "bat")
         self.assert_one_error(
             actual,
             params,
@@ -51,55 +51,55 @@ class Test_Specs(unittest.TestCase):
     def test_validate_matches_spec_returns_no_errors_when_a_non_required_field_is_missing(self):
         params = {}
         specs_object = [Input_Parameter("aardvark", False, [], [str], None)]
-        actual = validator.validate_matches_spec(specs_object, params, "aardvark")
+        actual = matches_spec_adaptor(specs_object, params, "aardvark")
         self.assert_no_errors(actual, params)
 
     def test_validate_matches_spec_returns_no_errors_when_a_non_required_field_is_present(self):
         params = {"aardvark": "bat"}
         specs_object = [Input_Parameter("aardvark", False, [], [str], None)]
-        actual = validator.validate_matches_spec(specs_object, params, "aardvark")
+        actual = matches_spec_adaptor(specs_object, params, "aardvark")
         self.assert_no_errors(actual, params)
 
     def test_validate_matches_spec_returns_no_errors_when_a_required_field_is_present(self):
         params = {"aardvark": "bat"}
         specs_object = [Input_Parameter("aardvark", True, [], [str], None)]
-        actual = validator.validate_matches_spec(specs_object, params, "aardvark")
+        actual = matches_spec_adaptor(specs_object, params, "aardvark")
         self.assert_no_errors(actual, params)
 
     def test_validate_matches_spec_returns_error_when_string_type_does_not_match(self):
         params = {"aardvark": 3}
         specs_object = [Input_Parameter("aardvark", True, [], [str], None)]
-        actual = validator.validate_matches_spec(specs_object, params, "aardvark")
+        actual = matches_spec_adaptor(specs_object, params, "aardvark")
         self.assert_one_error(actual, params, "Error: The field 'aardvark' must be a string. The value '3' is invalid.")
 
     def test_validate_matches_spec_returns_error_when_string_type_does_not_match(self):
         params = {"bat": 5.7}
         specs_object = [Input_Parameter("bat", True, [], [str], None)]
-        actual = validator.validate_matches_spec(specs_object, params, "bat")
+        actual = matches_spec_adaptor(specs_object, params, "bat")
         self.assert_one_error(actual, params, "Error: The field 'bat' must be a string. The value '5.7' is invalid.")
 
     def test_validate_matches_spec_returns_no_error_when_string_type_matches(self):
         params = {"aardvark": "bat"}
         specs_object = [Input_Parameter("aardvark", True, [], [str], None)]
-        actual = validator.validate_matches_spec(specs_object, params, "aardvark")
+        actual = matches_spec_adaptor(specs_object, params, "aardvark")
         self.assert_no_errors(actual, params)
 
     def test_validate_matches_spec_returns_error_when_int_type_does_not_match_string(self):
         params = {"aardvark": "bat"}
         specs_object = [Input_Parameter("aardvark", True, [], [int], None)]
-        actual = validator.validate_matches_spec(specs_object, params, "aardvark")
+        actual = matches_spec_adaptor(specs_object, params, "aardvark")
         self.assert_one_error(actual, params, "Error: The field 'aardvark' must be an integer. The value 'bat' is invalid.")
 
     def test_validate_matches_spec_returns_error_when_int_type_does_not_match_float(self):
         params = {"aardvark": 3.5}
         specs_object = [Input_Parameter("aardvark", True, [], [int], None)]
-        actual = validator.validate_matches_spec(specs_object, params, "aardvark")
+        actual = matches_spec_adaptor(specs_object, params, "aardvark")
         self.assert_one_error(actual, params, "Error: The field 'aardvark' must be an integer. The value '3.5' is invalid.")
 
     def test_validate_matches_spec_returns_error_when_int_type_matches(self):
         params = {"aardvark": 3}
         specs_object = [Input_Parameter("aardvark", True, [], [int], None)]
-        actual = validator.validate_matches_spec(specs_object, params, "aardvark")
+        actual = matches_spec_adaptor(specs_object, params, "aardvark")
         self.assert_no_errors(actual, params)
 
     def assert_one_error(self, actual, params, expected_error):
@@ -111,3 +111,6 @@ class Test_Specs(unittest.TestCase):
         self.assertEqual(params, actual.data)
         self.assertEqual([], actual.warnings)
         self.assertEqual([], actual.errors)
+
+def matches_spec_adaptor(specs_object, params, key):
+    return validator.validate_matches_spec(specs_object, params, key)
