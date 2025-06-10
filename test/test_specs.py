f692e957fefbdfe8c4de83a5a886cad1be950ad0
Chris Foley <chris@nice-code.co.uk>
2025-06-10 10:18:10 +0100

    Issue #154. Complete validations for V2 file format. 45

--- test/test_specs.py
+++ test/test_specs.py
@@ -97,6 +97,9 @@ class Test_Specs(unittest.TestCase):
         spec_type = [int]
         value = 3.5
         expected_error_message = "Error: The field 'aardvark' must be an integer. The value '3.5' is invalid."
+        self._test_error_message_when_type_of_value_is_not_ok(spec_type, value, expected_error_message)
+
+    def _test_error_message_when_type_of_value_is_not_ok(self, spec_type, value, expected_error_message):
         params = {"aardvark": value}
         specs_object = [Input_Parameter("aardvark", True, [], spec_type, None)]
         actual = matches_spec_adaptor(specs_object, params, "aardvark")
