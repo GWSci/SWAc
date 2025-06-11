453f50335d5493e93dfe23766e2df8bcb2e361e2
Chris Foley <chris@nice-code.co.uk>
2025-06-11 06:25:43 +0100

    Issue #154. Complete validations for V2 file format. 106

--- swacmod/input_files/input_files_version_2/validator.py
+++ swacmod/input_files/input_files_version_2/validator.py
@@ -102,11 +102,9 @@ def find_type_synonyms(t):
     return acceptable_types
 
 def validate_set_member_types(spec, errors, key, value):
-    collection_type = set
     iterate_members = lambda: value
-    member_description = "member"
     expected_member_type = lambda: spec.type[1]
-    _validate_member_types(spec, errors, key, value, collection_type, iterate_members, member_description, expected_member_type)
+    _validate_member_types(spec, errors, key, value, set, iterate_members, "member", expected_member_type)
 
 def _validate_member_types(spec, errors, key, value, collection_type, iterate_members, member_description, expected_member_type):
     if (type(value) != collection_type) or (spec.type[0] != collection_type):
@@ -119,18 +117,14 @@ def _validate_member_types(spec, errors, key, value, collection_type, iterate_me
             break
 
 def validate_dictionary_key_types(spec, errors, key, value):
-    collection_type = dict
     iterate_members = lambda: value.keys()
-    member_description = "dictionary key"
     expected_member_type = lambda: int
-    _validate_member_types(spec, errors, key, value, collection_type, iterate_members, member_description, expected_member_type)
+    _validate_member_types(spec, errors, key, value, dict, iterate_members, "dictionary key", expected_member_type)
 
 def validate_dictionary_value_types(spec, errors, key, value):
-    collection_type = dict
     iterate_members = lambda: value.values()
-    member_description = "dictionary value"
     expected_member_type = lambda: spec.type[1]
-    _validate_member_types(spec, errors, key, value, collection_type, iterate_members, member_description, expected_member_type)
+    _validate_member_types(spec, errors, key, value, dict, iterate_members, "dictionary value", expected_member_type)
 
 def _convert_type_to_string(spec_type):
     word = convert_type_to_english(spec_type[0])
