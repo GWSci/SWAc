4ce2171fa3403da37909c717b986b92a7a6c76b4
Chris Foley <chris@nice-code.co.uk>
2025-06-11 05:38:24 +0100

    Issue #154. Complete validations for V2 file format. 85

--- swacmod/input_files/input_files_version_2/validator.py
+++ swacmod/input_files/input_files_version_2/validator.py
@@ -82,10 +82,11 @@ def validate_matches_spec(specs_object, params, key):
 
     value = params[key]
 
+    t = spec.type[0]
     type_synonyms = {
         float: [float, int]
     }
-    acceptable_types = type_synonyms.get(spec.type[0], [spec.type[0]])
+    acceptable_types = type_synonyms.get(t, [t])
     if type(value) not in acceptable_types:
         type_string = _convert_type_to_string(spec.type)
         errors.append(f"Error: The field '{key}' must be {type_string}. The value '{value}' is invalid.")
