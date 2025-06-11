6e485a24067dc8d5f4d51c1be09973d78c022f0b
Chris Foley <chris@nice-code.co.uk>
2025-06-11 05:29:54 +0100

    Issue #154. Complete validations for V2 file format. 80

--- swacmod/input_files/input_files_version_2/validator.py
+++ swacmod/input_files/input_files_version_2/validator.py
@@ -90,13 +90,16 @@ def validate_matches_spec(specs_object, params, key):
         type_string = _convert_type_to_string(spec.type)
         errors.append(f"Error: The field '{key}' must be {type_string}. The value '{value}' is invalid.")
 
+    validate_set_members(key, errors, spec, value)
+    return result
+
+def validate_set_members(key, errors, spec, value):
     if (type(value) == set) and (spec.type[0] == set):
         for m in value:
             if type(m) != spec.type[1]:
                 type_string = _convert_type_to_string(spec.type)
                 errors.append(f"Error: The field '{key}' must be {type_string}. The member '{m}' is invalid.")
                 break
-    return result
 
 def _convert_type_to_string(spec_type):
     word = convert_type_to_english(spec_type[0])
