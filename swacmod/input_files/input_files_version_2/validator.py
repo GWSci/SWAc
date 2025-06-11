6468b7ab2b0a3f09cfe67e5463c0a4449c56b5d3
Chris Foley <chris@nice-code.co.uk>
2025-06-11 05:58:13 +0100

    Issue #154. Complete validations for V2 file format. 94

--- swacmod/input_files/input_files_version_2/validator.py
+++ swacmod/input_files/input_files_version_2/validator.py
@@ -84,6 +84,7 @@ def validate_matches_spec(specs_object, params, key):
 
     validate_type(key, errors, spec, value)
     validate_set_member_types(spec, errors, key, value)
+    validate_dictionary_key_types(spec, errors, key, value)
     return result
 
 def validate_type(key, errors, spec, value):
@@ -107,6 +108,16 @@ def validate_set_member_types(spec, errors, key, value):
         if type(m) not in acceptable_types:
             type_string = _convert_type_to_string(spec.type)
             errors.append(f"Error: The field '{key}' must be {type_string}. The member '{m}' is invalid.")
+            break
+
+def validate_dictionary_key_types(spec, errors, key, value):
+    if (type(value) != dict) or (spec.type[0] != dict):
+        return
+    acceptable_types = find_type_synonyms(int)
+    for m in value.keys():
+        if type(m) not in acceptable_types:
+            type_string = _convert_type_to_string(spec.type)
+            errors.append(f"Error: The field '{key}' must be {type_string}. The dictionary key '{m}' is invalid.")
             break
 
 def _convert_type_to_string(spec_type):
--- test/test_specs.py
+++ test/test_specs.py
@@ -145,6 +145,12 @@ class Test_Specs(unittest.TestCase):
             "x",
             "Error: The field 'aardvark' must be a dictionary mapping integers to integers. The value 'x' is invalid.")
 
+    def test_validate_spec_returns_error_when_dict_type_key_does_not_match(self):
+        self._test_error_message_when_type_of_value_is_not_ok(
+            [dict, int],
+            {1: 11, "y": 13, 3: 17},
+            "Error: The field 'aardvark' must be a dictionary mapping integers to integers. The dictionary key 'y' is invalid.")
+
     def _test_error_message_when_type_of_value_is_not_ok(
             self, spec_type, value, expected_error_message):
         params = {"aardvark": value}
