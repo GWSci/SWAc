f5410a53d81ddf2deb7295cdeeecfe3000a0ea78
Chris Foley <chris@nice-code.co.uk>
2025-06-11 06:58:30 +0100

    Issue #154. Complete validations for V2 file format. 130

--- swacmod/input_files/input_files_version_2/validator.py
+++ swacmod/input_files/input_files_version_2/validator.py
@@ -84,10 +84,10 @@ def validate_matches_spec(specs_object, params, key):
         config.errors.append(f"Error: No spec was found for the key '{config.key}'. Please contact support.")
         return result
 
-    if (config.spec.required) and (key not in params):
+    if (config.spec.required) and (key not in config.params):
         config.errors.append(f"Error: The required key '{config.key}' could not be found.")
 
-    if key not in params:
+    if key not in config.params:
         return result
 
     validate_type(config)
