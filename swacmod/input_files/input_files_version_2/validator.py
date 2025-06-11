e9ed0910b75a962795a4f17905b322c5a4fec705
Chris Foley <chris@nice-code.co.uk>
2025-06-11 06:45:10 +0100

    Issue #154. Complete validations for V2 file format. 123

--- swacmod/input_files/input_files_version_2/validator.py
+++ swacmod/input_files/input_files_version_2/validator.py
@@ -124,11 +124,12 @@ def _validate_member_types(
         iterate_members,
         member_description,
         expected_member_type):
-    is_collection_type = (
-        (type(config.value) == collection_type) 
+
+    is_collection_type = ((type(config.value) == collection_type) 
         and (config.spec.type[0] == collection_type))
     if not is_collection_type:
         return
+
     acceptable_types = find_type_synonyms(expected_member_type())
     for m in iterate_members():
         if type(m) not in acceptable_types:
