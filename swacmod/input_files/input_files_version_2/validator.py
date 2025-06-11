0a8ba3d812fd26fab80080439ba4463b5ab5638a
Chris Foley <chris@nice-code.co.uk>
2025-06-11 06:09:17 +0100

    Issue #154. Complete validations for V2 file format. 98

--- swacmod/input_files/input_files_version_2/validator.py
+++ swacmod/input_files/input_files_version_2/validator.py
@@ -104,37 +104,40 @@ def find_type_synonyms(t):
 def validate_set_member_types(spec, errors, key, value):
     collection_type = set
     iterate_members = lambda: value
+    member_description = "member"
     if (type(value) != collection_type) or (spec.type[0] != collection_type):
         return
     acceptable_types = find_type_synonyms(spec.type[1])
     for m in iterate_members():
         if type(m) not in acceptable_types:
             type_string = _convert_type_to_string(spec.type)
-            errors.append(f"Error: The field '{key}' must be {type_string}. The member '{m}' is invalid.")
+            errors.append(f"Error: The field '{key}' must be {type_string}. The {member_description} '{m}' is invalid.")
             break
 
 def validate_dictionary_key_types(spec, errors, key, value):
     collection_type = dict
     iterate_members = lambda: value.keys()
+    member_description = "dictionary key"
     if (type(value) != collection_type) or (spec.type[0] != collection_type):
         return
     acceptable_types = find_type_synonyms(int)
     for m in iterate_members():
         if type(m) not in acceptable_types:
             type_string = _convert_type_to_string(spec.type)
-            errors.append(f"Error: The field '{key}' must be {type_string}. The dictionary key '{m}' is invalid.")
+            errors.append(f"Error: The field '{key}' must be {type_string}. The {member_description} '{m}' is invalid.")
             break
 
 def validate_dictionary_value_types(spec, errors, key, value):
     collection_type = dict
     iterate_members = lambda: value.values()
+    member_description = "dictionary value"
     if (type(value) != collection_type) or (spec.type[0] != collection_type):
         return
     acceptable_types = find_type_synonyms(int)
     for m in iterate_members():
         if type(m) not in acceptable_types:
             type_string = _convert_type_to_string(spec.type)
-            errors.append(f"Error: The field '{key}' must be {type_string}. The dictionary value '{m}' is invalid.")
+            errors.append(f"Error: The field '{key}' must be {type_string}. The {member_description} '{m}' is invalid.")
             break
 
 def _convert_type_to_string(spec_type):
