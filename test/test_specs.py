b0cff85f2fcb638d21bc2370f6de2611a3236b71
Chris Foley <chris@nice-code.co.uk>
2025-06-11 11:13:27 +0100

    Issue #154. Complete validations for V2 file format. 176

--- test/test_specs.py
+++ test/test_specs.py
@@ -224,6 +224,12 @@ class Test_Specs(unittest.TestCase):
             [[1, 2], [3, 5], [7, "dog"]],
             "Error: The field 'aardvark' must be a list of list of integers. The inner list value 'dog' is invalid.")
 
+    def test_validate_spec_returns_error_when_list_list_float_does_not_match_member(self):
+        self._test_error_message_when_type_of_value_is_not_ok(
+            [list, list, float],
+            [[1, 2], [3.4, 5.6], [7.0, "dog"]],
+            "Error: The field 'aardvark' must be a list of list of floats. The inner list value 'dog' is invalid.")
+
     def _test_error_message_when_type_of_value_is_not_ok(
             self, spec_type, value, expected_error_message):
         params = {"aardvark": value}
