196c750177147efe7b1819ffcb364161a74a4525
Chris Foley <chris@nice-code.co.uk>
2025-06-12 05:48:57 +0100

    Issue #154. Complete validations for V2 file format. 185

--- test/test_specs.py
+++ test/test_specs.py
@@ -361,6 +361,12 @@ class Test_Specs(unittest.TestCase):
             [list, dict, list],
             [{"a": [1, "a"]}, {"a": [2, "b"]}, {"c": [3, "c"]}])
 
+    def test_validate_spec_no_error_when_constraints_are_none(self):
+        params = {"aardvark": "bat"}
+        specs_object = [Input_Parameter("aardvark", True, [], [str], None)]
+        actual = matches_spec_adaptor(specs_object, params, "aardvark")
+        self.assert_no_errors(actual, params)
+
     def _test_no_error_when_type_of_value_is_ok(self, spec_type, value):
         params = {"aardvark": value}
         specs_object = [Input_Parameter("aardvark", True, [], spec_type, None)]
