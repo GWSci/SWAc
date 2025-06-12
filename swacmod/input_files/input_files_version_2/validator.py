ae6674f5ec0086768b4ac4dfc2ea4d209781e226
Chris Foley <chris@nice-code.co.uk>
2025-06-12 08:02:52 +0100

    Issue #154. Complete validations for V2 file format. 239

--- swacmod/input_files/input_files_version_2/validator.py
+++ swacmod/input_files/input_files_version_2/validator.py
@@ -211,7 +211,7 @@ def _validate_inner_type(
         else:
             continue
         for x in inner_iterator:
-            if type(x) not in acceptable_types:
+            if not is_type_matching(type(x), acceptable_types):
                 type_string = english.format_type_list(config.spec.type)
                 config.errors.append(
                     f"Error: The field '{config.key}' must be {type_string}. "
