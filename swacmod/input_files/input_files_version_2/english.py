b39e20d484d31c9738339a44171f1011fc7cf62f
Chris Foley <chris@nice-code.co.uk>
2025-06-12 06:17:27 +0100

    Issue #154. Complete validations for V2 file format. 202

--- swacmod/input_files/input_files_version_2/english.py
+++ swacmod/input_files/input_files_version_2/english.py
@@ -1,5 +1,8 @@
 def format_list(a_list):
     quoted_list = [f"'{x}'" for x in a_list]
+    result = ""
+    if len(a_list) > 0:
+        result += quoted_list[0]
     if len(a_list) == 0:
         return ""
     return quoted_list[-1]
