13e9fb6e7007826fbb46b0f151dac74f0295a767
Chris Foley <chris@nice-code.co.uk>
2025-06-12 06:51:34 +0100

    Issue #154. Complete validations for V2 file format. 222

--- swacmod/input_files/input_files_version_2/validator.py
+++ swacmod/input_files/input_files_version_2/validator.py
@@ -92,7 +92,6 @@ def validate_all_match_spec_allowing_alt(specs_object, params):
 
 def is_alt(spec, params):
     value = params[spec.name]
-
     if (spec.alt_format == None) or (not isinstance(value, str)):
         return False
 
