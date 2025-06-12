81572246c6190bc619b3067a1dfa0cf79a87a209
Chris Foley <chris@nice-code.co.uk>
2025-06-12 14:32:34 +0100

    Issue #154. Complete validations for V2 file format. 284

--- swacmod/input_files/input_files_version_2/checks.py
+++ swacmod/input_files/input_files_version_2/checks.py
@@ -53,8 +53,6 @@ def check_type(param=None, name=None, t_types=None, len_list=None, keys=None):
         if not isinstance(param, t_type):
             msg = 'Parameter "%s" has to be %s, found a %s instead'
             type_in_english = MAPPING.get(t_type, [t_type, t_type])[0]
-            raise u.ValidationError(msg % (name, type_in_english,
-                                           type(param)))
 
         if t_type == expanded_list and new_len and len(param) != new_len:
             msg = 'Parameter "%s" has to be a list of length %d, found %d'
