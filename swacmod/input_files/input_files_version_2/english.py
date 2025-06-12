fb25c5674116233d850cde53c0f7eefbd8bf40b9
Chris Foley <chris@nice-code.co.uk>
2025-06-12 06:11:24 +0100

    Issue #154. Complete validations for V2 file format. 198

---
+++ swacmod/input_files/input_files_version_2/english.py
@@ -0,0 +1,2 @@
+def format_list(a_list):
+    return ""
--- test/test_english.py
+++ test/test_english.py
@@ -1,5 +1,6 @@
 import unittest
+import swacmod.input_files.input_files_version_2.english as english
 
 class Test_English(unittest.TestCase):
     def test_x(self):
-        self.assertEqual(1, 1)
+        self.assertEqual("", english.format_list([]))
