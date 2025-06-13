5b1ed8b631ef11cbfae595737522391c9ebc8778
Chris Foley <chris@nice-code.co.uk>
2025-06-13 09:53:13 +0100

    Issue #154. Complete validations for V2 file format. 290

--- swacmod/input_files/input_files_version_2/validation_new.py
+++ swacmod/input_files/input_files_version_2/validation_new.py
@@ -1843,7 +1843,6 @@ def val_nevtopt(data, name):
     )
 
 FUNC_PARAMS = [
-    val_num_cores,
     val_num_nodes,
     val_node_areas,
     val_start_date,
@@ -1943,6 +1942,8 @@ def validate(params, specs):
     }
 
     errors = []
+
+    do_validation(errors, specs, data, val_num_cores, "num_cores")
 
     for function in FUNC_PARAMS + FUNC_SERIES:
         param = function.__name__.replace("val_", "")
