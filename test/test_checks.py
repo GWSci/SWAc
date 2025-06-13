5050707d38249fdfa27b4ee65ec626f7bf39731e
Chris Foley <chris@nice-code.co.uk>
2025-06-13 16:50:30 +0100

    Issue #154. Complete validations for V2 file format. 371

--- test/test_checks.py
+++ test/test_checks.py
@@ -5,6 +5,7 @@ import swacmod.input_files.input_files_version_2.checks as checks
 class MyTestCase(unittest.TestCase):
     def test_validating_keys_when_keys_is_none(self):
         self.assert_validate_keys_passes({}, [dict], None)
+        self.assert_validate_keys_passes({"a":1}, [dict], None)
 
     def assert_validate_keys_passes(self, param, t_types, keys):
         checks.check_type(param=param, name="cat", t_types=t_types, len_list=None, keys=keys)
