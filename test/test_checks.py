77609ac7da769ceac50c5275a22574da63dc7a18
Chris Foley <chris@nice-code.co.uk>
2025-06-15 06:08:30 +0100

    Issue #154. Complete validations for V2 file format. 387

--- test/test_checks.py
+++ test/test_checks.py
@@ -39,6 +39,7 @@ class MyTestCase(unittest.TestCase):
         self.assert_validate_keys_fails([{}], [list, dict], ["a"])
         self.assert_validate_keys_fails([{"c":1}], [list, dict], ["a"])
         self.assert_validate_keys_fails([{"c":1}, {"a":1}], [list, dict], ["a"])
+        self.assert_validate_keys_fails([{"a":1}, {"c":1}], [list, dict], ["a"])
 
     def assert_validate_keys_passes(self, param, t_types, keys):
         checks.check_type(param=param, name="cat", t_types=t_types, len_list=None, keys=keys)
