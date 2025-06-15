0638350252270bf852f2eb4f6bbe9c054d1ba07a
Chris Foley <chris@nice-code.co.uk>
2025-06-15 08:05:44 +0100

    Issue #154. Complete validations for V2 file format. 430

--- swacmod/input_files/input_files_version_2/checks.py
+++ swacmod/input_files/input_files_version_2/checks.py
@@ -44,11 +44,11 @@ def check_type(param=None, name=None, t_types=None, len_list=None, keys=None):
 
     expanded_list = expand_t_type(list)
 
-    new_len = None
     if t_type == expanded_list and len_list:
         new_len = len_list[0]
         next_len_list = len_list[1:]
     else:
+        new_len = None
         next_len_list = len_list
 
     if t_type == expanded_list and new_len and len(param) != new_len:
