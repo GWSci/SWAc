9da715d0d5b59c58b82cac14cf23bf884a183ca1
Chris Foley <chris@nice-code.co.uk>
2025-06-15 06:30:32 +0100

    Issue #154. Complete validations for V2 file format. 402

--- test/test_checks.py
+++ test/test_checks.py
@@ -74,6 +74,7 @@ class MyTestCase(unittest.TestCase):
         self.assert_validate_list_length_fails(["a", "b"], [list], [1])
         self.assert_validate_list_length_fails([], [list], [2])
         self.assert_validate_list_length_fails(["a"], [list], [2])
+        self.assert_validate_list_length_fails(["a", "b", "c"], [list], [2])
 
     def assert_validate_list_length_passes(self, param, t_types, list_lengths):
         _validate_list_length_adaptor(param, t_types, list_lengths)
