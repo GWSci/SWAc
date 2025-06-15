0eb3b333d040d103e67ffbcb1ae895fc7ccf8966
Chris Foley <chris@nice-code.co.uk>
2025-06-15 06:08:16 +0100

    Issue #154. Complete validations for V2 file format. 386

--- test/test_checks.py
+++ test/test_checks.py
@@ -38,6 +38,7 @@ class MyTestCase(unittest.TestCase):
     def test_validating_keys_when_dict_is_nested_in_list_deos_not_match(self):
         self.assert_validate_keys_fails([{}], [list, dict], ["a"])
         self.assert_validate_keys_fails([{"c":1}], [list, dict], ["a"])
+        self.assert_validate_keys_fails([{"c":1}, {"a":1}], [list, dict], ["a"])
 
     def assert_validate_keys_passes(self, param, t_types, keys):
         checks.check_type(param=param, name="cat", t_types=t_types, len_list=None, keys=keys)
