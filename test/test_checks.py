b49dfb0ecbdbfb226389b01d8388a877e2b44666
Chris Foley <chris@nice-code.co.uk>
2025-06-15 05:50:02 +0100

    Issue #154. Complete validations for V2 file format. 379

--- test/test_checks.py
+++ test/test_checks.py
@@ -24,6 +24,9 @@ class MyTestCase(unittest.TestCase):
         self.assert_validate_keys_passes({"a":1, "b":2}, [dict], ["a"])
         self.assert_validate_keys_passes({"a":1, "b":2, "c":3}, [dict], ["a", "b"])
 
+    def test_validating_keys_when_type_is_not_dict(self):
+        self.assert_validate_keys_passes("aardvark", [str], ["a"])
+
     def assert_validate_keys_passes(self, param, t_types, keys):
         checks.check_type(param=param, name="cat", t_types=t_types, len_list=None, keys=keys)
 
