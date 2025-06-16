76f2ed12036832eca6273a55327b1eb649dee2d1
Chris Foley <chris@nice-code.co.uk>
2025-06-16 12:33:45 +0100

    Issue #154. Complete validations for V2 file format. 585

--- swacmod/input_files/input_files_version_2/validation_new.py
+++ swacmod/input_files/input_files_version_2/validation_new.py
@@ -68,29 +68,32 @@ def validate_zone_mapping_C(zone_name, errors, data, name):
     min_inclusive = 0
     message_format = "zone in %s"
     param = data["params"][name]
+    param_values = param.values()
     tot = data["params"]["num_nodes"]
     tzn = data["params"][zone_name]
     c.validate_keys(errors, param, name, data["specs"][name]["type"], range(1, tot + 1))
-    c.validate_min_inclusive(errors, param.values(), message_format % name, min_inclusive)
-    c.validate_max_inclusive(errors, param.values(), message_format % name, len(tzn))
+    c.validate_min_inclusive(errors, param_values, message_format % name, min_inclusive)
+    c.validate_max_inclusive(errors, param_values, message_format % name, len(tzn))
 
 def validate_zone_mapping_A(zone_name, min_inclusive, errors, data, name):
     message_format = "zone in %s"
     param = data["params"][name]
+    param_values = [i[0] for i in param.values()]
     tot = data["params"]["num_nodes"]
     tzn = data["params"][zone_name]
     c.validate_keys(errors, param, name, data["specs"][name]["type"], range(1, tot + 1))
-    c.validate_min_inclusive(errors, [i[0] for i in param.values()], message_format % name, min_inclusive)
-    c.validate_max_inclusive(errors, [i[0] for i in param.values()], message_format % name, len(tzn))
+    c.validate_min_inclusive(errors, param_values, message_format % name, min_inclusive)
+    c.validate_max_inclusive(errors, param_values, message_format % name, len(tzn))
 
 def validate_zone_mapping_B(zone_name, errors, data, name):
     message_format = "%s"
     param = data["params"][name]
+    param_values = param.values()
     tot = data["params"]["num_nodes"]
     tzn = data["params"][zone_name]
     c.validate_keys(errors, param, name, data["specs"][name]["type"], range(1, tot + 1))
-    c.validate_min_inclusive(errors, param.values(), message_format % name, 0)
-    c.validate_max_inclusive(errors, param.values(), message_format % name, len(tzn))
+    c.validate_min_inclusive(errors, param_values, message_format % name, 0)
+    c.validate_max_inclusive(errors, param_values, message_format % name, len(tzn))
 
 # TODO This is never called. Should it be?
 def val_single_cell_swrecharge_zone_mapping(errors, data, name):
