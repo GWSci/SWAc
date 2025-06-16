914ff9e02dcd57b3dee12b5e6108da37f58518b7
Chris Foley <chris@nice-code.co.uk>
2025-06-16 09:17:24 +0100

    Issue #154. Complete validations for V2 file format. 561

--- swacmod/input_files/input_files_version_2/validation_new.py
+++ swacmod/input_files/input_files_version_2/validation_new.py
@@ -75,16 +75,14 @@ def val_reporting_zone_mapping(errors, data, name):
 def val_rainfall_zone_mapping(errors, data, name):
     zone_name = "rainfall_zone_names"
     min_inclusive = 1
-    param = data["params"][name]
-    tot = data["params"]["num_nodes"]
-    rzn = data["params"][zone_name]
-    c.validate_keys(errors, param, name, data["specs"][name]["type"], range(1, tot + 1))
-    c.validate_min_inclusive(errors, [i[0] for i in param.values()], "zone in %s" % name, min_inclusive)
-    c.validate_max_inclusive(errors, [i[0] for i in param.values()], "zone in %s" % name, len(rzn))
+    validate_zone_mapping_A(zone_name, min_inclusive, errors, data, name)
 
 def val_pe_zone_mapping(errors, data, name):
     zone_name = "pe_zone_names"
     min_inclusive = 0
+    validate_zone_mapping_A(zone_name, min_inclusive, errors, data, name)
+
+def validate_zone_mapping_A(zone_name, min_inclusive, errors, data, name):
     param = data["params"][name]
     tot = data["params"]["num_nodes"]
     rzn = data["params"][zone_name]
