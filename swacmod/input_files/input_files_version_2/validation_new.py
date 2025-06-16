a9c54aa00dfffbe669558d4b7f77e2fa2b297f0d
Chris Foley <chris@nice-code.co.uk>
2025-06-16 12:31:20 +0100

    Issue #154. Complete validations for V2 file format. 584

--- swacmod/input_files/input_files_version_2/validation_new.py
+++ swacmod/input_files/input_files_version_2/validation_new.py
@@ -65,12 +65,13 @@ def val_node_areas(errors, data, name):
     c.validate_min_inclusive(errors, data["params"][name].values(), name, 0)
 
 def validate_zone_mapping_C(zone_name, errors, data, name):
+    min_inclusive = 0
     message_format = "zone in %s"
     param = data["params"][name]
     tot = data["params"]["num_nodes"]
     tzn = data["params"][zone_name]
     c.validate_keys(errors, param, name, data["specs"][name]["type"], range(1, tot + 1))
-    c.validate_min_inclusive(errors, param.values(), message_format % name, 0)
+    c.validate_min_inclusive(errors, param.values(), message_format % name, min_inclusive)
     c.validate_max_inclusive(errors, param.values(), message_format % name, len(tzn))
 
 def validate_zone_mapping_A(zone_name, min_inclusive, errors, data, name):
