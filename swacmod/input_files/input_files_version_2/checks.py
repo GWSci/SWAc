245ea633616bfdc6de32c798a60bef3c582ce3db
Chris Foley <chris@nice-code.co.uk>
2025-06-16 04:37:08 +0100

    Issue #154. Complete validations for V2 file format. 533

--- swacmod/input_files/input_files_version_2/checks.py
+++ swacmod/input_files/input_files_version_2/checks.py
@@ -81,7 +81,7 @@ def validate_min_inclusive(errors, values, name, low_l):
     try:
         if not all(i >= low_l for i in values):
             msg = 'Parameter "%s" requires values >= %s'
-            raise u.ValidationError(msg % (name, low_l))
+            errors.append(msg % (name, low_l))
     except TypeError:
         pass
 
