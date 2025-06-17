cd625592fe599a3ad5ef192be763e8de8d79c7ea
Charalampos Ntigkakis <99178328+hntig@users.noreply.github.com>
2025-06-17 11:54:39 +0100

    #147 update python version

--- setup_windows.bat
+++ setup_windows.bat
@@ -12,14 +12,14 @@ if %errorlevel% neq 0 (
 )
 
 @echo on
-py -3.12 -m venv env || exit /b
+python -m venv env || exit /b
 
 call env/Scripts/activate.bat || exit /b
 python -m pip install --upgrade pip || exit /b
 pip install -r requirements.txt || exit /b
 call env/Scripts/deactivate.bat || exit /b
 
-py -3.12 -m venv env-lint || exit /b
+python -m venv env-lint || exit /b
 
 call env-lint/Scripts/activate.bat || exit /b
 python -m pip install --upgrade pip || exit /b
