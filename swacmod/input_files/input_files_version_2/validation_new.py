d3c1ca0ba7f76c1ede97863686f4d5a2213073b9
Chris Foley <chris@nice-code.co.uk>
2025-06-19 08:45:19 +0100

    Issue #154. Complete validations for V2 file format. 649

--- swacmod/input_files/input_files_version_2/validation_new.py
+++ swacmod/input_files/input_files_version_2/validation_new.py
@@ -153,8 +153,7 @@ def val_single_cell_swrecharge_zone_mapping(errors, data, name):
 
 def val_free_throughfall(errors, data, name):
     param = data["params"][name]
-    tot = len(data["params"]["canopy_zone_names"])
-    c.validate_keys(errors, param, name, data["specs"][name]["type"], range(1, tot + 1))
+    validate_keys_are_zone_name_numbers("canopy_zone_names", errors, data, name)
     c.validate_min_inclusive(errors, param.values(), name, 0)
     c.validate_max_inclusive(errors, param.values(), name, 1.0)
 
