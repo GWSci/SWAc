6b760d96d4d92f63861ca770e92f5e10547a8bbc
Chris Foley <chris@nice-code.co.uk>
2025-06-20 08:21:37 +0100

    Issue #154. Complete validations for V2 file format. 682

--- swacmod/input_files/input_files_version_2/validation_context.py
+++ swacmod/input_files/input_files_version_2/validation_context.py
@@ -4,14 +4,18 @@ class Validation_Context_Factory:
         self.data = data
 
     def make(self, function, param):
-        return Validation_Context(self.errors, self.data, function, param)
+        return Validation_Context(self.errors, self.data, function, param, None)
+
+    def make_with_process_guard(self, function, param, required_process_key):
+        return Validation_Context(self.errors, self.data, function, param, required_process_key)
 
 class Validation_Context:
-    def __init__(self, errors, data, function, param):
+    def __init__(self, errors, data, function, param, required_process_key_or_none):
         self.errors = errors
         self.data = data
         self.function = function
         self.param = param
+        self.required_process_key_or_none = required_process_key_or_none
 
     def do_validation(self):
         if not self.is_param_skipped():
@@ -20,8 +24,11 @@ class Validation_Context:
     def is_param_skipped(self):
         param = self.param
         params = self.data["params"]
-        is_skipped = (params[param] is None) or self.is_alt()
+        is_skipped = (params[param] is None) or self.is_required_process_disabled() or self.is_alt()
         return is_skipped
+
+    def is_required_process_disabled(self):
+        pass
 
     def is_alt(self):
         value = self.data["params"][self.param]
