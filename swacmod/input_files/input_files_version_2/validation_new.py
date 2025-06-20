0602c137479f2b5a43baf8e6fb9db09419fa33af
Chris Foley <chris@nice-code.co.uk>
2025-06-20 08:28:41 +0100

    Issue #154. Complete validations for V2 file format. 687

--- swacmod/input_files/input_files_version_2/validation_new.py
+++ swacmod/input_files/input_files_version_2/validation_new.py
@@ -192,8 +192,6 @@ def val_soil_spatial(errors, data, name):
         errors.append(msg % name)
 
 def val_lu_spatial(errors, data, name):
-    if data["params"]["fao_process"] == "disabled":
-        return
     param = data["params"][name]
     validate_keys_are_nodes(errors, data, name)
     validate_list_length_equals_zone_name_count("landuse_zone_names", errors, data, name)
@@ -313,7 +311,7 @@ def _validate_params(errors, data):
         factory.make(val_soil_static_params, "soil_static_params"),
         factory.make(val_smd, "smd"),
         factory.make(val_soil_spatial, "soil_spatial"),
-        factory.make(val_lu_spatial, "lu_spatial"),
+        factory.make_with_process_guard(val_lu_spatial, "lu_spatial", "fao_process"),
         factory.make(val_zr, "zr"),
         factory.make(validate_fao_keys_and_list_length, "kc"),
         factory.make(validate_fao_keys_and_list_length, "taw"),
