f722796e001f5007d95a39a39e3b3e13259ee82d
Chris Foley <chris@nice-code.co.uk>
2025-06-24 11:16:26 +0100

    Hard-coded Python version for Windows.

--- setup_windows.bat
+++ setup_windows.bat
@@ -12,14 +12,14 @@ if %errorlevel% neq 0 (
 )
 
 @echo on
-python -m venv env || exit /b
+py -3.13 -m venv env || exit /b
 
 call env/Scripts/activate.bat || exit /b
 python -m pip install --upgrade pip || exit /b
 pip install -r requirements.txt || exit /b
 call env/Scripts/deactivate.bat || exit /b
 
-python -m venv env-lint || exit /b
+py -3.13 -m venv env-lint || exit /b
 
 call env-lint/Scripts/activate.bat || exit /b
 python -m pip install --upgrade pip || exit /b
