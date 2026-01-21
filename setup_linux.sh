8ad2d6670c9ac396bd25496b2962df2e203a0244
Chris Foley <chris@nice-code.co.uk>
2026-01-21 18:31:08 +0000

    Issue #176. Set Up Travis Pipeline. 11

--- setup_linux.sh
+++ setup_linux.sh
@@ -6,4 +6,13 @@ python3 -m venv env
 
 source env/bin/activate
 pip install -r requirements.txt
+pip list --outdated
+deactivate
+
+python3 -m venv env-lint
+
+source env-lint/bin/activate
+pip install -r requirements.txt
+pip install -r requirements-lint.txt
+pip list --outdated
 deactivate
