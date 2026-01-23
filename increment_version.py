d29a2f0799da055276766f8807d95446f0e29991
Chris Foley <chris@nice-code.co.uk>
2026-01-23 09:04:19 +0000

    Issue #176. Set Up Travis Pipeline. 59

--- increment_version.py
+++ increment_version.py
@@ -1,6 +1,7 @@
 import subprocess
 import ast
 
+semver_version_filename = "_version.txt"
 version_filename = '_version.py'
 commit_id_filename = '_commit_id.py'
 build_time_filename = '_build_time.py'
@@ -13,6 +14,9 @@ def increment_version():
     new_version = get_new_version(old_version)
     write_new_version(new_version)
     subprocess.run(['git', 'add', version_filename])
+
+    semver_version_contents = f"v{new_version[0]}.{new_version[1]}.{new_version[2]}."
+    spit(semver_version_filename, semver_version_contents)
 
 def get_old_version(filename=version_filename, file_open=_default_file_open):
     with file_open(filename, 'r') as file:
@@ -32,6 +36,10 @@ def get_new_version(old_version):
 def write_new_version(new_version, filename=version_filename, file_open=_default_file_open):
     with file_open(filename, 'w') as file:
         file.write(f'version = {new_version}')
+
+def spit(filename, contents):
+    with open(filename, 'w') as f:
+        f.write(contents)
 
 if (__name__ == "__main__"):
     increment_version()
