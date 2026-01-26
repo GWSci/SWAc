1538c77f0251f43ac93ba9667d9866fffe8b2d4a
Chris Foley <chris@nice-code.co.uk>
2026-01-26 07:08:05 +0000

    Issue #176. Set Up Travis Pipeline. 95

--- github_release.py
+++ github_release.py
@@ -2,7 +2,7 @@ def create_github_release():
     print("Hello, World!")
 
 def convert_version_to_tag_name(version):
-    return "v0.0.0"
+    return "v0.0." + str(version[2])
 
 if (__name__ == "__main__"):
     create_github_release()
--- test/test_github_release.py
+++ test/test_github_release.py
@@ -4,3 +4,4 @@ from github_release import convert_version_to_tag_name
 class Test_Github_Release(unittest.TestCase):
     def test_convert_version_to_tag_name(self):
         self.assertEqual("v0.0.0", convert_version_to_tag_name([0, 0, 0]))
+        self.assertEqual("v0.0.1", convert_version_to_tag_name([0, 0, 1]))
