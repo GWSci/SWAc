8f2db012c205067b81e48f0ae9790f93e9ed2c0f
Chris Foley <chris@nice-code.co.uk>
2026-01-26 09:30:52 +0000

    Issue #176. Set Up Travis Pipeline. 120

--- github_release.py
+++ github_release.py
@@ -1,8 +1,10 @@
 import os
 import _version
-from github_release_helper import parse_repo_slug, convert_version_to_tag_name, Create_Release_Raw_Inputs
+from github_release_helper import *
 
 def create_github_release():
+    raw_inputs = gather_create_release_raw_inputs()
+    create_release = convert_raw_inputs_to_create_release(raw_inputs)
     print("Hello, World!")
 
 def gather_create_release_raw_inputs():
@@ -14,35 +16,6 @@ def gather_create_release_raw_inputs():
         version = version,
         commit_id = commit_id,
     )
-
-def make_create_release():
-    # gather
-    repo_slug = os.environ["TRAVIS_REPO_SLUG"]
-    version = _version.version
-    commit_id = os.environ["TRAVIS_COMMIT"]
-    raw_inputs = gather_create_release_raw_inputs()
-
-    # parse
-    owner, repo = parse_repo_slug(repo_slug)
-    tag_name = convert_version_to_tag_name(version)
-
-    # organise
-    header = {
-        "accept": "application/vnd.github+json",
-    }
-    path = {
-        "owner": owner,
-        "repo": repo,
-    }
-    body = {
-        "tag_name": tag_name,
-        "target_commitish": commit_id
-    }
-    return {
-        "header": header,
-        "path": path,
-        "body": body
-    }
 
 if (__name__ == "__main__"):
     create_github_release()
