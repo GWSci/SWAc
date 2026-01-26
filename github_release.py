ca467198e91e1366c43137dc0e85af2efd5df8ba
Chris Foley <chris@nice-code.co.uk>
2026-01-26 07:08:32 +0000

    Issue #176. Set Up Travis Pipeline. 96

--- github_release.py
+++ github_release.py
@@ -2,7 +2,8 @@ def create_github_release():
     print("Hello, World!")
 
 def convert_version_to_tag_name(version):
-    return "v0.0." + str(version[2])
+    patch = str(version[2])
+    return f"v0.0.{patch}"
 
 if (__name__ == "__main__"):
     create_github_release()
