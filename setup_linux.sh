4cefb4b9b31303d50f0de2dce7c6b41a316b4764
Chris Foley <chris@nice-code.co.uk>
2026-01-27 10:13:33 +0000

    Issue #176. Set Up Travis Pipeline. 160

--- setup_linux.sh
+++ setup_linux.sh
@@ -3,9 +3,13 @@
 set -e
 
 sudo apt-get update
+
+sudo apt-get -y install python3.13
 sudo apt-get -y install pandoc
 
-python3 -m venv env
+python3.13 -m venv env
+
+ls env/Scripts/
 
 source env/bin/activate
 pip install -r requirements.txt
