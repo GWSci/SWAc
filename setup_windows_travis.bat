96483748774d50ae3864c1dd80310042f73e2caf
Chris Foley <chris@nice-code.co.uk>
2026-01-28 05:40:19 +0000

    Issue #176. Set Up Travis Pipeline. 180

--- setup_windows_travis.bat
+++
@@ -1,9 +0,0 @@
-choco install python313 -y
-choco install pandoc -y
-
-py -3.13 -m venv env || exit /b
-
-call env/Scripts/activate.bat || exit /b
-python -m pip install --upgrade pip || exit /b
-pip install -r requirements.txt || exit /b
-call env/Scripts/deactivate.bat || exit /b
